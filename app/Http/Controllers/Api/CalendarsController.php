4d158f22ad389f792d05c8b5985e21e0cc342975
John <juaorok@hotmail.com>
2019-11-22 17:02:58 -0300

    Create CalendarsController.php

---
+++ app/Http/Controllers/Api/CalendarsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CalendarResource;
+use App\Models\Calendar;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Response;
+
+class CalendarsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return ResourceCollection
+     */
+    public function index()
+    {
+        $calendars = Calendar::all()->where('is_enabled', '=', true);
+        return CalendarResource::collection($calendars);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
