038de4323d82766754e1ab9701c18352ad5fecd7
John <juaorok@hotmail.com>
2019-11-22 17:02:09 -0300

    Create CalendarResource.php

---
+++ app/Http/Resources/CalendarResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CalendarResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->name,
+            'slug' => $this->slug,
+            'start' => $this->start_date->format('Y-m-d H:i'),
+            'end' => $this->end_date->format('Y-m-d H:i'),
+            'textColor' => $this->color
+        ];
+    }
+}
