712ec0d26ce25f42ab6fee0eb19340d2a7043e3d
John <juaorok@hotmail.com>
2019-11-22 18:50:52 -0300

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,9 +20,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-//        Registered::class => [
-//            SendEmailVerificationNotification::class,
-//        ],
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
         Failed::class => [
             FailedLoginListener::class
         ],
