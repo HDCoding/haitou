d5a9017e3cd33d992eea76f6c19d7e041487fadd
John <juaorok@hotmail.com>
2019-11-22 18:50:17 -0300

    Create LockscreenController.php

---
+++ app/Http/Controllers/Auth/LockscreenController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LockscreenRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LockscreenController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('lockscreen');
+    }
+
+    public function lock()
+    {
+        if (!session()->has('locked')) {
+            session()->put('locked', [
+                'status' => true,
+                'page' => url()->previous()
+            ]);
+        }
+
+        return view('auth.lockscreen');
+    }
+
+    public function unlock(LockscreenRequest $request)
+    {
+        $check = Hash::check($request->input('password'), $request->user()->password);
+
+        if (!$check) {
+            return redirect()->route('lockscreen')->withErrors(['Sua senha não corresponde ao seu perfil.']);
+        }
+
+        $lock = $this->getLock();
+
+        session()->put('locked', [
+            'status' => false,
+            'page' => ''
+        ]);
+
+        return redirect()->to($lock['page']);
+    }
+
+    public function getLock()
+    {
+        return session()->get('locked');
+    }
+}
