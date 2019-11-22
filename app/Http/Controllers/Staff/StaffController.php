19706e12dc57a4b80f3c89060648655782385539
John <juaorok@hotmail.com>
2019-11-22 20:17:45 -0300

    Create StaffController.php

---
+++ app/Http/Controllers/Staff/StaffController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Helpers\SystemInformation;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+use Spatie\SslCertificate\SslCertificate;
+
+class StaffController extends Controller
+{
+    public function index()
+    {
+        $pendent = User::where('status', '=', 0)->count();
+        $activated = User::where('status', '=', 1)->count();
+        $suspended = User::where('status', '=', 2)->count();
+        $banned = User::where('status', '=', 3)->count();
+
+        $system = new SystemInformation();
+
+        $certificate = SslCertificate::createForHostName('spatie.be');//config('app.url')
+
+        return view('staff.staff.index', compact('pendent', 'activated', 'suspended', 'banned', 'system', 'certificate'));
+    }
+}
