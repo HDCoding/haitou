9da89f2f3a7823f8b38516bd6f0ceaf1f50b4742
John <juaorok@hotmail.com>
2019-11-22 18:50:22 -0300

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Mail\AccountActivation;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
@@ -23,12 +27,14 @@ class RegisterController extends Controller
 
     use RegistersUsers;
 
+    protected $points;
+
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/register';
 
     /**
      * Create a new controller instance.
@@ -38,35 +44,42 @@ class RegisterController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
-    }
-
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
+        $this->points = setting('points_signup');
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param RegisterRequest $request
      * @return \App\User
      */
-    protected function create(array $data)
+    protected function register(RegisterRequest $request)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        $points = $this->points;
+
+        //Account activation code
+        $code = sha1_gen();
+
+        //Create new user account
+        $user = new User();
+        $user->role_id = 1;
+        $user->username = $request->input('username');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->state_id = 25;
+        $user->points += $points;
+        $user->experience += $points;
+        $user->passkey = md5_gen();
+        $user->birthday = Carbon::today();
+        $user->mood_id = 1;
+        $user->code = $code;
+        $user->save();
+
+        //send email to new member
+        Mail::to($user)->send(new AccountActivation($code));
+
+        //informs that the account was created
+        return view('auth.activation')
+            ->with('info', 'Conta criada com sucesso, verifique seu e-mail para ativar sua conta, Caixa de Entrada ou SPAM.');
     }
 }
