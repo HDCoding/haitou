40506a0d6b9e5319aba5430b550ea721a92965a0
John <juaorok@hotmail.com>
2019-11-22 22:41:10 -0300

    Update ReportsController.php

--- app/Http/Controllers/Site/ReportsController.php
+++ app/Http/Controllers/Site/ReportsController.php
@@ -3,83 +3,70 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Site\ReportRequest;
+use App\Models\Calendar;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Report;
+use App\Models\Torrent;
+use App\User;
 use Illuminate\Http\Request;
 
-class \ReportsController extends Controller
+class ReportsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function calendar($calendar_id)
     {
-        //
+        $calendar = Calendar::findOrFail($calendar_id);
+        return view('site.reports.calendar', compact('calendar'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function comment($comment_id)
     {
-        //
+        $comment = Comment::findOrFail($comment_id);
+        return view('site.reports.comment', compact('comment'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function post($post_id)
     {
-        //
+        $post = Post::findOrFail($post_id);
+        return view('site.reports.post', compact('post'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function torrent($torrent_id)
     {
-        //
+        $torrent = Torrent::findOrFail($torrent_id);
+        return view('site.reports.torrent', compact('torrent'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function user($user_id)
     {
-        //
+        $user = User::findOrFail($user_id)->select(['id', 'name']);
+        return view('site.reports.user', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function store(ReportRequest $request)
     {
-        //
-    }
+        $user = $request->user();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $report = new Report();
+        $report->user_id = $user->id;
+        $report->calendar_id = $request->input('calendar_id');
+        $report->comment_id = $request->input('comment_id');
+        $report->member_id = $request->input('member_id');
+        $report->post_id = $request->input('post_id');
+        $report->torrent_id = $request->input('torrent_id');
+        $report->report_type = $request->input('report_type');
+        $report->name = $request->input('name');
+        $report->reason = $request->input('reason');
+        $report->save();
+
+        //give points to user
+        $points = setting('points_report');
+        $user->updatePoints($points);
+
+//        $this->log::novo('Registrou um novo report.');
+
+        toastr()->success('Report recebido com sucesso. Iremos analizar o mais breve possível.', 'Report');
+        return redirect()->to('home');
     }
 }
