80fa933fca954dd829de258b560435f42046ef49
John <juaorok@hotmail.com>
2019-11-22 22:40:54 -0300

    Update HomeController.php

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class \HomeController extends Controller
+class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
