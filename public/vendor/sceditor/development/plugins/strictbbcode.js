f6b975f931312c7b1d382affd565d69060c57759
John <juaorok@hotmail.com>
2019-11-23 16:54:37 -0300

    Add Package SCEditor
    Add Package SCEditor

---
+++ public/vendor/sceditor/development/plugins/strictbbcode.js
@@ -0,0 +1,21 @@
+/**
+ * SCEditor Strict BBCode Plugin
+ * http://www.sceditor.com/
+ *
+ * Copyright (C) 2016, Sam Clarke (samclarke.com)
+ *
+ * SCEditor is licensed under the MIT license:
+ *	http://www.opensource.org/licenses/mit-license.php
+ *
+ * @author Sam Clarke
+ */
+// (function (sceditor) {
+// 	'use strict';
+
+// 	// var extend = sceditor.utils.extend;
+
+// 	sceditor.plugins.strictbbcode = function () {
+// 		// override bbcode plugin add and update default bbcodes to have attrs
+// 		// and override their exec funcs
+// 	};
+// }(sceditor));
