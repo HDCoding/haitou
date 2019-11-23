1be38473ad20774ff0d2268266afa70d5095cb07
John <juaorok@hotmail.com>
2019-11-22 22:40:56 -0300

    Update InvitationsController.php

--- app/Http/Controllers/Site/InvitationsController.php
+++ app/Http/Controllers/Site/InvitationsController.php
@@ -3,83 +3,73 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Site\InvitationRequest;
+use App\Mail\AccountInvitation;
+use App\Models\Invitation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
-class \InvitationsController extends Controller
+class InvitationsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+        $invites = $user->invited_users()->where('user_id', '=', $user->id)->where('is_accepted', '=', false)->get();
+        return view('site.invites.index', compact('invites'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(InvitationRequest $request)
     {
-        //
+        //membro que esta convidando
+        $user = $request->user();
+        //email de quem vai receber o convite
+        $email = $request->input('email');
+        //codigo enviado para o(a) convidado(a)
+        $code = sha1_gen();
+
+        $invitedays = $this->setting->setting('invitedays');
+
+        //insere os dados no banco
+        $invite = new Invitation();
+        $invite->user_id = $user->id;
+        $invite->email = $email;
+        $invite->code = $code;
+        $invite->expires_on = Carbon::now()->addDays($invitedays);
+        $invite->save();
+
+        //subtrai um "invite" de quem enviou o convite
+        $user->invites -= 1;
+        $user->save();
+
+        //send email
+        Mail::to($email)->send(new AccountInvitation($invite));
+
+        // Activity Log
+        $this->log::novo("Membro {$user->name} enviou um convite para {$email} .");
+
+        toastr()->success('Convite enviado com sucesso.', 'Sucesso');
+        return redirect()->to('invites');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function resend(Request $request, $invite_id)
     {
-        //
-    }
+        $user = $request->user();
+        $invite = Invitation::findOrFail($invite_id);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        abort_unless($invite->user_id === $user->id, 403);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        if ($invite->accepted_by !== null) {
+            toastr()->error('O convite que você está tentando re-enviar já foi usado.', 'Aviso');
+            return redirect()->route('invites');
+        }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+        Mail::to($invite->email)->send(new AccountInvitation($invite));
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        // Activity Log
+//        $this->log::novo("Membro {$user->name} reenviou o convite para {$invite->email} .");
+
+        toastr()->success('O convite foi reenviado com sucesso!', 'Aviso');
+        return redirect()->route('invites');
     }
 }
