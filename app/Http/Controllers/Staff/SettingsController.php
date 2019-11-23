e9f987c2f3dc6927ef642bae65dc6db3fde036c0
John <juaorok@hotmail.com>
2019-11-22 21:43:39 -0300

    Create SettingsController.php

---
+++ app/Http/Controllers/Staff/SettingsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Staff\SettingsRequest;
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+    public function index()
+    {
+        $setting = Setting::all();
+        return view('staff.settings.index', compact('setting'));
+    }
+
+    public function update(SettingsRequest $request, $setting_id)
+    {
+        Setting::findOrFail($setting_id)->update($request->except('_token'));
+        toastr()->info('Configurações atualizada.', 'Sucesso');
+        return redirect()->to('staff/settings');
+    }
+}
