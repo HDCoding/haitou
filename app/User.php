ec8324d0887170255090a367c6c4f26ea7f9e106
John <juaorok@hotmail.com>
2019-11-23 20:55:54 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -2,12 +2,42 @@
 
 namespace App;
 
+use App\Models\Attachment;
+use App\Models\Bookmark;
+use App\Models\Calendar;
+use App\Models\Cheater;
+use App\Models\Comment;
+use App\Models\Complete;
+use App\Models\Donation;
+use App\Models\FailedLogin;
+use App\Models\Fansub;
 use App\Models\Group;
+use App\Models\Historic;
 use App\Models\Invitation;
+use App\Models\Like;
 use App\Models\Log;
+use App\Models\Login;
+use App\Models\Lottery;
+use App\Models\LotteryWinner;
+use App\Models\Moderate;
+use App\Models\Moderator;
 use App\Models\Mood;
+use App\Models\News;
+use App\Models\Note;
+use App\Models\Peer;
+use App\Models\Permission;
+use App\Models\Poll;
+use App\Models\Post;
+use App\Models\Rating;
+use App\Models\Report;
 use App\Models\State;
+use App\Models\Subscription;
+use App\Models\Thank;
+use App\Models\Topic;
+use App\Models\Torrent;
+use App\Models\UserBonus;
 use App\Models\Vip;
+use App\Models\Vote;
 use App\Traits\UsersOnline;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Gstt\Achievements\Achiever;
@@ -141,17 +171,104 @@ class User extends Authenticatable
 
     public function group()
     {
-        return $this->belongsTo(Group::class);
+        return $this->belongsTo(Group::class, 'group_id');
     }
 
     public function mood()
     {
-        return $this->belongsTo(Mood::class);
+        return $this->belongsTo(Mood::class, 'mood_id');
     }
 
     public function state()
     {
-        return $this->belongsTo(State::class);
+        return $this->belongsTo(State::class, 'state_id');
+    }
+
+    public function user_bonus()
+    {
+        return $this->hasMany(UserBonus::class, 'user_id');
+    }
+
+    public function bookmarks()
+    {
+        return $this->hasMany(Bookmark::class, 'user_id');
+    }
+
+    public function calendars()
+    {
+        return $this->hasMany(Calendar::class, 'user_id');
+    }
+
+    public function cheaters()
+    {
+        return $this->hasMany(Cheater::class, 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class, 'user_id');
+    }
+
+    public function donations()
+    {
+        return $this->hasMany(Donation::class, 'user_id');
+    }
+
+    public function failed_logins()
+    {
+        return $this->hasMany(FailedLogin::class, 'user_id');
+    }
+
+    public function fansubs()
+    {
+        return $this->belongsToMany(Fansub::class, 'fansub_users')
+            ->withPivot('id', 'job', 'is_admin')
+            ->withTimestamps();
+    }
+
+    public function attachments()
+    {
+        return $this->hasMany(Attachment::class, 'user_id');
+    }
+
+    public function moderators()
+    {
+        return $this->hasMany(Moderator::class, 'staff_id');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'user_id');
+    }
+
+    public function subscriptions()
+    {
+        return $this->hasMany(Subscription::class, 'user_id');
+    }
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
+
+    public function historics()
+    {
+        return $this->hasMany(Historic::class, 'user_id');
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class, 'user_id');
+    }
+
+    public function logins()
+    {
+        return $this->hasMany(Login::class, 'user_id');
     }
 
     public function logs()
@@ -159,14 +276,81 @@ class User extends Authenticatable
         return $this->hasMany(Log::class, 'user_id');
     }
 
+    public function lotteries()
+    {
+        return $this->belongsToMany(Lottery::class, 'lottery_users')
+            ->withPivot('id', 'numbers')
+            ->withTimestamps();
+    }
+
+    public function lottery_winners()
+    {
+        return $this->hasMany(LotteryWinner::class, 'user_id');
+    }
+
+    public function moderates()
+    {
+        return $this->hasMany(Moderate::class);
+    }
+
+    public function news()
+    {
+        return $this->hasMany(News::class, 'staff_id');
+    }
+
+    public function peers()
+    {
+        return $this->hasMany(Peer::class, 'user_id');
+    }
+
+    public function permission()
+    {
+        return $this->hasOne(Permission::class, 'user_id');
+    }
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class, 'user_id');
+    }
+
+    public function polls()
+    {
+        return $this->hasMany(Poll::class, 'user_id');
+    }
+
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class, 'user_id');
+    }
+
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
+
+    public function thanks()
+    {
+        return $this->hasMany(Thank::class, 'user_id');
+    }
+
+    public function completes()
+    {
+        return $this->hasMany(Complete::class, 'user_id');
+    }
+
+    public function torrents()
+    {
+        return $this->hasMany(Torrent::class, 'uploader_id');
+    }
+
+    public function notes()
+    {
+        return $this->hasMany(Note::class, 'user_id');
+    }
+
     public function vips()
     {
         return $this->hasMany(Vip::class, 'user_id');
-    }
-
-    public function invitations()
-    {
-        return $this->hasMany(Invitation::class);
     }
 
     public function sluggable()
@@ -233,7 +417,7 @@ class User extends Authenticatable
 
     public function levelImage()
     {
-        $level = $this->getLevel();
+        $level = $this->level();
         return asset("images/ranks/{$level}.png");
     }
 
@@ -253,4 +437,12 @@ class User extends Authenticatable
             $user->save();
         }
     }
+
+    public function groupName()
+    {
+        if (auth()->check()) {
+            return $this->group()->select('name')->pluck('name')->first();
+        }
+    }
+
 }
