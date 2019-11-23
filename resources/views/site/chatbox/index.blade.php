4d5b733a752ece73937fdae432ef4f4bd46a5118
John <juaorok@hotmail.com>
2019-11-23 15:37:36 -0300

    Update index.blade.php

---
+++ resources/views/site/chatbox/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('subtitle', 'Chat')
+
+@section('content')
+
+    <div class="font-weight-bold py-3 h4">Chat</div>
+
+    <hr class="border-light container-m--x mt-0 mb-4">
+
+    Em desenvolvimento
+
+@endsection
