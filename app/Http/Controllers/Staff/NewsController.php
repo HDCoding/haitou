c7cdc6447e19a057f4b85cc5be6c09464dde0a83
John <juaorok@hotmail.com>
2019-11-23 16:55:52 -0300

    Update NewsController.php

--- app/Http/Controllers/Staff/NewsController.php
+++ app/Http/Controllers/Staff/NewsController.php
@@ -12,7 +12,7 @@ class NewsController extends Controller
     public function index()
     {
         $news = News::with('user:id,name')
-            ->select('id', 'user_id', 'name', 'views')
+            ->select('id', 'user_id', 'username', 'views')
             ->orderBy('id', 'DESC')->get();
         return view('staff.news.index', compact('news'));
     }
