ad7111a4e6ebbf6822a3d00d23fe1b5879a32e26
John <juaorok@hotmail.com>
2019-11-22 22:40:17 -0300

    Update ActorsController.php

--- app/Http/Controllers/Site/ActorsController.php
+++ app/Http/Controllers/Site/ActorsController.php
@@ -3,83 +3,20 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Actor;
+use App\Models\Bookmark;
 use Illuminate\Http\Request;
 
-class \ActorsController extends Controller
+class ActorsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function show($actor_id, $slug)
     {
-        //
-    }
+        $actor = Actor::where('id', '=', $actor_id)->whereSlug($slug)->firstOrFail();
+        $actor->increment('views');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $user_id = auth()->user()->id;
+        $bookmarked = Bookmark::where('actor_id', '=', $actor->id)->where('user_id', '=', $user_id)->first();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('site.actors.actor', compact('actor', 'bookmarked'));
     }
 }
