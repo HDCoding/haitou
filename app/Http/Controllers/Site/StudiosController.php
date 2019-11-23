852a9823c9897cc74dcb490403b7a40beff4c37b
John <juaorok@hotmail.com>
2019-11-22 22:41:18 -0300

    Update StudiosController.php

--- app/Http/Controllers/Site/StudiosController.php
+++ app/Http/Controllers/Site/StudiosController.php
@@ -3,83 +3,22 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Studio;
 use Illuminate\Http\Request;
 
-class \StudiosController extends Controller
+class StudiosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function show($studio_id, $slug)
     {
-        //
-    }
+        $studio = Studio::where('id', '=', $studio_id)->whereSlug($slug)->firstOrFail();
+        $studio->increment('views');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $comments = $studio->comments()->latest()->paginate(5);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+        if (request()->ajax()) {
+            return view('includes.comments', compact('comments'));
+        }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return view('site.studios.studio', compact('studio', 'comments'));
     }
 }
