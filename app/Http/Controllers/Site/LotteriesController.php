9be75f7c09306bc0b2104db3140ff2dc83003d1c
John <juaorok@hotmail.com>
2019-11-22 22:41:00 -0300

    Update LotteriesController.php

--- app/Http/Controllers/Site/LotteriesController.php
+++ app/Http/Controllers/Site/LotteriesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class \LotteriesController extends Controller
+class LotteriesController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,7 +14,7 @@ class \LotteriesController extends Controller
      */
     public function index()
     {
-        //
+        return view('site.lotteries.index');
     }
 
     /**
