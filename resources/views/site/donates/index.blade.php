63291327739e636aafa4ecf960a20f653efb8f86
John <juaorok@hotmail.com>
2019-11-23 15:37:41 -0300

    Update index.blade.php

---
+++ resources/views/site/donates/index.blade.php
@@ -0,0 +1,237 @@
+@extends('layouts.dashboard')
+
+@section('subtitle', 'Doações')
+
+@section('content')
+
+    <div class="container px-0">
+
+        <h1 class="text-center font-weight-bolder pt-5 mb-3">
+            Doações
+        </h1>
+        <div class="text-center text-muted text-large font-weight-light">
+            Sua doação ajuda a manter o seu site no ar.
+        </div>
+
+        <div class="row no-gutters row-bordered ui-bordered text-center">
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-briefcase text-big"></i></div>
+                <h5 class="m-0">Bronze</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">5</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">10 GB</p>
+                        <p class="ui-company-text mb-2">14 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-store text-big"></i></div>
+                <h5 class="m-0">Prata</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">10</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">20 GB</p>
+                        <p class="ui-company-text mb-2">28 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-apartment text-big"></i></div>
+                <h5 class="m-0">Ouro</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">15</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">30 GB</p>
+                        <p class="ui-company-text mb-2">56 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-briefcase text-big"></i></div>
+                <h5 class="m-0">Esmeralda</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">20</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">40 GB</p>
+                        <p class="ui-company-text mb-2">84 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row no-gutters row-bordered ui-bordered text-center">
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-store text-big"></i></div>
+                <h5 class="m-0">Diamante</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">30</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">50 GB</p>
+                        <p class="ui-company-text mb-2">112 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-apartment text-big"></i></div>
+                <h5 class="m-0">Elite</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">40</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">65 GB</p>
+                        <p class="ui-company-text mb-2">140 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-apartment text-big"></i></div>
+                <h5 class="m-0">Mestre</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">50</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">80 GB</p>
+                        <p class="ui-company-text mb-2">168 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+            <div class="d-flex col-md flex-column py-4">
+                <div class="display-1 text-primary my-4"><i class="lnr lnr-apartment text-big"></i></div>
+                <h5 class="m-0">Mito</h5>
+                <div class="flex-grow-1">
+                    <div class="py-4 my-2">
+                        <span class="d-inline-block text-muted text-big align-middle mr-2">R$</span>
+                        <span class="display-3 d-inline-block font-weight-bold align-middle">100</span>
+                    </div>
+                    <div class="pb-5">
+                        <p class="ui-company-text mb-2">100 GB</p>
+                        <p class="ui-company-text mb-2">200 VIP</p>
+                        <p class="ui-company-text mb-0">00 Points</p>
+                        <p class="ui-company-text mb-0">00 Invites</p>
+                    </div>
+                </div>
+                <div class="px-md-3 px-lg-5">
+                    <a href="javascript:void(0)" class="btn btn-outline-primary btn-lg btn-round">Escolher</a>
+                </div>
+            </div>
+
+        </div>
+
+        <hr class="border-light mt-5">
+        <h3 class="text-center font-weight-light my-5">Dúvidas</h3>
+
+        <div class="row">
+            <div class="col-md-6">
+
+                <h5 class="mb-2">Dapibus ac facilisis in?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Lorem ipsum dolor sit amet, ius virtute suscipit te. Ius prima euismod consequat eu.
+                </p>
+
+                <h5 class="mb-2">Cras justo odio?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Etiam vivendo eu sea, purto ponderum mediocritatem at pro.
+                </p>
+
+                <h5 class="mb-2">Porta ac consectetur ac?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Iuvaret deleniti vulputate nec ne, id vix lucilius legendos deseruisse.
+                </p>
+
+                <h5 class="mb-2">Ne ornatus albucius ius?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Lorem ipsum dolor sit amet, mea in pertinax hendrerit gloriatur.
+                </p>
+
+            </div>
+            <div class="col-md-6">
+
+                <h5 class="mb-2">Ne ornatus albucius ius?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Lorem ipsum dolor sit amet, mea in pertinax hendrerit gloriatur.
+                </p>
+
+                <h5 class="mb-2">Quo insolens intellegam dissentiet at?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Ex fugit legimus fuisset per. Ex quidam option diceret ius.
+                </p>
+
+                <h5 class="mb-2">Ad his assum delenit blandit?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Ne ornatus albucius ius, nostrum dignissim repudiandae an usu.
+                </p>
+
+                <h5 class="mb-2">Dapibus ac facilisis in?</h5>
+                <p class="ui-company-text text-muted mb-4">
+                    Lorem ipsum dolor sit amet, ius virtute suscipit te. Ius prima euismod consequat eu.
+                </p>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
