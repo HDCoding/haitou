a0113d2b9c768124813442c957db723015f415f7
John <juaorok@hotmail.com>
2019-11-23 15:44:09 -0300

    Update index.blade.php

--- resources/views/staff/logs/index.blade.php
+++ resources/views/staff/logs/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Staff - Logs')
+@section('subtitle', trans('dashboard.logs'))
 
 @section('content')
+
+    <div class="font-weight-bold py-3 h4">Logs</div>
+
+    <hr class="border-light container-m--x mt-0 mb-4">
 
 @endsection
