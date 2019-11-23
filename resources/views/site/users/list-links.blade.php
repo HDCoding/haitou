ba9cabe162f4b570eb93645957301083e993f586
John <juaorok@hotmail.com>
2019-11-23 15:42:01 -0300

    Create list-links.blade.php

---
+++ resources/views/site/users/list-links.blade.php
@@ -0,0 +1,10 @@
+<div class="col-md-3 pt-0">
+    <div class="list-group list-group-flush account-settings-links">
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/account') }}" href="{{ url('user/edit/account') }}">Geral</a>
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/password') }}" href="{{ url('user/edit/password') }}">Alterar senha</a>
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/privacy') }}" href="{{ url('user/edit/privacy') }}">Privacidade</a>
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/social') }}" href="{{ url('user/edit/social') }}">Social e Info</a>
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/passkey') }}" href="{{ url('user/edit/passkey') }}">Passkey</a>
+        <a class="list-group-item list-group-item-action {{ ative_page('user/edit/email') }}" href="{{ url('user/edit/email') }}">E-mail</a>
+    </div>
+</div>
