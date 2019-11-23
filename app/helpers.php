ae34ec0ba48becf4c5067537b70f088f4687bf19
John <juaorok@hotmail.com>
2019-11-23 16:55:28 -0300

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -141,3 +141,42 @@ if (!function_exists('hideref')) {
         return "https://dereferer.me/?".urlencode($strUrl);
     }
 }
+
+
+if (!function_exists('format_date')) {
+    /**
+     * Formata data do timestamp
+     */
+    function format_date($date)
+    {
+        return \Carbon\Carbon::parse($date)->format('d/m/Y');
+    }
+}
+
+if (! function_exists('setting')) {
+    /**
+     * Get / set the specified setting value.
+     *
+     * If an array is passed, we'll assume you want to set settings.
+     *
+     * @param string|array $key
+     * @param mixed $default
+     * @return mixed|\App\Models\Setting
+     */
+    function setting($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('setting');
+        }
+
+        if (is_array($key)) {
+            return app('setting')->set($key);
+        }
+
+        try {
+            return app('setting')->get($key, $default);
+        } catch (\PDOException $e) {
+            return $default;
+        }
+    }
+}
