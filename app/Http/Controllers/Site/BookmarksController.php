c33f3aabbe1d2af574297f5cf422e800cd3b7069
John <juaorok@hotmail.com>
2019-11-22 22:40:30 -0300

    Update BookmarksController.php

--- app/Http/Controllers/Site/BookmarksController.php
+++ app/Http/Controllers/Site/BookmarksController.php
@@ -3,83 +3,71 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Bookmark;
 use Illuminate\Http\Request;
 
-class \BookmarksController extends Controller
+class BookmarksController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $user = $request->user();
+
+        $favorite = new Bookmark();
+        $favorite->user_id = $user->id;
+        $favorite->character_id = $request->input('character_id');
+        $favorite->actor_id = $request->input('actor_id');
+        $favorite->media_id = $request->input('media_id');
+        $favorite->save();
+
+        return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function destroy($bookmark_id)
     {
-        //
+        $user = auth()->user();
+
+        $bookmark = Bookmark::findOrFail($bookmark_id);
+
+        abort_unless($bookmark->user_id == $user->id, 401);
+
+        $bookmark->delete();
+
+        return redirect()->back();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function actors(Request $request)
     {
-        //
+        //return all logged user bookmarks
+        $user = $request->user();
+
+        $bookmarks = Bookmark::with('actor:id,name,slug,image')
+            ->where('user_id', '=', $user->id)
+            ->where('actor_id', '!=', null)->paginate(30);
+
+        return view('site.users.bookmarks.actors', compact('bookmarks'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function characters(Request $request)
     {
-        //
+        //return all logged user bookmarks
+        $user = $request->user();
+
+        $bookmarks = Bookmark::with('character:id,name,slug,image')
+            ->where('user_id', '=', $user->id)
+            ->where('character_id', '!=', null)->paginate(30);
+
+        return view('site.users.bookmarks.characters', compact('bookmarks'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function medias(Request $request)
     {
-        //
+        //return all logged user bookmarks
+        $user = $request->user();
+
+        $bookmarks = Bookmark::with('media:id,name,slug,poster')
+            ->where('user_id', '=', $user->id)
+            ->where('media_id', '!=', null)->paginate(30);
+
+        return view('site.users.bookmarks.medias', compact('bookmarks'));
     }
 }
