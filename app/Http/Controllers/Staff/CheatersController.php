e67684c61b189f7b93ad0d5019d93812e2e15268
John <juaorok@hotmail.com>
2019-11-22 21:42:56 -0300

    Create CheatersController.php

---
+++ app/Http/Controllers/Staff/CheatersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use App\Models\Cheater;
+use App\Models\Historic;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CheatersController extends Controller
+{
+    /**
+     * Possible Ghost Leech Cheaters.
+     */
+    public function index()
+    {
+        //cheaters
+        $cheaters = Historic::with('user,id,name')
+            ->select(['*'])
+            ->join(
+                DB::raw('(SELECT MAX(id) AS id FROM historics GROUP BY historics.user_id) AS unique_history'),
+                function ($join) {
+                    $join->on('historics.id', '=', 'unique_history.id');
+                }
+            )
+            ->where('is_seeder', '=', 0)
+            ->where('is_active', '=', 1)
+            ->where('seed_time', '=', 0)
+            ->where('real_downloaded', '=', 0)
+            ->where('real_uploaded', '=', 0)
+            ->whereNull('completed_at')
+            ->latest()
+            ->paginate(30);
+
+        //programs from DB
+        $programs = Cheater::paginate(30);
+
+        return view('staff.cheaters.index', compact('cheaters', 'programs'));
+    }
+}
