8bc43f4f0e19a9722a7e0ae48a2e7cf7fe3f4ad2
John <juaorok@hotmail.com>
2019-11-23 15:45:24 -0300

    Create hourly.blade.php

---
+++ resources/views/staff/traffics/hourly.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.dashboard')
+
+@section('subtitle', 'Tráfico: Por Hora')
+
+@section('content')
+
+    <div class="font-weight-bold py-3 h4">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="{{ url('staff') }}">@lang('dashboard.title')</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{ url('staff/traffics') }}">Tráficos</a>
+            </li>
+            <li class="breadcrumb-item active">Por Hora</li>
+        </ol>
+    </div>
+
+    <hr class="container-m-nx border-light mt-0 mb-5">
+
+    <div class="card">
+        <div class="card-header">Tráfico: Por Hora</div>
+        <table class="table card-table">
+            <thead class="thead-light">
+                <tr>
+                    <th>Hora</th>
+                    <th>Recebido</th>
+                    <th>Enviado</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($hourly as $key => $value)
+                <tr>
+                    <td>{{ $value['label'] }}</td>
+                    <td>{{ $value['rx'] }}</td>
+                    <td>{{ $value['tx'] }}</td>
+                    <td>{{ $value['total'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+@endsection
