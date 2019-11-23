590d8b8a8ecb250959e62bbe8d4535326dda67de
John <juaorok@hotmail.com>
2019-11-22 22:41:13 -0300

    Update RulesController.php

--- app/Http/Controllers/Site/RulesController.php
+++ app/Http/Controllers/Site/RulesController.php
@@ -2,84 +2,24 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Achievements\UserReadRules;
 use App\Http\Controllers\Controller;
+use App\Models\Rule;
 use Illuminate\Http\Request;
 
-class \RulesController extends Controller
+class RulesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $rules = Rule::select('id', 'name', 'description')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+        $user = $request->user();
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if ($user->read_rules == false) {
+            $user->unlock(new UserReadRules());
+            $user->read_rules = true;
+            $user->save();
+        }
+        return view('site.rules.index', compact('rules'));
     }
 }
