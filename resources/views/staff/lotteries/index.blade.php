3ad84299bb38068bf9e56f58e21de23d52c83b24
John <juaorok@hotmail.com>
2019-11-23 15:44:11 -0300

    Update index.blade.php

--- resources/views/staff/lotteries/index.blade.php
+++ resources/views/staff/lotteries/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Staff - Sorteios')
+@section('subtitle', trans('dashboard.lotteries'))
 
 @section('content')
+
+    <div class="font-weight-bold py-3 h4">Sorteios</div>
+
+    <hr class="border-light container-m--x mt-0 mb-4">
 
 @endsection
