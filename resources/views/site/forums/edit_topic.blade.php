4b07b4fd01413704d5cc77f73406210ea285aa3b
John <juaorok@hotmail.com>
2019-11-23 15:38:04 -0300

    Create edit_topic.blade.php

---
+++ resources/views/site/forums/edit_topic.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.dashboard')
+
+@section('subtitle', 'Forum')
+
+@section('css')
+    <!-- sceditor -->
+    <link rel="stylesheet" href="{{ asset('vendor/sceditor/minified/themes/default.min.css') }}">
+@endsection
+
+@section('content')
+
+    <div class="font-weight-bold py-3 h4">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="{{ url('home') }}">Home</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="{{ url('forum') }}">Fórum</a>
+            </li>
+            <li class="breadcrumb-item active">Editar Tópico</li>
+        </ol>
+    </div>
+
+    <hr class="container-m-nx border-light mt-0 mb-5">
+
+    <div class="card mb-4">
+        <h6 class="card-header">Editar Tópico</h6>
+        <div class="card-body">
+
+            @includeIf('errors.errors', [$errors])
+            <!-- New Topic -->
+            {!! Form::open(['route' => ['new.topic', 'id' => $forum->id, 'slug' => $forum->slug], 'class' => 'form-horizontal']) !!}
+
+            <div class="form-group">
+                {!! Form::label('name', 'Título do tópico: *', ['class' => 'form-label']) !!}
+                {!! Form::text('name', null, ['class' => 'form-control', 'required', 'maxlength' => 250]) !!}
+            </div>
+
+            {!! Form::submit('Editar', ['class' => 'btn btn-primary btn-rounded btn-outline-primary']) !!}
+            <br>
+            {!! Form::close() !!}
+            <!-- END New Topic -->
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+    <!-- sceditor -->
+    <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/minified/formats/bbcode.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/languages/pt-BR.js') }}"></script>
+
+    <!-- script -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            var textarea = document.getElementById('content');
+            sceditor.create(textarea, {
+                format: 'bbcode',
+                locale: 'pt-BR',
+                emoticonsRoot: '/vendor/sceditor/',
+                style: '/vendor/sceditor/minified/themes/content/default.min.css'
+            });
+        });
+    </script>
+@endsection
