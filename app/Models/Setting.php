fc7fea262effb901da6f663222a895dac584f611
John <juaorok@hotmail.com>
2019-11-23 16:55:56 -0300

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Events\SettingSaved;
 use App\Helpers\BBCode;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Setting extends Model
 {
@@ -11,7 +13,9 @@ class Setting extends Model
 
     protected $table = 'settings';
 
-    protected $bbcode;
+    protected $dispatchesEvents = [
+        'saved' => SettingSaved::class
+    ];
 
     protected $fillable = [
         'display_name',
@@ -24,9 +28,77 @@ class Setting extends Model
         return (new BBCode())->parse($this->value, true);
     }
 
-    public function setting(string $value)
+    /**
+     * Get all settings with cache support.
+     * @return mixed
+     */
+    public static function allCached()
     {
-        //TODO: fix key => value
-        return self::select($value)->where('id', '=', 1)->pluck($value)->first();
+        return Cache::tags(['settings'])->rememberForever('settings.all:pt-br', function () {
+            return self::all()->mapWithKeys(function ($setting) {
+                return [$setting->key => $setting->value];
+            });
+        });
+    }
+
+    /**
+     * Determine if the given setting key exists.
+     * @param $key
+     * @return bool
+     */
+    public static function has($key)
+    {
+        return static::where('key', $key)->exists();
+    }
+
+    /**
+     * Get setting for the given key.
+     * @param string $key
+     * @param mixed $default
+     * @return string|array
+     */
+    public static function get($key, $default = null)
+    {
+        return static::where('key', $key)->first()->value ?? $default;
+    }
+
+    /**
+     * Set the given setting.
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public static function set($key, $value)
+    {
+        static::updateOrCreate(['key' => $key], ['value' => $value]);
+    }
+
+    /**
+     * Set the given settings.
+     * @param $settings
+     */
+    public static function setMany($settings)
+    {
+        foreach ($settings as $key => $value) {
+            self::set($key, $value);
+        }
+    }
+
+    /**
+     * Get the value of the setting.
+     * @return mixed
+     */
+    public function getValueAttribute()
+    {
+        return unserialize($this->value);
+    }
+
+    /**
+     * Set the value of the setting.
+     * @param $value
+     */
+    public function setPlainValueAttribute($value)
+    {
+        $this->attributes['value'] = serialize($value);
     }
 }
