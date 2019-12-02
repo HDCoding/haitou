006f4db1a23c8a0c7397e66f8a080bd19bdf2f4f
John <juaorok@hotmail.com>
2019-12-02 14:30:34 -0300

    Create MediasController.php

---
+++ app/Http/Controllers/Site/MediasController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Site\RatingRequest;
+use App\Models\Bookmark;
+use App\Models\Media;
+use App\Models\Rating;
+use Illuminate\Http\Request;
+
+class MediasController extends Controller
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->middleware('auth');
+        $this->request = $request;
+    }
+
+    public function show($media_id, $slug)
+    {
+        $media = Media::where('id', '=', $media_id)->whereSlug($slug)->firstOrFail();
+        $media->increment('views'); //increment views
+
+        $user = $this->request->user(); //logged user
+
+        $comments = $media->comments()->latest()->paginate(5);
+
+        if (request()->ajax()) {
+            return view('includes.comments', compact('comments'));
+        }
+
+        $voted = $media->rating()->where('user_id', '=', $user->id)->first();
+        $bookmarked = $media->bookmarks()->where('user_id', '=', $user->id)->first();
+
+        return view('site.medias.media', compact('media', 'comments', 'voted', 'bookmarked'));
+    }
+
+    public function vote(RatingRequest $request, $media_id)
+    {
+        $media = Media::findOrFail($media_id);
+
+        $user = $request->user();
+
+        $vote = $request->input('vote');
+
+        $voted = $media->rating()->where('user_id', '=', $user->id)->first();
+
+        if ($voted) {
+            $voted->vote = $vote;
+            $voted->update();
+        } else {
+            $newRating = new Rating();
+            $newRating->media_id = $media->id;
+            $newRating->user_id = $user->id;
+            $newRating->vote = $vote;
+            $newRating->save();
+        }
+        if ($vote == 0 && !$voted) {
+            return redirect()->route('media.show', [$media->id, $media->slug]);
+        }
+
+        return redirect()->route('media.show', [$media->id, $media->slug]);
+    }
+}
