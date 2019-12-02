b971cbdc89a4f2ac48861a4f58fbac1f1dba69c3
John <juaorok@hotmail.com>
2019-12-02 14:30:46 -0300

    Create SearchesController.php

---
+++ app/Http/Controllers/Site/SearchesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class SearchesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function search(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+
+            $search = $request->input('search');
+
+            $users = User::with('group:id,name')
+                ->where('username', 'like', '%'.$search.'%')
+                ->select('id', 'username', 'slug', 'avatar')
+                ->where('status', '!=', 0)
+                ->paginate(30);
+
+            return view('site.searches.search', compact('users'));
+
+        } else {
+
+            toastr()->error('Erro na pesquisa', 'Erro');
+            return redirect()->to('home');
+        }
+    }
+}
