c1a13c7be869f0369773e7ae4b3450cad6bafd6a
John <juaorok@hotmail.com>
2019-12-02 14:43:53 -0300

    Create form.blade.php

---
+++ resources/views/staff/studios/form.blade.php
@@ -0,0 +1,47 @@
+@section('css')
+    <!-- sceditor -->
+    <link href="{{ asset('vendor/sceditor/minified/themes/default.min.css') }}" rel="stylesheet" />
+@endsection
+
+<div class="form-group">
+    {!! Form::label('name', 'Nome *') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('logo', 'Logo') !!}
+    {!! Form::text('logo', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('website', 'Website') !!}
+    {!! Form::text('website', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+    {!! Form::label('description', 'Descrição') !!}
+    {!! Form::textarea('description', null, ['class' => 'form-control note', 'rows' => 8]) !!}
+</div>
+
+{!! Form::submit($submitButton, ['class' => 'btn btn-primary btn-rounded btn-outline']) !!}
+<br>
+
+@section('script')
+    <!-- sceditor -->
+    <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/minified/formats/bbcode.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/languages/pt-BR.js') }}"></script>
+
+    <!-- sceditor -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            let textarea = document.getElementById('description');
+            sceditor.create(textarea, {
+                format: 'bbcode',
+                locale: 'pt-BR',
+                emoticonsRoot: '/vendor/sceditor/',
+                style: '/vendor/sceditor/minified/themes/content/default.min.css'
+            });
+        });
+    </script>
+@endsection
