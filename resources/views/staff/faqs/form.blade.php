76dd931fdc86b59fcef879c7e6a47e128f9523a9
John <juaorok@hotmail.com>
2019-12-02 14:43:01 -0300

    Create form.blade.php

---
+++ resources/views/staff/faqs/form.blade.php
@@ -0,0 +1,39 @@
+@section('css')
+    <!-- sceditor -->
+    <link href="{{ asset('vendor/sceditor/minified/themes/default.min.css') }}" rel="stylesheet">
+@endsection
+
+<div class="form-group">
+    {!! Form::label('question', 'Pergunta:') !!}
+    {!! Form::text('question', null, ['class' => 'form-control', 'maxlength' => 250]) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('category_id', 'Categoria:') !!}
+    {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+</div>
+<div class="form-group">
+    {!! Form::label('answer', 'Resposta:') !!}
+    {!! Form::textarea('answer', null, ['class' => 'form-control', 'rows' => 8]) !!}
+</div>
+{!! Form::submit($submitButton, ['class' => 'btn btn-primary btn-rounded btn-outline-primary']) !!}
+<br>
+
+@section('scripts')
+    <!-- sceditor -->
+    <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/minified/formats/bbcode.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/languages/pt-BR.js') }}"></script>
+
+    <!-- sceditor -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function() {
+            let textarea = document.getElementById('answer');
+            sceditor.create(textarea, {
+                format: 'bbcode',
+                locale: 'pt-BR',
+                emoticonsRoot: '/vendor/sceditor/',
+                style: '/vendor/sceditor/minified/themes/content/default.min.css'
+            });
+        });
+    </script>
+@endsection
