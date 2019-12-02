6fd4d6742366326962a736752bb74a6946806869
John <juaorok@hotmail.com>
2019-12-02 14:43:27 -0300

    Create create.blade.php

---
+++ resources/views/staff/news/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Adicionar')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Staff Painel</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('staff') }}">@lang('dashboard.title')</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('staff/news') }}">@lang('dashboard.news')</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Adicionar</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Adicionar</h4>
+                        @includeIf('errors.errors', [$errors])
+                        {!! Form::open(['url' => 'staff/news', 'class' => 'form-horizontal']) !!}
+                        @include('staff.news.form', ['submitButton' => 'Adicionar'])
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
