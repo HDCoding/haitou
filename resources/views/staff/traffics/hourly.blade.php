9a4b41dea8cef4c270074112f191d1bfcc570139
John <juaorok@hotmail.com>
2019-12-02 14:43:59 -0300

    Create hourly.blade.php

---
+++ resources/views/staff/traffics/hourly.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Hora')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Staff Painel</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('staff') }}">@lang('dashboard.title')</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('staff/traffics') }}">Tráficos</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Hora</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Hora</h4>
+                        <div class="table-responsive m-t-40">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>Hora</th>
+                                    <th>Recebido</th>
+                                    <th>Enviado</th>
+                                    <th>Total</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($hourly as $key => $value)
+                                    <tr>
+                                        <td>{{ $value['label'] }}</td>
+                                        <td>{{ $value['rx'] }}</td>
+                                        <td>{{ $value['tx'] }}</td>
+                                        <td>{{ $value['total'] }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
