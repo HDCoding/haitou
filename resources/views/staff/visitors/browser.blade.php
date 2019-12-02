447b466fa89e44a99786fce6e84b9b8734516742
John <juaorok@hotmail.com>
2019-12-02 14:44:12 -0300

    Create browser.blade.php

---
+++ resources/views/staff/visitors/browser.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Browser')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Staff Painel</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('staff') }}">@lang('dashboard.title')</a></li>
+                            <li class="breadcrumb-item"><a href="#">@lang('dashboard.visitors')</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Browser</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card block.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
