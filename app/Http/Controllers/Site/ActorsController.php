6084f5592d5e440388388690c6924763fbd5e9c1
John <juaorok@hotmail.com>
2019-12-02 14:29:57 -0300

    Create ActorsController.php

---
+++ app/Http/Controllers/Site/ActorsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use App\Models\Actor;
+use App\Models\Bookmark;
+use Illuminate\Http\Request;
+
+class ActorsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function show($actor_id, $slug, Request $request)
+    {
+        $actor = Actor::where('id', '=', $actor_id)->whereSlug($slug)->firstOrFail();
+        $actor->increment('views');
+
+        $user = $request->user();
+        $bookmarked = $user->bookmarks()->where('actor_id', '=', $actor->id)->first();
+
+        return view('site.actors.actor', compact('actor', 'bookmarked'));
+    }
+}
