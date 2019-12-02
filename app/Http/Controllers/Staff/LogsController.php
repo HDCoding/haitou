7bd17cb3d5d9d9b95ebb12b3911786930d27c85b
John <juaorok@hotmail.com>
2019-12-02 14:31:38 -0300

    Create LogsController.php

---
+++ app/Http/Controllers/Staff/LogsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogsController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('staff.logs.index');
+    }
+}
