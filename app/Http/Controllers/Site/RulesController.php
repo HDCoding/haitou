bd5f84eecfc8233dd5f7df5399ffa669489d1c83
John <juaorok@hotmail.com>
2019-12-02 14:30:44 -0300

    Create RulesController.php

---
+++ app/Http/Controllers/Site/RulesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Achievements\UserReadRules;
+use App\Http\Controllers\Controller;
+use App\Models\Rule;
+use Illuminate\Http\Request;
+
+class RulesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request)
+    {
+        $rules = Rule::select('id', 'name', 'description')->get();
+
+        $user = $request->user();
+
+        if ($user->read_rules == false) {
+            $user->unlock(new UserReadRules());
+            $user->readed_rules = true;
+            $user->save();
+        }
+        return view('site.rules.index', compact('rules'));
+    }
+}
