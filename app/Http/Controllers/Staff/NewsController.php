e52a9a371fefdb069fdf03fc83547a5307960930
John <juaorok@hotmail.com>
2019-12-02 14:31:48 -0300

    Create NewsController.php

---
+++ app/Http/Controllers/Staff/NewsController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Staff;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Staff\NewsRequest;
+use App\Models\News;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function __construct()
+    {
+//        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $news = News::with('user:id,username')
+            ->select('id', 'user_id', 'name', 'views')
+            ->orderBy('id', 'DESC')->get();
+        return view('staff.news.index', compact('news'));
+    }
+
+    public function create()
+    {
+        return view('staff.news.create');
+    }
+
+    public function store(NewsRequest $request)
+    {
+        $new = new News($request->except('_token'));
+        $new->staff_id = auth()->user()->id;
+        $new->save();
+        toastr()->success('News cadastrada.', 'Sucesso');
+        return redirect()->to('staff/news');
+    }
+
+    public function edit($news_id)
+    {
+        $new = News::findOrFail($news_id);
+        return view('staff.news.edit', compact('new'));
+    }
+
+    public function update(NewsRequest $request, $news_id)
+    {
+        $new = News::findOrFail($news_id);
+        $new->staff_id = auth()->user()->id;
+        $new->update($request->except('_token'));
+        toastr()->info('News atualizada.', 'Sucesso');
+        return redirect()->to('staff/news');
+    }
+
+    public function destroy($news_id)
+    {
+        News::findOrFail($news_id)->delete();
+        toastr()->warning('News deletada.', 'Aviso');
+        return redirect()->to('staff/news');
+    }
+}
