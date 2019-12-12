3b30b5950114062688f73ff5cf8a5810f2adfa6a
John <juaorok@hotmail.com>
2019-12-12 08:23:25 -0300

    Update application.blade.php

--- resources/views/layouts/application.blade.php
+++ resources/views/layouts/application.blade.php
@@ -2,46 +2,36 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-    <title>Site :: @yield('title')</title>
-
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicons/favicon.ico') }}">
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900" rel="stylesheet">
-
-    <!-- Core stylesheets -->
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" class="theme-settings-bootstrap-css">
-    <link rel="stylesheet" href="{{ asset('css/demo.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/appwork.css') }}" class="theme-settings-appwork-css">
-    <link rel="stylesheet" href="{{ asset('css/theme-corporate.css') }}" class="theme-settings-theme-css">
-    <link rel="stylesheet" href="{{ asset('css/colors.css') }}" class="theme-settings-colors-css">
-    <link rel="stylesheet" href="{{ asset('css/uikit.css') }}">
-
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon.ico') }}">
+    <title>Site :: @yield('subtitle')</title>
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/style.min.css') }}" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
     @yield('styles')
-
 </head>
 
 <body>
-<div class="page-loader">
-    <div class="bg-primary"></div>
-</div>
 
-<!-- Content -->
 @yield('layout-content')
-<!-- / Content -->
 
-<!-- Core scripts -->
-<script src="{{ asset('vendor/jquery/jquery-3.4.1.min.js') }}"></script>
-<script src="{{ asset('vendor/popper/popper.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.js') }}"></script>
+<script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{ asset('vendor/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
-@yield('scripts')
+@yield('script')
 
 </body>
 </html>
