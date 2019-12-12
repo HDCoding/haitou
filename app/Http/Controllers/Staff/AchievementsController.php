744e2bd93bdcd0280c329a714ee77cac47c428a2
John <juaorok@hotmail.com>
2019-12-12 08:22:06 -0300

    Update AchievementsController.php

--- app/Http/Controllers/Staff/AchievementsController.php
+++ app/Http/Controllers/Staff/AchievementsController.php
@@ -10,7 +10,7 @@ class AchievementsController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
