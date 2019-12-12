4e67d4cc6e563044cdc3646526708082dbe29ea0
John <juaorok@hotmail.com>
2019-12-12 08:22:43 -0300

    Update TrafficsController.php

--- app/Http/Controllers/Staff/TrafficsController.php
+++ app/Http/Controllers/Staff/TrafficsController.php
@@ -12,7 +12,7 @@ class TrafficsController extends Controller
 
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
         $this->vnstat = new NetwordInformation();
     }
 
