54d4f6f899659defd5d5e1e10a12020ebbe37c8c
John <juaorok@hotmail.com>
2019-12-12 08:22:12 -0300

    Update CharactersController.php

--- app/Http/Controllers/Staff/CharactersController.php
+++ app/Http/Controllers/Staff/CharactersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Staff;
 
+use App\Helpers\ImageUploader;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Staff\CharactersRequest;
 use App\Models\Character;
@@ -9,9 +10,12 @@ use Illuminate\Http\Request;
 
 class CharactersController extends Controller
 {
+    protected $imageFile;
+
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
+        $this->imageFile = new ImageUploader();
     }
 
     public function index()
@@ -27,8 +31,20 @@ class CharactersController extends Controller
 
     public function store(CharactersRequest $request)
     {
-        $character = new Character($request->except('_token'));
+        $character = new Character();
+        $character->name = $request->input('name');
+        //Image
+        $img = $request->file('image');
+        $filename = md5_gen() . '.' . $img->getClientOriginalExtension(); //recebe nome aleatório e a extensão do arquivo
+        //End Image
+
+        $character->image = $filename;
+        $character->description = $request->input('description');
         $character->save();
+
+        //upload da imagem
+        $this->imageFile->uploadImage(Character::uploaderFolder, $filename, $img);
+
         toastr()->success('Novo personagem cadastrada.', 'Sucesso');
         return redirect()->to('staff/characters');
     }
@@ -41,14 +57,36 @@ class CharactersController extends Controller
 
     public function update(CharactersRequest $request, $character_id)
     {
-        Character::findOrFail($character_id)->update($request->except('_token'));
+        $character = Character::findOrFail($character_id);
+        $character->name = $request->input('name');
+        //Image
+        $img = $request->file('image');
+        if ($img != null) {
+            $this->imageFile->removeImage(Character::uploaderFolder, $character->image); //remove a imagem antiga
+            $filename = md5_gen() . '.' . $img->getClientOriginalExtension(); //recebe nome aleatório e a extensão do arquivo
+        } else {
+            $filename = $character->image; //se o usuario nao atualizar a imagem o nome e extensão continua igual
+        }
+        //End Image
+
+        $character->image = $filename;
+        $character->description = $request->input('description');
+        $character->update();
+
+        if ($img != null) {
+            $this->imageFile->uploadImage(Character::uploaderFolder, $filename, $img); //upload da imagem
+        }
+
         toastr()->info('Personagem atualizado.', 'Sucesso');
         return redirect()->to('staff/characters');
     }
 
     public function destroy($character_id)
     {
-        Character::findOrFail($character_id)->delete();
+        $character = Character::findOrFail($character_id);
+        //$this->imageFile->removeImage(Character::uploaderFolder, $character->image);
+        $character->delete();
+
         toastr()->warning('Personagem deletado.', 'Aviso');
         return redirect()->to('staff/characters');
     }
