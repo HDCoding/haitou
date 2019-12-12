02e1591d3b203b519ffb6824904fc414d6771448
John <juaorok@hotmail.com>
2019-12-12 08:23:12 -0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,82 +1,86 @@
-@extends('layouts.auth')
+@extends('layouts.application')
 
-@section('title', 'Login')
+@section('subtitle', 'Login')
 
-@section('styles')
-    <!-- Page -->
-    <link href="{{ asset('css/pages/authentication.css') }}" rel="stylesheet">
-@endsection
-
-@section('content')
+@section('layout-content')
 
     <!-- Content -->
+    <div class="main-wrapper">
 
-    <div class="authentication-wrapper authentication-2 ui-bg-cover ui-bg-overlay-container px-4" style="background-image: url('{{ asset('images/login-register.jpg') }}');">
-        <div class="ui-bg-overlay bg-dark opacity-25"></div>
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
 
-        <div class="authentication-inner py-5">
-
-            <div class="card">
-                <div class="p-4 p-sm-5">
-                    <!-- Logo -->
-                    <div class="d-flex justify-content-center align-items-center pb-2 mb-4">
-                        <div class="ui-w-60">
-                            <div class="w-100 position-relative">
-                                <img src="{{ asset('images/favicons/apple-icon-60x60.png') }}" alt="Logo">
-                            </div>
-                        </div>
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url({{ asset('images/login-register.jpg') }}) no-repeat center center;">
+            <div class="auth-box">
+                <div id="loginform">
+                    <div class="logo">
+                        <span class="db"><img src="{{ asset('images/favicons/apple-icon-60x60.png') }}" alt="logo" /></span>
+                        <h5 class="font-medium m-b-20 m-t-20">Login</h5>
                     </div>
-                    <!-- / Logo -->
-
-                    <h5 class="text-center text-muted font-weight-normal mb-4">Faça login na sua conta</h5>
-                    @includeIf('errors.errors', [$errors])
-                    @include('includes.messages')
                     <!-- Form -->
-                    <form class="js-validation-login form-horizontal" method="POST" action="{{ url('login') }}">
-                        @csrf
-                        <div class="form-group">
-                            <label for="email" class="form-label">E-mail</label>
-                            <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" autocomplete="off" id="email" maxlength="70" name="email" placeholder="E-mail" required value="{{ old('email') }}">
-                            @if($errors->has('email'))
-                                <span class="invalid-feedback">Este campo é obrigatório</span>
-                            @endif
+                    <div class="row">
+                        <div class="col-12">
+                            @includeIf('errors.errors', [$errors])
+                            @include('includes.messages')
+                            <form class="js-validation-login form-horizontal m-t-20" method="POST" action="{{ url('login') }}">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }} form-control-lg" autocomplete="off" id="email" maxlength="70" name="email" placeholder="E-mail" required value="{{ old('email') }}">
+                                    @if($errors->has('email'))
+                                        <span class="invalid-feedback">Este campo é obrigatório</span>
+                                    @endif
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }} form-control-lg" autocomplete="off" id="password" minlength="6" maxlength="16" name="password" placeholder="Senha" required>
+                                    @if($errors->has('password'))
+                                        <span class="invalid-feedback">Este campo é obrigatório</span>
+                                    @endif
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-12">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name="remember" {{ old('remember') ? 'checked' : '' }} id="remember">
+                                            <label class="custom-control-label" for="remember">Lembrar-me</label>
+                                            <a href="{{ route('password.request') }}" id="to-recover" class="text-dark float-right"><i class="fa fa-lock m-r-5"></i> Esqueceu a senha?</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group text-center">
+                                    <div class="col-xs-12 p-b-20">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Entrar</button>
+                                    </div>
+                                </div>
+                                <div class="form-group m-b-0 m-t-10">
+                                    <div class="col-sm-12 text-center">
+                                        Não tem uma conta ainda? <a class="text-info m-l-5" href="{{ url('register') }}">Registre-se</a>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div class="form-group">
-                            <label for="password" class="form-label d-flex justify-content-between align-items-end">
-                                Senha
-                                <a href="javascript:void(0)" class="d-block small">Esqueceu a senha?</a>
-                            </label>
-                            <input type="password" class="form-control {{ $errors->has('password') ? 'is-invalid' : '' }}" autocomplete="off" id="password" minlength="6" maxlength="16" name="password" placeholder="Senha" required>
-                            @if($errors->has('password'))
-                                <span class="invalid-feedback">Este campo é obrigatório</span>
-                            @endif
-                        </div>
-                        <div class="d-flex justify-content-between align-items-center m-0">
-                            <label class="custom-control custom-checkbox m-0">
-                                <input type="checkbox" class="custom-control-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <span class="custom-control-label">Lembrar-me</span>
-                            </label>
-                            <button type="submit" class="btn btn-primary">Entrar</button>
-                        </div>
-                    </form>
-                    <!-- / Form -->
-
-                </div>
-                <div class="card-footer py-3 px-4 px-sm-5">
-                    <div class="text-center text-muted">
-                        Não tem uma conta ainda? <a href="{{ url('register') }}">Registre-se</a>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
-
     <!-- / Content -->
 
 @endsection
 
-@section('scripts')
+@section('script')
     <script src="{{ asset('vendor/validate/validate.js') }}"></script>
     <script src="{{ asset('js/pages/login.js') }}"></script>
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $(".preloader").fadeOut();
+    </script>
 @endsection
