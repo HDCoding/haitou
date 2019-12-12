77da05ce6b75994a9aac7e5a6426a710df3b2b04
John <juaorok@hotmail.com>
2019-12-12 08:22:27 -0300

    Update LogsController.php

--- app/Http/Controllers/Staff/LogsController.php
+++ app/Http/Controllers/Staff/LogsController.php
@@ -9,7 +9,7 @@ class LogsController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
