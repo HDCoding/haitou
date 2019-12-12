bc88c863b8b95f473e871d081357eb8eef65b584
John <juaorok@hotmail.com>
2019-12-12 08:26:02 -0300

    Update index.blade.php

--- resources/views/staff/reports/index.blade.php
+++ resources/views/staff/reports/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.reports'))
 
+@section('css')
+    <!-- DataTables -->
+    <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -26,8 +31,8 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.reports')</h4>
-                        <div class="table-responsive m-t-40">
-                            <table class="table">
+                        <div class="table-responsive m-t-15">
+                            <table class="table" id="datatable">
                                 <thead>
                                 <tr>
                                     <th class="text-center">#</th>
@@ -37,26 +42,38 @@
                                 </tr>
                                 </thead>
                                 <tbody>
-                                @forelse($reports as $report)
+                                @foreach($reports as $report)
                                     <tr>
                                         <td class="text-center">{{ $report->id }}</td>
                                         <td>{{ link_to_route('reports.show', $report->name, ['id' => $report->id]) }}</td>
                                         <td>{!! $report->type() !!}</td>
                                         <td class="hidden-xs">{!! $report->solved() !!}</td>
                                     </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4" class="text-center">Nenhum registro no momento.</td>
-                                    </tr>
-                                @endforelse
+                                @endforeach
                                 </tbody>
                             </table>
                         </div>
-                        {{ $reports->links() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            $('#datatable').DataTable({
+                "displayLength": 50,
+                "searching": true,
+                "responsive": true,
+                "order": [[ 1, "asc" ]],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                }
+            });
+        });
+    </script>
 @endsection
