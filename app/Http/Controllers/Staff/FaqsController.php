0004ad397f48934f65e3e047cc769a0f1d4b29ae
John <juaorok@hotmail.com>
2019-12-12 09:36:50 -0300

    Update FaqsController.php

--- app/Http/Controllers/Staff/FaqsController.php
+++ app/Http/Controllers/Staff/FaqsController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Staff\FaqsRequest;
 use App\Models\Category;
 use App\Models\Faq;
-use Illuminate\Http\Request;
 
 class FaqsController extends Controller
 {
