a30235c33b5ea489da51898c11a5ff7fe42e67fc
John <juaorok@hotmail.com>
2019-12-12 08:22:18 -0300

    Update FansubsController.php

--- app/Http/Controllers/Staff/FansubsController.php
+++ app/Http/Controllers/Staff/FansubsController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Staff;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Staff\FansubsRequest;
 use App\Models\Fansub;
+use App\Models\FansubUser;
+use App\User;
 use Illuminate\Http\Request;
 
 class FansubsController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
@@ -51,5 +53,33 @@ class FansubsController extends Controller
         Fansub::findOrFail($fansub_id)->delete();
         toastr()->warning('Fansub deletado.', 'Sucesso');
         return redirect()->to('staff/fansubs');
+    }
+
+    public function members($fansub_id)
+    {
+        $fansub = Fansub::findOrFail($fansub_id);
+        $members = FansubUser::with('user:id,username')->where('fansub_id', '=', $fansub_id)->get();
+        $users = User::select(['id', 'username'])->pluck('username', 'id');
+        return view('staff.fansubs.members', compact('fansub', 'members', 'users'));
+    }
+
+    public function addMembers(Request $request)
+    {
+        $fansub_member = new FansubUser();
+        $fansub_id = $request->input('fansub_id');
+        $fansub_member->fansub_id = $fansub_id;
+        $fansub_member->user_id = $request->input('user_id');
+        //$fansub_member->username = $request->input('username');
+        $fansub_member->job = $request->input('job');
+        $fansub_member->is_admin = $request->input('is_admin') ? true : false;
+        $fansub_member->save();
+        return redirect()->to('staff/fansub/' . $fansub_id . '/members');
+    }
+
+    public function delMembers($member_id)
+    {
+        FansubUser::findOrFail($member_id)->delete();
+        toastr()->warning('Membro deletado do fansub.', 'Aviso');
+        return redirect()->back();
     }
 }
