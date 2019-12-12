0a8ffdc3268efe48d8258485ab9d6cd1a20cf515
John <juaorok@hotmail.com>
2019-12-12 08:22:17 -0300

    Update FailedLoginsController.php

--- app/Http/Controllers/Staff/FailedLoginsController.php
+++ app/Http/Controllers/Staff/FailedLoginsController.php
@@ -10,7 +10,7 @@ class FailedLoginsController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
