4c9c677d82f5c813e1c4854740a978abb74de275
John <juaorok@hotmail.com>
2019-12-12 08:24:34 -0300

    Update index.blade.php

--- resources/views/staff/characters/index.blade.php
+++ resources/views/staff/characters/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.characters'))
 
+@section('css')
+    <!-- DataTables -->
+    <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -31,30 +36,28 @@
                                 <span class="ion ion-md-add"></span> Adicionar
                             </button>
                         </a>
-                        <div class="table-responsive m-t-40">
-                            <table class="table">
+                        <div class="table-responsive m-t-15">
+                            <table class="table" id="datatable">
                                 <thead>
                                 <tr>
-                                    <th class="text-center"><i class="fas fa-user"></i></th>
+                                    <th><i class="fas fa-user"></i></th>
                                     <th>Nome</th>
                                     <th>Views</th>
-                                    <th class="text-center">Opções</th>
+                                    <th>Opções</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 @foreach($characters as $character)
                                     <tr>
-                                        <td class="text-center">
-                                            <img class="" src="{{ $character->image }}" alt="{{ $character->name }}" width="70px">
-                                        </td>
-                                        <td class="">{{ $character->name }}</td>
+                                        <th>
+                                            <img class="" src="{{ asset('characters/'.$character->image) }}" alt="{{ $character->name }}" width="70px">
+                                        </th>
+                                        <td>{{ $character->name }}</td>
+                                        <td><span class="badge badge-info">{{ $character->views }}</span></td>
                                         <td>
-                                            <span class="badge badge-info">{{ $character->views }}</span>
-                                        </td>
-                                        <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/characters/' . $character->id . '/edit') }}" class="btn btn-xs" type="button" data-toggle="tooltip" title="Editar Personagem"><i class="fas fa-pencil-alt text-info"></i> Editar</a>
-                                                <a href="javascript:;" onclick="document.getElementById('character-del-{{ $character->id }}').submit();" class="btn btn-xs" type="button" data-toggle="tooltip" title="Remover Personagem"><i class="fa fa-times text-danger"></i> Deletar</a>
+                                                <a href="{{ url('staff/characters/' . $character->id . '/edit') }}" data-toggle="tooltip" title="Editar Personagem"><i class="fas fa-pencil-alt text-info"></i> Editar</a>
+                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('character-del-{{ $character->id }}').submit();" data-toggle="tooltip" title="Remover Personagem"><i class="fa fa-times text-danger"></i> Deletar</a>
                                                 {!! Form::open(['url' => 'staff/characters/' . $character->id, 'method' => 'DELETE', 'id' => 'character-del-' . $character->id , 'style' => 'display: none']) !!}
                                                 {!! Form::close() !!}
                                             </div>
@@ -70,4 +73,21 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            $('#datatable').DataTable({
+                "displayLength": 50,
+                "searching": true,
+                "responsive": true,
+                "order": [[ 1, "asc" ]],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                }
+            });
+        });
+    </script>
 @endsection
