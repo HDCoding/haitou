0cfc7e0565763112cf2730f70065585ecb6a0b42
John <juaorok@hotmail.com>
2019-12-12 08:22:28 -0300

    Update LotteriesController.php

--- app/Http/Controllers/Staff/LotteriesController.php
+++ app/Http/Controllers/Staff/LotteriesController.php
@@ -9,7 +9,7 @@ class LotteriesController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
