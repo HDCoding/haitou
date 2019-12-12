a9d67d4a1d7024ea110ec69d04f2521c42de1e08
John <juaorok@hotmail.com>
2019-12-12 08:22:37 -0300

    Update StaffController.php

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -12,20 +12,15 @@ class StaffController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
     {
-        $pendent = User::where('status', '=', 0)->count();
-        $activated = User::where('status', '=', 1)->count();
-        $suspended = User::where('status', '=', 2)->count();
-        $banned = User::where('status', '=', 3)->count();
-
         $system = new SystemInformation();
 
         $certificate = SslCertificate::createForHostName('spatie.be');//config('app.url')
 
-        return view('staff.staff.index', compact('pendent', 'activated', 'suspended', 'banned', 'system', 'certificate'));
+        return view('staff.staff.index', compact('system', 'certificate'));
     }
 }
