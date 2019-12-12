9674464222927b897f93940f9ea3b095bb13728f
John <juaorok@hotmail.com>
2019-12-12 08:23:44 -0300

    Create index.blade.php

---
+++ resources/views/site/home/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Home')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Home</h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card block.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
