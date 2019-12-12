a4ed6ac089e14cd77111cb5fe24c957357aebed2
John <juaorok@hotmail.com>
2019-12-12 08:22:29 -0300

    Update MediasController.php

--- app/Http/Controllers/Staff/MediasController.php
+++ app/Http/Controllers/Staff/MediasController.php
@@ -17,7 +17,7 @@ class MediasController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
