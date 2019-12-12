1bddf7f2aee52064ee1aef83928b8cc5df2dc2bc
John <juaorok@hotmail.com>
2019-12-12 08:25:13 -0300

    Update index.blade.php

--- resources/views/staff/groups/index.blade.php
+++ resources/views/staff/groups/index.blade.php
@@ -26,18 +26,17 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.groups')</h4>
-                        <a href="{{ url('staff/roles/create') }}">
+                        <a href="{{ url('staff/groups/create') }}">
                             <button type="button" class="btn btn-xs btn-outline-primary">
                                 <span class="ion ion-md-add"></span> Adicionar
                             </button>
                         </a>
-                        <div class="table-responsive m-t-40">
+                        <div class="table-responsive m-t-15">
                             <table class="table">
                                 <thead>
                                 <tr>
-                                    <th class="text-center"><i class="fas fa-user"></i></th>
+                                    <th><i class="fas fa-user"></i></th>
                                     <th>Nome</th>
-                                    <th>Descrição</th>
                                     <th>Cor</th>
                                     <th>Contas</th>
                                     <th>H-N-R</th>
@@ -47,18 +46,17 @@
                                 <tbody>
                                 @foreach($groups as $group)
                                     <tr>
-                                        <td class="text-center">{{ $group->icon }}</td>
+                                        <td>{{ $group->icon }}</td>
                                         <td>{{ $group->name }}</td>
-                                        <td>{{ $group->description }}</td>
-                                        <td class="text-center">{{ $group->color }}</td>
-                                        <td class="text-center">{{ $group->users()->count() }}</td>
-                                        <td class="text-center">{{ $group->hnr / 3600 }} Horas</td>
+                                        <td style="color:{{ $group->color }}"><strong>{{ $group->color }}</strong></td>
+                                        <td>{{ $group->users()->count() }}</td>
+                                        <td>{{ ($group->hnr / 3600) }} Horas</td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/roles/' . $group->id . '/edit') }}" class="btn btn-xs" type="button" data-toggle="tooltip" title="Editar Grupo"><i class="fas fa-pencil-alt text-info"></i></a>
+                                                <a href="{{ url('staff/groups/' . $group->id . '/edit') }}" data-toggle="tooltip" title="Editar Grupo"><i class="fas fa-pencil-alt text-info"></i></a>
 {{--                                                @if(auth()->user()->permission->full_access)--}}
-                                                    <a href="javascript:;" onclick="document.getElementById('role-del-{{$group->id}}').submit();" class="btn btn-xs" type="button" data-toggle="tooltip" title="Remover Grupo"><i class="fa fa-times text-danger"></i></a>
-                                                    {!! Form::open(['url' => 'staff/roles/' . $group->id, 'method' => 'DELETE', 'id' => 'role-del-' . $group->id , 'style' => 'display: none']) !!}
+                                                    <a class="m-l-15" href="javascript:;" onclick="document.getElementById('role-del-{{$group->id}}').submit();" data-toggle="tooltip" title="Remover Grupo"><i class="fa fa-times text-danger"></i></a>
+                                                    {!! Form::open(['url' => 'staff/groups/' . $group->id, 'method' => 'DELETE', 'id' => 'role-del-' . $group->id , 'style' => 'display: none']) !!}
                                                     {!! Form::close() !!}
 {{--                                                @endif--}}
                                             </div>
