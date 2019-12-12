517a5c79cd4f291444547c246ce871cbb3635463
John <juaorok@hotmail.com>
2019-12-12 08:26:22 -0300

    Update index.blade.php

--- resources/views/staff/torrents/index.blade.php
+++ resources/views/staff/torrents/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.torrents'))
 
+@section('css')
+    <!-- DataTables -->
+    <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -26,8 +31,8 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.torrents')</h4>
-                        <div class="table-responsive m-t-40">
-                            <table class="table">
+                        <div class="table-responsive m-t-15">
+                            <table class="table" id="datatable">
                                 <thead>
                                 <tr>
                                     <th class="text-center">#</th>
@@ -44,51 +49,34 @@
                                         <td class="text-center">{{ $torrent->id }}</td>
                                         <td class="">{{ $torrent->category->name }}</td>
                                         <td class="">{{ $torrent->name }}</td>
-                                        <td class="">{{ $torrent->getSize() }}</td>
+                                        <td class="">{{ $torrent->size() }}</td>
                                         <td>
                                             <span class="badge badge-info">{{ $torrent->views }}</span>
                                         </td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <button class="btn" type="button">Opções</button>
-                                                <div class="btn-group">
-                                                    <button class="btn dropdown-toggle" data-toggle="dropdown"
-                                                            aria-expanded="false">
-                                                        <span class="caret"></span>
-                                                    </button>
-                                                    <ul class="dropdown-menu">
-                                                        <li class="dropdown-header">Torrent</li>
-                                                        <li>
-                                                            <a tabindex="-1" href="{{ url('staff/torrents/' . $torrent->id . '/edit') }}"><i class="fa fa-pencil text-info"></i> Editar Torrent</a>
-                                                        </li>
-                                                        <li class="divider"></li>
-                                                        <li class="dropdown-header">Opções</li>
-                                                        <li>
-                                                            <a tabindex="-1" href="javascript:;" onclick="document.getElementById('file-freeleech-{{ $torrent->id }}').submit();"><i class="fa fa-download text-info"></i>
-                                                                Freeleech @if($torrent->is_freeleech) <i class="fa fa-check text-success"></i> @endif </a>
-                                                            {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/freeleech', 'method' => 'PUT', 'id' => 'file-freeleech-' . $torrent->id , 'style' => 'display: none']) !!}
-                                                            {!! Form::close() !!}
-                                                        </li>
-                                                        <li>
-                                                            <a tabindex="-1" href="javascript:;" onclick="document.getElementById('file-silver-{{ $torrent->id }}').submit();"><i class="fa fa-star-half-empty text-default"></i>
-                                                                Silver @if($torrent->is_silver) <i class="fa fa-check text-success"></i> @endif </a>
-                                                            {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/silver', 'method' => 'PUT', 'id' => 'file-silver-' . $torrent->id , 'style' => 'display: none']) !!}
-                                                            {!! Form::close() !!}
-                                                        </li>
-                                                        <li>
-                                                            <a tabindex="-1" href="javascript:;" onclick="document.getElementById('file-doubleup-{{ $torrent->id }}').submit();"><i class="fa fa-forward text-warning"></i> Double
-                                                                UP @if($torrent->is_doubleup) <i class="fa fa-check text-success"></i> @endif </a>
-                                                            {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/doubleup', 'method' => 'PUT', 'id' => 'file-doubleup-' . $torrent->id , 'style' => 'display: none']) !!}
-                                                            {!! Form::close() !!}
-                                                        </li>
+                                                <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    Opções
+                                                </button>
+                                                <div class="dropdown-menu">
+                                                    <a class="dropdown-item" href="{{ url('staff/torrents/' . $torrent->id . '/edit') }}"><i class="fas fa-pencil-alt text-info"></i> Editar</a>
 
-                                                        <li class="divider"></li>
-                                                        <li>
-                                                            <a tabindex="-1" href="javascript:;" onclick="document.getElementById('file-del-{{ $torrent->id }}').submit();"><i class="fa fa-times text-danger"></i> Remover Torrent</a>
-                                                            {!! Form::open(['url' => 'staff/torrents/' . $torrent->id, 'method' => 'DELETE', 'id' => 'file-del-' . $torrent->id , 'style' => 'display: none']) !!}
-                                                            {!! Form::close() !!}
-                                                        </li>
-                                                    </ul>
+                                                    <a class="dropdown-item" class="m-l-15" href="javascript:;" onclick="document.getElementById('file-freeleech-{{ $torrent->id }}').submit();"><i class="fa fa-download text-info"></i> Freeleech
+                                                        @if($torrent->is_freeleech) <i class="fa fa-check text-success"></i> @endif </a>
+                                                    {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/freeleech', 'method' => 'PUT', 'id' => 'file-freeleech-' . $torrent->id , 'style' => 'display: none']) !!}
+                                                    {!! Form::close() !!}
+                                                    <a class="dropdown-item" class="m-l-15" href="javascript:;" onclick="document.getElementById('file-silver-{{ $torrent->id }}').submit();"><i class="fas fa-star-half-alt text-cyan"></i> Silver
+                                                        @if($torrent->is_silver) <i class="fa fa-check text-success"></i> @endif </a>
+                                                    {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/silver', 'method' => 'PUT', 'id' => 'file-silver-' . $torrent->id , 'style' => 'display: none']) !!}
+                                                    {!! Form::close() !!}
+                                                    <a class="dropdown-item" class="m-l-15" href="javascript:;" onclick="document.getElementById('file-doubleup-{{ $torrent->id }}').submit();"><i class="fa fa-forward text-warning"></i>  Double UP
+                                                        @if($torrent->is_doubleup) <i class="fa fa-check text-success"></i> @endif </a>
+                                                    {!! Form::open(['url' => 'staff/torrent/' . $torrent->id . '/doubleup', 'method' => 'PUT', 'id' => 'file-doubleup-' . $torrent->id , 'style' => 'display: none']) !!}
+                                                    {!! Form::close() !!}
+                                                    <div class="dropdown-divider"></div>
+                                                    <a class="dropdown-item" class="m-l-15" href="javascript:;" onclick="document.getElementById('file-del-{{ $torrent->id }}').submit();"><i class="fa fa-times text-danger"></i> Deletar</a>
+                                                    {!! Form::open(['url' => 'staff/torrents/' . $torrent->id, 'method' => 'DELETE', 'id' => 'file-del-' . $torrent->id , 'style' => 'display: none']) !!}
+                                                    {!! Form::close() !!}
                                                 </div>
                                             </div>
                                         </td>
@@ -103,4 +91,21 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            $('#datatable').DataTable({
+                "displayLength": 50,
+                "searching": true,
+                "responsive": true,
+                "order": [[ 1, "asc" ]],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                }
+            });
+        });
+    </script>
 @endsection
