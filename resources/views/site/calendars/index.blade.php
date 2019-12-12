b174d6284bd7f19803a0dfe776446ec34f419228
John <juaorok@hotmail.com>
2019-12-12 08:23:32 -0300

    Create index.blade.php

---
+++ resources/views/site/calendars/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Calendário')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Calendário</h4>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        This is some text within a card block.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
