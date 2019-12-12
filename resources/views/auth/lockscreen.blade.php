2dc851a940dfdb5710456dc0c6742476d077ec0e
John <juaorok@hotmail.com>
2019-12-12 08:23:11 -0300

    Update lockscreen.blade.php

--- resources/views/auth/lockscreen.blade.php
+++ resources/views/auth/lockscreen.blade.php
@@ -1,64 +1,54 @@
-@extends('layouts.auth')
+@extends('layouts.application')
 
-@section('title', 'Login')
+@section('subtitle', 'Tela de Bloqueio')
 
-@section('styles')
-    <!-- Page -->
-    <link href="{{ asset('css/pages/authentication.css') }}" rel="stylesheet">
-@endsection
-
-@section('content')
+@section('layout-content')
 
     <!-- Content -->
+    <div class="main-wrapper">
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
 
-    <div class="authentication-wrapper authentication-2 ui-bg-cover ui-bg-overlay-container px-4" style="background-image: url('{{ asset('images/login-register.jpg') }}');">
-        <div class="ui-bg-overlay bg-dark opacity-25"></div>
-
-        <div class="authentication-inner py-5">
-
-            <div class="card">
-                <div class="card-body p-sm-5">
-
-                    <div class="media align-items-center">
-                        <img src="{{ auth()->user()->getAvatar() }}" alt="Avatar" class="d-block ui-w-60 rounded-circle">
-                        <div class="media-body ml-3">
-                            <div class="text-light small font-weight-semibold line-height-1 mb-1">LOGADO COMO</div>
-                            <div class="text-large font-weight-bolder line-height-1">{{ auth()->user()->username }}</div>
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url({{ asset('images/login-register.jpg') }}) no-repeat center center;">
+            <div class="auth-box">
+                <div>
+                    <div class="logo">
+                        <span class="db"><img alt="thumbnail" class="rounded-circle" width="100" src="{{ auth()->user()->avatar() }}"></span>
+                        <h5 class="font-medium m-b-20">Tela de Bloqueio</h5>
+                    </div>
+                    <!-- Form -->
+                    <div class="row">
+                        <div class="col-12">
+                            @includeIf('errors.errors', [$errors])
+                            @include('includes.messages')
+                            <form class="js-validation-lockscreen form-horizontal m-t-20" method="POST" action="{{ url('unlockscreen') }}">
+                                @csrf
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }} form-control-lg" type="password" name="password" required placeholder="Digite sua senha" id="password" minlength="6" maxlength="16" autocomplete="off">
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="form-group text-center">
+                                    <div class="col-xs-12 p-b-20">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Login</button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
-
-                    <hr class="my-4">
-
-                @includeIf('errors.errors', [$errors])
-                @include('includes.messages')
-                <!-- Form -->
-                    {!! Form::open(['url' => 'unlockscreen', 'class' => 'js-validation-lockscreen form-horizontal']) !!}
-                    <p class="text-muted small">Por favor, digite sua senha para prosseguir.</p>
-                    <div class="input-group">
-                        <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" name="password" required placeholder="Digite sua senha" id="password" minlength="6" maxlength="16" autocomplete="off">
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-success icon-btn">
-                                <i class="ion ion-md-arrow-forward"></i>
-                            </button>
-                        </div>
-                    </div>
-                {!! Form::close() !!}
-                <!-- / Form -->
-
-                </div>
-                <div class="card-footer text-center text-muted small px-sm-5">
-                    Não é você? <a href="{{ url('login') }}">Faça o login com uma conta diferente</a>
                 </div>
             </div>
-
         </div>
     </div>
-
     <!-- / Content -->
 
 @endsection
@@ -66,4 +56,8 @@
 @section('scripts')
     <script src="{{ asset('vendor/validate/validate.js') }}"></script>
     <script src="{{ asset('js/pages/lockscreen.js') }}"></script>
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $(".preloader").fadeOut();
+    </script>
 @endsection
