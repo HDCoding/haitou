36198f7a813ded51952efcc6ff88442a8ee31c1e
John <juaorok@hotmail.com>
2019-12-12 08:23:15 -0300

    Update email.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,66 +1,67 @@
-@extends('layouts.auth')
+@extends('layouts.application')
 
-@section('title', 'Esqueceu a senha')
+@section('subtitle', 'Esqueceu a senha')
 
-@section('css')
-    <!-- Page -->
-    <link href="{{ asset('css/pages/authentication.css') }}" rel="stylesheet">
-@endsection
-
-@section('content')
+@section('layout-content')
 
     <!-- Content -->
+    <div class="main-wrapper">
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
 
-    <div class="authentication-wrapper authentication-2 ui-bg-cover ui-bg-overlay-container px-4" style="background-image: url('{{ asset('images/login-register.jpg') }}');">
-        <div class="authentication-inner py-5">
-            @includeIf('errors.errors', [$errors])
-            @include('includes.messages')
-            <!-- Form -->
-            {!! Form::open(['url' => route('password.email'), 'class' => 'js-validation-forget-password card']) !!}
-            <div class="p-4 p-sm-5">
-                <!-- Logo -->
-                <div class="d-flex justify-content-center align-items-center pb-2 mb-4">
-                    <div class="ui-w-60">
-                        <div class="w-100 position-relative">
-                            <img src="{{ asset('images/favicons/apple-icon-60x60.png') }}" alt="Logo">
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background:url({{ asset('images/login-register.jpg') }}) no-repeat center center;">
+            <div class="auth-box">
+                <div id="loginform">
+                    <div class="logo">
+                        <span class="db"><img src="{{ asset('images/favicons/apple-icon-60x60.png') }}" alt="logo" /></span>
+                        <h5 class="font-medium m-b-20 m-t-20">Redefinir sua senha</h5>
+                    </div>
+                    <!-- Form -->
+                    <div class="row">
+                        <div class="col-12">
+                            @includeIf('errors.errors', [$errors])
+                            @include('includes.messages')
+                            <form class="js-validation-forget-password form-horizontal m-t-20" method="POST" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }} form-control-lg" autocomplete="off" id="email" maxlength="70" name="email" placeholder="E-mail" required value="{{ old('email') }}">
+                                    @if($errors->has('email'))
+                                        <span class="invalid-feedback">Este campo é obrigatório</span>
+                                    @endif
+                                </div>
+                                <div class="form-group text-center">
+                                    <div class="col-xs-12 p-b-20">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Resetar</button>
+                                    </div>
+                                </div>
+                                <div class="form-group m-b-0 m-t-10">
+                                    <div class="col-sm-12 text-center">
+                                        Lembrou sua senha? <a href="{{ url('login') }}">Login</a>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-                <!-- / Logo -->
-
-                <h5 class="text-center text-muted font-weight-normal mb-4">Redefinir sua senha</h5>
-
-                <hr class="mt-0 mb-4">
-
-                <p>Digite seu endereço de e-mail e nós lhe enviaremos um link para redefinir sua senha.</p>
-
-                <div class="form-group">
-                    <label for="email" class="form-label">E-mail</label>
-                    <input id="email" type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" name="email" value="{{ old('email') }}" required maxlength="70" autocomplete="off">
-                    @if($errors->has('email'))
-                        <span class="invalid-feedback">Este campo é obrigatório</span>
-                    @endif
-                </div>
-
-                <button type="submit" class="btn btn-primary btn-block">Resetar</button>
-
-                <div class="card-footer py-3 px-4 px-sm-5">
-                    <div class="text-center text-muted">
-                        Lembrou sua senha? <a href="{{ url('login') }}">Login</a>
-                    </div>
-                </div>
             </div>
-            {!! Form::close() !!}
-            <!-- / Form -->
-
         </div>
     </div>
-
     <!-- / Content -->
 
 @endsection
 
-@section('scripts')
+@section('script')
     <script src="{{ asset('vendor/validate/validate.js') }}"></script>
     <script src="{{ asset('js/pages/email.js') }}"></script>
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $(".preloader").fadeOut();
+    </script>
 @endsection
