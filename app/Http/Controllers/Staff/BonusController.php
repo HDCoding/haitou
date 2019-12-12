88a245016f78b8f1423d674a01286a43f2dd594f
John <juaorok@hotmail.com>
2019-12-12 08:22:09 -0300

    Update BonusController.php

--- app/Http/Controllers/Staff/BonusController.php
+++ app/Http/Controllers/Staff/BonusController.php
@@ -11,7 +11,7 @@ class BonusController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
