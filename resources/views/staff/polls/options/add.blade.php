2ccc6ef77c36a1a9fd5d27764022bb77df16bce1
John <juaorok@hotmail.com>
2019-12-12 08:25:57 -0300

    Update add.blade.php

--- resources/views/staff/polls/options/add.blade.php
+++ resources/views/staff/polls/options/add.blade.php
@@ -32,9 +32,9 @@
                         {!! Form::hidden('poll_id', $poll->id) !!}
 
                         <div class="form-group after-add-more">
-                            {!! Form::label('option', 'Opção: *', ['class' => 'col-xs-12']) !!}
+                            {!! Form::label('name', 'Opção: *', ['class' => 'col-xs-12']) !!}
                             <div class="input-group">
-                                {!! Form::text('option[]', null, ['class' => 'form-control', 'maxlength' => 250, 'required']) !!}
+                                {!! Form::text('name[]', null, ['class' => 'form-control', 'maxlength' => 250, 'required']) !!}
                                 <span class="input-group-btn">
                                     <button class="btn btn-success add-more" type="button">
                                         <i class="fa fa-plus-circle"></i>
@@ -43,14 +43,14 @@
                             </div>
                         </div>
                         <br>
-                        {!! Form::submit('Adicionar', ['class' => 'btn btn-primary btn-rounded btn-outline-primary']) !!}
+                        {!! Form::submit('Adicionar', ['class' => 'btn btn-rounded btn-outline-primary']) !!}
                         {!! Form::close() !!}
 
                         <!-- Copy Fields-These are the fields which we get through jquery and then add after the above input -->
                         <div class="form-group copy-fields" id="hide" style="display: none">
-                            <label for="option" class="col-xs-12">Opção: *</label>
+                            <label for="name" class="col-xs-12">Opção: *</label>
                             <div class="form-group control-group input-group">
-                                <input id="option" type="text" name="option[]" class="form-control" maxlength="255" required>
+                                <input id="name" type="text" name="name[]" class="form-control" maxlength="255" required>
                                 <div class="input-group-btn">
                                     <button class="btn btn-danger remove" type="button">
                                         <i class="fa fa-minus-circle"></i>
