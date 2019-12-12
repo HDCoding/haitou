a7dfb68ced0b4fe990a9607aeff3a323c3d10b11
John <juaorok@hotmail.com>
2019-12-12 09:37:17 -0300

    Update TorrentsController.php

--- app/Http/Controllers/Staff/TorrentsController.php
+++ app/Http/Controllers/Staff/TorrentsController.php
@@ -8,7 +8,6 @@ use App\Models\Category;
 use App\Models\Fansub;
 use App\Models\Media;
 use App\Models\Torrent;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class TorrentsController extends Controller
