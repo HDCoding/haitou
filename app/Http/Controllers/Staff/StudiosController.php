6378390a683ad30042ac14f89e888648bd1f7655
John <juaorok@hotmail.com>
2019-12-12 08:22:41 -0300

    Update StudiosController.php

--- app/Http/Controllers/Staff/StudiosController.php
+++ app/Http/Controllers/Staff/StudiosController.php
@@ -11,7 +11,7 @@ class StudiosController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
