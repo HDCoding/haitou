27b8d5d2ca209f9a5e715c046db0cedb5f9b4b69
John <juaorok@hotmail.com>
2019-12-12 08:22:22 -0300

    Update FreeSlotsController.php

--- app/Http/Controllers/Staff/FreeSlotsController.php
+++ app/Http/Controllers/Staff/FreeSlotsController.php
@@ -12,7 +12,7 @@ class FreeSlotsController extends Controller
 {
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
