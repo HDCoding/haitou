39be965f1ecee1adb96ab9ded2c856cddf963714
John <juaorok@hotmail.com>
2019-12-12 08:26:15 -0300

    Update form.blade.php

--- resources/views/staff/studios/form.blade.php
+++ resources/views/staff/studios/form.blade.php
@@ -7,26 +7,21 @@
     {!! Form::label('name', 'Nome *') !!}
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
-
 <div class="form-group">
     {!! Form::label('logo', 'Logo') !!}
     {!! Form::text('logo', null, ['class' => 'form-control']) !!}
 </div>
-
 <div class="form-group">
     {!! Form::label('website', 'Website') !!}
     {!! Form::text('website', null, ['class' => 'form-control']) !!}
 </div>
-
 <div class="form-group">
     {!! Form::label('description', 'Descrição') !!}
     {!! Form::textarea('description', null, ['class' => 'form-control note', 'rows' => 8]) !!}
 </div>
+{!! Form::submit($submitButton, ['class' => 'btn btn-rounded btn-outline-primary']) !!}
 
-{!! Form::submit($submitButton, ['class' => 'btn btn-primary btn-rounded btn-outline']) !!}
-<br>
-
-@section('script')
+@section('scripts')
     <!-- sceditor -->
     <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
     <script src="{{ asset('vendor/sceditor/minified/formats/bbcode.js') }}"></script>
