33ec79ef6f3a88f7995c14c3ff365980a7678698
John <juaorok@hotmail.com>
2019-12-12 09:36:48 -0300

    Update FailedLoginsController.php

--- app/Http/Controllers/Staff/FailedLoginsController.php
+++ app/Http/Controllers/Staff/FailedLoginsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
 use App\Models\FailedLogin;
-use Illuminate\Http\Request;
 
 class FailedLoginsController extends Controller
 {
@@ -15,7 +14,7 @@ class FailedLoginsController extends Controller
 
     public function index()
     {
-        $attempts = FailedLogin::with('user:id,name')->latest()->paginate(25);
+        $attempts = FailedLogin::with('user:id,username')->latest()->paginate(25);
         return view('staff.failedlogins.index', compact('attempts'));
     }
 }
