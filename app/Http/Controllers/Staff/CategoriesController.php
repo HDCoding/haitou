3dbe5d006bbc818aeffa7ae751583acbeea173c9
John <juaorok@hotmail.com>
2019-12-12 09:36:41 -0300

    Update CategoriesController.php

--- app/Http/Controllers/Staff/CategoriesController.php
+++ app/Http/Controllers/Staff/CategoriesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Staff\CategoriesRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesController extends Controller
 {
