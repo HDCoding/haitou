64965d2aaef78284748982af7c91258d7a095baa
John <juaorok@hotmail.com>
2019-12-12 08:23:21 -0300

    Update dashboard-left.blade.php

--- resources/views/includes/dashboard-left.blade.php
+++ resources/views/includes/dashboard-left.blade.php
@@ -11,7 +11,7 @@
                         <div class="user-pic"><img src="{{ asset('images/avatar.jpg') }}" alt="users" class="rounded-circle" width="40" /></div>
                         <div class="user-content hide-menu ml-2">
                             <a href="javascript:void(0)" class="" id="Userdd" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <h5 class="mb-0 user-name font-medium">Steave Jobs <i class="fa fa-angle-down"></i></h5>
+                                <h5 class="m-b-0 user-name font-medium">Steave Jobs <i class="fa fa-angle-down"></i></h5>
                                 <span class="op-5 user-email">varun@gmail.com</span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="Userdd">
