67bd8e16eb4f76b152de868ac6a3b748b789e529
John <juaorok@hotmail.com>
2019-12-12 08:24:07 -0300

    Update form.blade.php

--- resources/views/staff/actors/form.blade.php
+++ resources/views/staff/actors/form.blade.php
@@ -1,6 +1,6 @@
 @section('css')
     <!-- DatePicker -->
-    <link rel="stylesheet" href="{{ asset('vendor/bootstrap-datepicker/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">
     <!-- sceditor -->
     <link rel="stylesheet" href="{{ asset('vendor/sceditor/minified/themes/default.min.css') }}">
 @endsection
@@ -10,28 +10,28 @@
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('image', 'Imagem:', ['class' => 'form-label']) !!}
-    {!! Form::text('image', null, ['class' => 'form-control']) !!}
+    {!! Form::label('imagem', 'Imagem:', ['class' => 'form-label']) !!}
+    {!! Form::file('image', ['accept' => 'image/*']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('website', 'Web Site:', ['class' => 'form-label']) !!}
     {!! Form::text('website', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
-    {!! Form::label('dob', 'Data de Nascimento:', ['class' => 'form-label']) !!}
-    {!! Form::text('dob', null, ['class' => 'form-control']) !!}
+    {!! Form::label('birthday', 'Data de Nascimento:', ['class' => 'form-label']) !!}
+    {!! Form::text('birthday', null, ['class' => 'form-control']) !!}
 </div>
 <div class="form-group">
     {!! Form::label('description', 'Descrição: *', ['class' => 'form-label']) !!}
     {!! Form::textarea('description', null, ['class' => 'form-control', 'rows' => 8]) !!}
 </div>
 
-{!! Form::submit($submitButton, ['class' => 'btn btn-primary btn-rounded btn-outline-primary']) !!}
+{!! Form::submit($submitButton, ['class' => 'btn btn-rounded btn-outline-primary']) !!}
 
-@section('script')
-    <!-- DatePicker -->
-    <script src="{{ asset('vendor/bootstrap-datepicker/bootstrap-datepicker.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap-datepicker/locales/bootstrap-datepicker.pt-BR.min.js') }}"></script>
+@section('scripts')
+    <!-- datepicker -->
+    <script src="{{ asset('vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js') }}"></script>
 
     <!-- sceditor -->
     <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
@@ -41,13 +41,13 @@
     <!-- script -->
     <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         $(document).ready(function() {
-            $('#dob').datepicker({
+            $('#birthday').datepicker({
                 format: 'yyyy-mm-dd',
                 language: 'pt-BR',
                 autoclose: true
             });
 
-            var textarea = document.getElementById('description');
+            let textarea = document.getElementById('description');
             sceditor.create(textarea, {
                 format: 'bbcode',
                 locale: 'pt-BR',
