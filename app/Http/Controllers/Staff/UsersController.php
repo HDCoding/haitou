1c4b2d402c206a70cf5555fc624700b12e6072e1
John <juaorok@hotmail.com>
2019-12-12 21:34:40 -0300

    Update UsersController.php

--- app/Http/Controllers/Staff/UsersController.php
+++ app/Http/Controllers/Staff/UsersController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\Staff\UserSuspendedRequest;
 use App\Http\Requests\Staff\UserUpdatesRequest;
 use App\Http\Requests\Staff\UserWarnedRequest;
 use App\Models\Group;
+use App\Models\Log;
 use App\Models\Moderate;
 use App\Models\Note;
 use App\User;
@@ -18,7 +19,7 @@ use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
-    private $pendent, $activated, $suspended, $banned;
+    private $pendent, $activated, $suspended, $banned, $log;
 
     public function __construct(User $user)
     {
@@ -27,6 +28,7 @@ class UsersController extends Controller
         $this->activated = $user->where('status', '=', 1)->count();
         $this->suspended = $user->where('status', '=', 2)->count();
         $this->banned = $user->where('status', '=', 3)->count();
+        $this->log = new Log();
     }
 
     public function index()
@@ -53,14 +55,14 @@ class UsersController extends Controller
         $user = User::find($user_id);
         $user->update($request->except('_token'));
 
-        $group = $request->input('group');
+        $group = $request->input('group_id');
         if ($user->group_id !== $group) {
             $user->group_id = $group;
             $user->save();
-            //$this->log::novo('Staff atualizou classe do membro', true);
+            $this->log::record('Staff atualizou classe do membro', true);
         }
 
-        //$this->log::novo('Staff atualizou conta do membro', true);
+        $this->log::novo('Staff atualizou conta do membro', true);
 
         toastr()->info('Usuário atualizado.', 'Sucesso');
         return redirect()->to('staff/users');
@@ -85,7 +87,7 @@ class UsersController extends Controller
 
         DB::table('users')->where('id', '=', $user_id)->update(['status' => 3]);
 
-        //$this->log::novo('Staff baniu um membro', true);
+        $this->log::record('Staff baniu um membro', true);
 
         toastr()->warning('Usuário Banido.', 'Aviso');
         return redirect()->to('staff/users');
@@ -111,7 +113,7 @@ class UsersController extends Controller
 
         DB::table('users')->where('id', '=', $user_id)->update(['status' => 2]);
 
-        //$this->log::novo('Staff suspendeu conta de membro', true);
+        $this->log::record('Staff suspendeu conta de membro', true);
 
         toastr()->info('Usuário Suspenso.', 'Aviso');
         return redirect()->to('staff/users');
@@ -137,7 +139,7 @@ class UsersController extends Controller
 
         DB::table('users')->where('id', '=', $user_id)->update(['is_warned' => true]);
 
-        //$this->log::novo('Staff advertiu membro', true);
+        $this->log::record('Staff advertiu membro', true);
 
         toastr()->info('Usuário Advertido.', 'Aviso');
         return redirect()->to('staff/users');
@@ -198,58 +200,58 @@ class UsersController extends Controller
     public function formPermission(int $user_id)
     {
         $user = User::where('id', '=', $user_id)->first();
-        $permission = Permission::where('user_id', '=', $user->id)->first();
+//        $permission = Permission::where('user_id', '=', $user->id)->first();
 
-        return view('staff.users.permissions', compact('user', 'permission'));
+        return view('staff.users.permissions', compact('user'));
     }
 
     public function updatePermission(Request $request, int $user_id)
     {
-        $permission = Permission::where('user_id', '=', $user_id)->first();
-        $permission->actors_comment = $request->input('actors_comment') ? true : false;
-        $permission->calendars_create = $request->input('calendars_create') ? true : false;
-        $permission->calendars_comment = $request->input('calendars_comment') ? true : false;
-        $permission->characters_comment = $request->input('characters_comment') ? true : false;
-        $permission->chatbox_view = $request->input('chatbox_view') ? true : false;
-        $permission->fansubs_comment = $request->input('fansubs_comment') ? true : false;
-        $permission->medias_comment = $request->input('medias_comment') ? true : false;
-        $permission->studios_comment = $request->input('studios_comment') ? true : false;
-        $permission->torrents_comment = $request->input('torrents_comment') ? true : false;
-        $permission->torrents_upload = $request->input('torrents_upload') ? true : false;
-        $permission->torrents_download = $request->input('torrents_download') ? true : false;
-        $permission->staff_panel = $request->input('staff_panel') ? true : false;
-        $permission->full_access = $request->input('full_access') ? true : false;
-        $permission->achievements_mod = $request->input('achievements_mod') ? true : false;
-        $permission->actors_mod = $request->input('actors_mod') ? true : false;
-        $permission->backups_mod = $request->input('backups_mod') ? true : false;
-        $permission->bonus_mod = $request->input('bonus_mod') ? true : false;
-        $permission->calendars_mod = $request->input('calendars_mod') ? true : false;
-        $permission->categories_mod = $request->input('categories_mod') ? true : false;
-        $permission->characters_mod = $request->input('characters_mod') ? true : false;
-        $permission->cheaters_mod = $request->input('cheaters_mod') ? true : false;
-        $permission->commands_mod = $request->input('commands_mod') ? true : false;
-        $permission->failed_logins_mod = $request->input('failed_logins_mod') ? true : false;
-        $permission->fansubs_mod = $request->input('fansubs_mod') ? true : false;
-        $permission->faqs_mod = $request->input('faqs_mod') ? true : false;
-        $permission->forums_mod = $request->input('forums_mod') ? true : false;
-        $permission->genres_mod = $request->input('genres_mod') ? true : false;
-        $permission->logs_mod = $request->input('logs_mod') ? true : false;
-        $permission->lotteries_mod = $request->input('lotteries_mod') ? true : false;
-        $permission->medias_mod = $request->input('medias_mod') ? true : false;
-        $permission->moods_mod = $request->input('moods_mod') ? true : false;
-        $permission->news_mod = $request->input('news_mod') ? true : false;
-        $permission->permissions_mod = $request->input('permissions_mod') ? true : false;
-        $permission->polls_mod = $request->input('polls_mod') ? true : false;
-        $permission->reports_mod = $request->input('reports_mod') ? true : false;
-        $permission->requests_mod = $request->input('requests_mod') ? true : false;
-        $permission->groups_mod = $request->input('groups_mod') ? true : false;
-        $permission->rules_mod = $request->input('rules_mod') ? true : false;
-        $permission->settings_mod = $request->input('settings_mod') ? true : false;
-        $permission->studios_mod = $request->input('studios_mod') ? true : false;
-        $permission->torrents_mod = $request->input('torrents_mod') ? true : false;
-        $permission->users_mod = $request->input('users_mod') ? true : false;
-        $permission->visitors_mod = $request->input('visitors_mod') ? true : false;
-        $permission->save();
+//        $permission = Permission::where('user_id', '=', $user_id)->first();
+//        $permission->actors_comment = $request->input('actors_comment') ? true : false;
+//        $permission->calendars_create = $request->input('calendars_create') ? true : false;
+//        $permission->calendars_comment = $request->input('calendars_comment') ? true : false;
+//        $permission->characters_comment = $request->input('characters_comment') ? true : false;
+//        $permission->chatbox_view = $request->input('chatbox_view') ? true : false;
+//        $permission->fansubs_comment = $request->input('fansubs_comment') ? true : false;
+//        $permission->medias_comment = $request->input('medias_comment') ? true : false;
+//        $permission->studios_comment = $request->input('studios_comment') ? true : false;
+//        $permission->torrents_comment = $request->input('torrents_comment') ? true : false;
+//        $permission->torrents_upload = $request->input('torrents_upload') ? true : false;
+//        $permission->torrents_download = $request->input('torrents_download') ? true : false;
+//        $permission->staff_panel = $request->input('staff_panel') ? true : false;
+//        $permission->full_access = $request->input('full_access') ? true : false;
+//        $permission->achievements_mod = $request->input('achievements_mod') ? true : false;
+//        $permission->actors_mod = $request->input('actors_mod') ? true : false;
+//        $permission->backups_mod = $request->input('backups_mod') ? true : false;
+//        $permission->bonus_mod = $request->input('bonus_mod') ? true : false;
+//        $permission->calendars_mod = $request->input('calendars_mod') ? true : false;
+//        $permission->categories_mod = $request->input('categories_mod') ? true : false;
+//        $permission->characters_mod = $request->input('characters_mod') ? true : false;
+//        $permission->cheaters_mod = $request->input('cheaters_mod') ? true : false;
+//        $permission->commands_mod = $request->input('commands_mod') ? true : false;
+//        $permission->failed_logins_mod = $request->input('failed_logins_mod') ? true : false;
+//        $permission->fansubs_mod = $request->input('fansubs_mod') ? true : false;
+//        $permission->faqs_mod = $request->input('faqs_mod') ? true : false;
+//        $permission->forums_mod = $request->input('forums_mod') ? true : false;
+//        $permission->genres_mod = $request->input('genres_mod') ? true : false;
+//        $permission->logs_mod = $request->input('logs_mod') ? true : false;
+//        $permission->lotteries_mod = $request->input('lotteries_mod') ? true : false;
+//        $permission->medias_mod = $request->input('medias_mod') ? true : false;
+//        $permission->moods_mod = $request->input('moods_mod') ? true : false;
+//        $permission->news_mod = $request->input('news_mod') ? true : false;
+//        $permission->permissions_mod = $request->input('permissions_mod') ? true : false;
+//        $permission->polls_mod = $request->input('polls_mod') ? true : false;
+//        $permission->reports_mod = $request->input('reports_mod') ? true : false;
+//        $permission->requests_mod = $request->input('requests_mod') ? true : false;
+//        $permission->groups_mod = $request->input('groups_mod') ? true : false;
+//        $permission->rules_mod = $request->input('rules_mod') ? true : false;
+//        $permission->settings_mod = $request->input('settings_mod') ? true : false;
+//        $permission->studios_mod = $request->input('studios_mod') ? true : false;
+//        $permission->torrents_mod = $request->input('torrents_mod') ? true : false;
+//        $permission->users_mod = $request->input('users_mod') ? true : false;
+//        $permission->visitors_mod = $request->input('visitors_mod') ? true : false;
+//        $permission->save();
 
         return redirect()->route('staff.user.permissions', [$user_id]);
     }
