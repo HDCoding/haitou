76bf1759eecb59bbb0f732077d4c09f65b5a61bc
John <juaorok@hotmail.com>
2019-12-12 21:34:37 -0300

    Update FreeSlotsController.php

--- app/Http/Controllers/Staff/FreeSlotsController.php
+++ app/Http/Controllers/Staff/FreeSlotsController.php
@@ -20,9 +20,14 @@ class FreeSlotsController extends Controller
         return view('staff.freeslots.index', compact('freeslots'));
     }
 
-    public function update(FreeSlotsRequest $request, $request_id)
+    public function create()
     {
-        $sitepot = Freeslot::findOrFail($request_id);
+        return view('staff.freeslots.create');
+    }
+
+    public function store(FreeSlotsRequest $request)
+    {
+        $sitepot = new Freeslot();
         //pega os dados do form
         $freeleech = $request->input('is_freeleech') ? true : false;
         $silver = $request->input('is_silver') ? true : false;
@@ -32,33 +37,56 @@ class FreeSlotsController extends Controller
             return redirect()->back()->withInput();
         }
 
+        $sitepot->name = $request->input('name');
         $sitepot->required = $request->input('required');
         $sitepot->days = $request->input('days');
         $sitepot->is_doubleup = $request->input('is_doubleup') ? true : false;
         $sitepot->is_silver = $silver;
         $sitepot->is_freeleech = $freeleech;
+        $sitepot->is_active = true;
+        $sitepot->save();
 
+        toastr()->info('Request criado.', 'Sucesso');
+        return redirect()->to('staff/freeslots');
+    }
+
+    public function edit($freeslot_id)
+    {
+        $sitepot = Freeslot::findOrFail($freeslot_id);
+        return view('staff.freeslots.edit', compact('sitepot'));
+    }
+
+    public function update(FreeSlotsRequest $request, $freeslot_id)
+    {
+        $sitepot = Freeslot::findOrFail($freeslot_id);
+        //pega os dados do form
+        $freeleech = $request->input('is_freeleech') ? true : false;
+        $silver = $request->input('is_silver') ? true : false;
+
+        if ($silver && $freeleech) {
+            toastr()->error('Silver e Freeleech dão conflito, escolha apenas 1.', 'Erro');
+            return redirect()->back()->withInput();
+        }
+
+        $sitepot->name = $request->input('name');
+        $sitepot->required = $request->input('required');
+        $sitepot->days = $request->input('days');
+        $sitepot->is_doubleup = $request->input('is_doubleup') ? true : false;
+        $sitepot->is_silver = $silver;
+        $sitepot->is_freeleech = $freeleech;
         $sitepot->update();
 
         toastr()->info('Request atulizado.', 'Sucesso');
         return redirect()->to('staff/freeslots');
     }
 
-    public function enableDisable()
+    public function destroy($freeslot_id)
     {
-        $request = Freeslot::findOrFail(1);
-        $request->is_enabled = !$request->is_enabled;
-        $request->required = 0;
-        $request->actual = 0;
-        $request->days = 0;
-        $request->is_freeleech = false;
-        $request->is_silver = false;
-        $request->is_doubleup = false;
-        $request->update();
-
-        DB::table('request_points')->truncate();
-
-        toastr()->warning('Request Ativado/Desativado.', 'Aviso');
+        $sitepot = Freeslot::findOrFail($freeslot_id);
+        $sitepot->is_active = false;
+        $sitepot->save();
+        $sitepot->delete();
+        toastr()->warning('Free Slot deletado.', 'Aviso');
         return redirect()->to('staff/freeslots');
     }
 }
