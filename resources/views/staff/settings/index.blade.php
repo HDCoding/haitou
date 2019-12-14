dd3eb06b01380c05d18b89b2ce1d00dabbc41d5f
John <juaorok@hotmail.com>
2019-12-14 07:31:02 -0300

    Update index.blade.php

--- resources/views/staff/settings/index.blade.php
+++ resources/views/staff/settings/index.blade.php
@@ -38,22 +38,43 @@
                             <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#favicon" role="tab"><span class="hidden-sm-up"><i class="ti-user"></i></span> <span class="hidden-xs-down">Favicon</span></a> </li>
                             <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#images" role="tab"><span class="hidden-sm-up"><i class="ti-email"></i></span> <span class="hidden-xs-down">Imagens</span></a> </li>
                         </ul>
-                        @includeIf('errors.errors', [$errors])
                         <!-- Tab panes -->
                         <div class="tab-content tabcontent-border">
                             <div class="tab-pane active" id="setting" role="tabpanel">
-                                <div class="form-group">
-                                    {!! Form::label('site_title', 'Site Título: *') !!}
-                                    {!! Form::text('site_title', $settings, ['class' => 'form-control']) !!}
-                                </div>
-                                <div class="form-group">
-                                    {!! Form::label('meta_keywords', 'Meta Keywords') !!}
-                                    {!! Form::textarea('meta_keywords', $settings, ['class' => 'form-control', 'rows' => 3]) !!}
-                                </div>
-                                <div class="form-group">
-                                    {!! Form::label('meta_description', 'Meta Descrição') !!}
-                                    {!! Form::textarea('meta_description', $settings, ['class' => 'form-control', 'rows' => 3]) !!}
-                                </div>
+                                @includeIf('errors.errors', [$errors])
+                                <form method="post" action="{{ route('settings.store') }}" class="form-horizontal">
+                                    @csrf
+
+                                    @if(count(config('settings', [])) )
+
+                                        @foreach(config('settings') as $section => $fields)
+                                            <div class="panel panel-info">
+                                                <div class="panel-heading">
+                                                    {{ $fields['title'] }}
+                                                </div>
+
+                                                <div class="panel-body">
+                                                    <div class="row">
+                                                        <div class="col-md-7 col-md-offset-2">
+                                                            @foreach($fields['elements'] as $field)
+                                                                @includeIf('staff.settings.fields.' . $field['type'] )
+                                                            @endforeach
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        <!-- end panel for {{ $fields['title'] }} -->
+                                        @endforeach
+
+                                    @endif
+
+                                    <div class="row m-b-md">
+                                        <div class="col-md-12">
+                                            <button class="btn-primary btn">Salvar Configurações</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                             <div class="tab-pane p-20" id="favicon" role="tabpanel">
                                 2
