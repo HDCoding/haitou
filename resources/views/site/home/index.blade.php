6d04e9f18f9734bf41f36220e879fe7f33fee736
John <juaorok@hotmail.com>
2019-12-14 07:30:37 -0300

    Update index.blade.php

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', 'Home')
 
+@section('css')
+    <link href="{{ asset('vendor/vegas/vegas.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -14,14 +18,45 @@
 
     <div class="container-fluid">
         <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <div class="card vegas-fixed-background" id="home-cover">
+                    <div class="card-body py-5 my-5">
+                        {{--<h4 class="text-center text-dark ">Texto</h4>--}}
+                    </div>
+                </div>
+            </div>
+
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        This is some text within a card block.
+                        Sem ideias do que colocar na Home :( <br>
+                        Uma ajuda por favor
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+@endsection
+
+@section('scripts')
+    <!-- VegasJS -->
+    <script src="{{ asset('vendor/vegas/vegas.js') }}"></script>
+
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(function() {
+            // Fixed bg
+            $('#home-cover').vegas({
+                overlay: false,
+                timer: false,
+                shuffle: true,
+                slides: [
+                    { src: "{{ asset('images/home.jpg') }}" },
+                ],
+                transition: ['fade', 'zoomOut', 'zoomIn', 'blur'],
+                animation: ['kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight']
+            });
+        });
+    </script>
 
 @endsection
