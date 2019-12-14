616b2f44725c7b60ee90f3c5b00678d4f7fcb694
John <juaorok@hotmail.com>
2019-12-14 07:30:51 -0300

    Update index.blade.php

--- resources/views/site/rules/index.blade.php
+++ resources/views/site/rules/index.blade.php
@@ -17,7 +17,31 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        This is some text within a card block.
+                        <h4 class="card-title">Regras</h4>
+                        <h6 class="card-subtitle">Use default tab with class <code>nav-tabs & tabcontent-border </code></h6>
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs" role="tablist">
+                            @foreach($rules as $key => $rule)
+                            <li class="nav-item">
+                                <a class="nav-link {{ $key == 0 ? 'active' : ''}}" data-toggle="tab" href="#tab-{{ $rule->id }}" role="tab">
+                                    <span class="hidden-sm-up">
+                                        <i class="{{ $rule->icon }}"></i>
+                                    </span>
+                                    <span class="hidden-xs-down">{{ $rule->name }}</span>
+                                </a>
+                            </li>
+                            @endforeach
+                        </ul>
+                        <!-- Tab panes -->
+                        <div class="tab-content tabcontent-border m-t-20">
+                            @foreach($rules as $key => $rule)
+                            <div class="tab-pane {{ $key == 0 ? 'active' : ''}}" id="tab-{{ $rule->id }}" role="tabpanel">
+                                @if($rule->id == $rule->id)
+                                    {!! $rule->descriptionHtml() !!}
+                                @endif
+                            </div>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
