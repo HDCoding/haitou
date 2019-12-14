4f32a290ac740f62b47b81ddb52da1dc06b321ce
John <juaorok@hotmail.com>
2019-12-14 07:30:40 -0300

    Create calendar.blade.php

---
+++ resources/views/site/reports/calendar.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Reportar Calendário')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Home</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Reportar Calendário</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Reportar: {{ $calendar->name }}</h4>
+                        @includeIf('errors.errors', [$errors])
+                        @include('includes.messages')
+                        {!! Form::open(['url' => 'report', 'class' => 'form-horizontal']) !!}
+                        {!! Form::hidden('report_type', 4) !!}
+                        {!! Form::hidden('calendar_id', $calendar->id) !!}
+                        @include('site.reports.form', ['submitButton' => 'Reportar'])
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
