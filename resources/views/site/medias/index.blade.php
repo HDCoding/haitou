f2c388532ed68aa1464a3dd7cb234c69d91196d3
John <juaorok@hotmail.com>
2019-12-17 22:18:23 -0300

    Update index.blade.php

--- resources/views/site/medias/index.blade.php
+++ resources/views/site/medias/index.blade.php
@@ -25,7 +25,26 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        This is some text within a card block.
+                        <h3 class="card-title">Rounded Chair</h3>
+                        <h6 class="card-subtitle">globe type chair for rest</h6>
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-6">
+                                <div class="white-box text-center"> <img src="../../assets/images/gallery/chair.jpg" class="img-responsive"> </div>
+                            </div>
+                            <div class="col-lg-9 col-md-9 col-sm-6">
+                                <h4 class="box-title m-t-40">Product description</h4>
+                                <p>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. but the majority have suffered alteration in some form, by injected humour</p>
+                                <h2 class="m-t-40">$153 <small class="text-success">(36% off)</small></h2>
+                                <button class="btn btn-dark btn-rounded m-r-5" data-toggle="tooltip" title="" data-original-title="Add to cart"><i class="ti-shopping-cart"></i> </button>
+                                <button class="btn btn-primary btn-rounded"> Buy Now </button>
+                                <h3 class="box-title m-t-40">Key Highlights</h3>
+                                <ul class="list-unstyled">
+                                    <li><i class="fa fa-check text-success"></i> Sturdy structure</li>
+                                    <li><i class="fa fa-check text-success"></i> Designed to foster easy portability</li>
+                                    <li><i class="fa fa-check text-success"></i> Perfect furniture to flaunt your wonderful collectibles</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
