a38f748e11376c035e7b21075a6255fb866e325d
John <juaorok@hotmail.com>
2019-12-17 22:17:58 -0300

    Update comments.blade.php

--- resources/views/includes/comments.blade.php
+++ resources/views/includes/comments.blade.php
@@ -1,48 +1,55 @@
-<div class="card-body">
+<div class="comment-widgets scrollable" style="height:560px;">
     @forelse($comments as $comment)
-        <div class="media pb-1 m-b-3">
-            <img src="{{ $comment->user->getAvatar() }}" class="d-block ui-w-40 rounded-circle" alt="avatar">
-            <div class="media-body ml-3">
-                {{ link_to_route('user.profile', $comment->user->name, ['slug' => $comment->user->slug]) }}
+    <!-- Comment Row -->
+    <div class="d-flex flex-row comment-row">
+        <div class="p-2">
+            <img src="{{ $comment->user->avatar() }}" alt="user" width="50" class="rounded-circle">
+        </div>
+        <div class="comment-text active w-100">
+            <h6 class="font-medium">
+                {{ link_to_route('user.profile', $comment->username, ['slug' => $comment->user->slug]) }}
+            </h6>
+            <span class="m-b-15 d-block">
+                {{ $comment->content }}
+            </span>
+            <div class="comment-footer">
+                <span class="text-muted float-right">
+                    {{ format_date_time($comment->created_at) }}
+                </span>
                 @if($comment->is_spoiler)
-                    <mark class="text-danger">Spoiler!</mark>
+                    <span class="label label-success label-rounded">Spoiler!</span>
                 @endif
-                <p class="my-1">{{ $comment->content }}</p>
-                <div class="clearfix">
-                    <span class="float-left text-muted small mr-3">{{ $comment->created_at->format('d/m/Y H:i') }}</span>
-
-                    @if($comment->user_id == auth()->user()->id || auth()->user()->permission->staff_panel)
+                <span class="action-icons active">
+                    @if($comment->user_id == auth()->user()->id)
                         <a href="{{ route('comments.edit', [$comment->id]) }}">
-                            <button type="button" class="btn btn-xs btn-outline-primary">
-                                <span class="fas fa-pencil-alt" data-toggle="tooltip" title="Editar Comentário"></span>
-                            </button>
+                            <i class="ti-pencil-alt" data-toggle="tooltip" title="Editar Comentário"></i>
                         </a>
                     @endif
-
-                    @if($comment->user_id == auth()->user()->id || auth()->user()->permission->staff_panel)
-                        <a href="javascript:;"
-                           onclick="document.getElementById('comment-del-{{ $comment->id }}').submit();">
-                            <button type="submit" class="btn btn-xs btn-outline-danger">
-                                <span class="fas fa-times" data-toggle="tooltip" title="Deletar Comentário"></span>
-                            </button>
+                    @if($comment->user_id == auth()->user()->id)
+                        <a href="javascript:;" onclick="document.getElementById('comment-del-{{ $comment->id }}').submit();">
+                            <i class="icon-close" data-toggle="tooltip" title="Deletar Comentário"></i>
                         </a>
                         {!! Form::open(['url' => 'comments/' . $comment->id, 'method' => 'DELETE', 'id' => 'comment-del-' . $comment->id , 'style' => 'display: none']) !!}
                         {!! Form::close() !!}
                     @endif
-
                     @if($comment->user_id !== auth()->user()->id)
-                        <a href="{{ route('comment.report', [$comment->id]) }}" class="btn btn-xs btn-outline-dark">
-                            <span class="fas fa-flag" data-toggle="tooltip" title="Reportar Comentário"></span>
+                        <a href="{{ route('comment.report', [$comment->id]) }}">
+                            <i class="fas fa-flag text-dark" data-toggle="tooltip" title="Reportar Comentário"></i>
                         </a>
                     @endif
-                </div>
+                </span>
             </div>
         </div>
+    </div>
+    <!-- Comment Row -->
     @empty
         <p class="text-center push-30">Nenhum comentário no momento. Seja a(o) primeira(o)</p>
         @if(!isset($torrent->allow_comments))
-            <p class="text-center push-30 ">Comentários foram desativados para esse torrent.</p>
+            <p class="text-center push-30">Comentários foram desativados para esse torrent.</p>
         @endif
     @endforelse
+</div>
+<div class="m-t-40">
     {{ $comments->links() }}
 </div>
+
