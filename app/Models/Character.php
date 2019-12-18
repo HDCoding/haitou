75ab18d114871d04c0b00dfcbe1aa83e3f28da6b
John <juaorok@hotmail.com>
2019-12-17 22:17:55 -0300

    Update Character.php

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -41,4 +41,9 @@ class Character extends Model
     {
         return (new BBCode())->parse($this->description, true);
     }
+
+    public function image()
+    {
+        return asset('images/' . self::uploaderFolder . '/' . $this->image);
+    }
 }
