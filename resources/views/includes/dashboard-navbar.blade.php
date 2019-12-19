223891fcd07b928c057359824ff4302055b6bd3a
John <juaorok@hotmail.com>
2019-12-19 13:51:24 -0300

    Update dashboard-navbar.blade.php

--- resources/views/includes/dashboard-navbar.blade.php
+++ resources/views/includes/dashboard-navbar.blade.php
@@ -75,10 +75,9 @@
                     <a class="nav-link waves-effect waves-dark" href="javascript:void(0)">
                         <i class="ti-search"></i>
                     </a>
-                    <form class="app-search position-absolute">
-                        <input type="text" class="form-control" placeholder="Search &amp; enter">
-                        <a class="srh-btn"><i class="ti-close"></i></a>
-                    </form>
+                    {!! Form::open(['url' => 'search', 'class' => 'app-search position-absolute']) !!}
+                        {!! Form::text('search', null, ['class' => 'form-control', 'placeholder' => 'Pesquisar...', 'required', 'minlength' => 3, 'maxlengt' => 30]) !!}
+                    {!! Form::close() !!}
                 </li>
             </ul>
             <!-- ============================================================== -->
