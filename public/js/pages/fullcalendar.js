73fcc3719cdf262f0b332d8cff91273d7b6180f7
John <juaorok@hotmail.com>
2019-12-19 13:49:12 -0300

    Fullcalendar
    Fullcalendar

---
+++ public/js/pages/fullcalendar.js
@@ -0,0 +1,49 @@
+$(function () {
+    let today = new Date();
+    let y = today.getFullYear();
+    let m = today.getMonth();
+    let d = today.getDate();
+
+    // Default view
+    // color classes: [ fc-event-success | fc-event-info | fc-event-warning | fc-event-danger | fc-event-dark ]
+    $('#fullcalendar-default').fullCalendar({
+        // Bootstrap styling
+        themeSystem: 'bootstrap4',
+        bootstrapFontAwesome: {
+            close: ' ion ion-md-close',
+            prev: ' ion ion-ios-arrow-back scaleX--1-rtl',
+            next: ' ion ion-ios-arrow-forward scaleX--1-rtl',
+            prevYear: ' ion ion-ios-arrow-dropleft-circle scaleX--1-rtl',
+            nextYear: ' ion ion-ios-arrow-dropright-circle scaleX--1-rtl'
+        },
+        header: {
+            left: 'title',
+            center: 'month,agendaWeek,agendaDay',
+            right: 'prev,next today'
+        },
+        defaultDate: today,
+        navLinks: true, // can click day/week names to navigate views
+        selectable: true,
+        selectHelper: true,
+        weekNumbers: true, // Show week numbers
+        nowIndicator: true, // Show "now" indicator
+        firstDay: 1, // Set "Monday" as start of a week
+        editable: false,
+        draggable: false,
+        eventLimit: true, // allow "more" link when too many events
+        droppable: false,
+        events: {
+            url: '/api/calendars',
+            error: function () {
+                $('#aviso').show();
+                $('#fullcalendar-default').hide();
+            }
+        },
+        loading: function (bool) {
+            $('#loading').toggle(bool);
+        },
+        eventClick: function (event, jsEvent, view) {
+            window.location.href = '/calendars/' + event.id +'.'+ event.slug;
+        }
+    });
+});
