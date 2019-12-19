6683cdb719950dcd96ffb5cc6a8a03f0585a95f3
John <juaorok@hotmail.com>
2019-12-19 13:51:49 -0300

    Create edit.blade.php

---
+++ resources/views/site/torrents/edit.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Editar')
+
+@section('css')
+    <!-- select2 -->
+    <link href="{{ asset('vendor/select2/dist/css/select2.min.css') }}" rel="stylesheet">
+    <!-- sceditor -->
+    <link href="{{ asset('vendor/sceditor/minified/themes/default.min.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Home</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('torrents') }}">Torrents</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Editar</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">Editar</h3>
+                        @includeIf('errors.errors', [$errors])
+                        {!! Form::model($torrent, ['route' => ['update.torrent', 'id' => $torrent->id], 'method' => 'PUT',  'class' => 'form-horizontal']) !!}
+
+                        <div class="form-group">
+                            {!! Form::label('name', 'Nome:') !!}
+                            {!! Form::text('name', $torrent->name, ['class' => 'form-control', 'maxlength' => '250', 'required']) !!}
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('category_id', 'Categoria:') !!}
+                                {!! Form::select('category_id', $categories, $torrent->category_id, ['class' => 'custom-select select2']) !!}
+                            </div>
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('media_id', 'Mídia:') !!}
+                                {!! Form::select('media_id', $medias, $torrent->media_id, ['class' => 'custom-select select2']) !!}
+                            </div>
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('fansub_id', 'Fansub:') !!}
+                                {!! Form::select('fansub_id', $fansubs, $torrent->fansub_id, ['class' => 'custom-select select2']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-row">
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('allow_comments', 'Habilita comentário:') !!}
+                                {!! Form::select('allow_comments', [false => 'Não', true => 'Sim'], $torrent->allowcomments, ['class' => 'custom-select', 'required']) !!}
+
+                            </div>
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('is_freeleech', 'Free Leech:') !!}
+                                {!! Form::select('is_freeleech', [false => 'Não', true => 'Sim'], $torrent->freeleech, ['class' => 'custom-select', 'required']) !!}
+
+                            </div>
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('is_silver', 'Silver:') !!}
+                                {!! Form::select('is_silver', [false => 'Não', true => 'Sim'], $torrent->silver, ['class' => 'custom-select', 'required']) !!}
+
+                            </div>
+                            <div class="form-group col-sm-3">
+                                {!! Form::label('is_doubleup', 'Double UP:') !!}
+                                {!! Form::select('is_doubleup', [false => 'Não', true => 'Sim'], $torrent->doubleup, ['class' => 'custom-select', 'required']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('description', 'Descrição:') !!}
+                            {!! Form::textarea('description', $torrent->description) !!}
+                        </div>
+
+                        {!! Form::submit('Atulizar', ['class' => 'btn btn-primary btn-outline-primary']) !!}
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('scripts')
+    <!-- Select2 -->
+    <script src="{{ asset('vendor/select2/dist/js/select2.min.js') }}"></script>
+    <!-- sceditor -->
+    <script src="{{ asset('vendor/sceditor/minified/sceditor.min.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/minified/formats/bbcode.js') }}"></script>
+    <script src="{{ asset('vendor/sceditor/languages/pt-BR.js') }}"></script>
+
+    <!-- script -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $(document).ready(function () {
+            $('.select2').select2();
+
+            let textarea = document.getElementById('description');
+            sceditor.create(textarea, {
+                format: 'bbcode',
+                locale: 'pt-BR',
+                emoticonsRoot: '/vendor/sceditor/',
+                style: '/vendor/sceditor/minified/themes/content/default.min.css'
+            });
+        });
+    </script>
+@endsection
