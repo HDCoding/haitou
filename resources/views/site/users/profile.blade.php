8e73e7e139c6233dd0a4a88b99c500e659f0b411
John <juaorok@hotmail.com>
2019-12-22 21:04:51 -0300

    Create profile.blade.php

---
+++ resources/views/site/users/profile.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Perfil')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Home</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Perfil</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Perfil</h4>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
