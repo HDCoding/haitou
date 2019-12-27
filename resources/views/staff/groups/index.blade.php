d7c1e8feabc821372d3b007c1a64bd85153f7dfa
John <juaorok@hotmail.com>
2019-12-27 14:41:40 -0300

    Update index.blade.php

--- resources/views/staff/groups/index.blade.php
+++ resources/views/staff/groups/index.blade.php
@@ -27,7 +27,7 @@
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.groups')</h4>
                         <a href="{{ url('staff/groups/create') }}">
-                            <button type="button" class="btn btn-xs btn-outline-primary">
+                            <button type="button" class="btn btn-xs btn-primary">
                                 <span class="ion ion-md-add"></span> Adicionar
                             </button>
                         </a>
