118df20eaba65439464bccfd6f153ce542cf8acf
John <juaorok@hotmail.com>
2019-12-27 14:41:43 -0300

    Update index.blade.php

--- resources/views/staff/medias/index.blade.php
+++ resources/views/staff/medias/index.blade.php
@@ -32,7 +32,7 @@
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.medias')</h4>
                         <a href="{{ url('staff/medias/create') }}">
-                            <button type="button" class="btn btn-xs btn-outline-primary">
+                            <button type="button" class="btn btn-xs btn-primary">
                                 <span class="ion ion-md-add"></span> Adicionar
                             </button>
                         </a>
