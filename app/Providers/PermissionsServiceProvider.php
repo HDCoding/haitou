120f6928a3796cd6ecb91f16ab06d9c442983e3e
John <juaorok@hotmail.com>
2019-12-27 12:56:27 -0300

    Create PermissionsServiceProvider.php

---
+++ app/Providers/PermissionsServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Allow;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class PermissionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return bool
+     */
+    public function boot()
+    {
+        try {
+            Allow::get()->map(function ($permission) {
+                Gate::define($permission->slug, function ($user) use ($permission) {
+                    return $user->hasPermissionTo($permission);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            return false;
+        }
+    }
+}
