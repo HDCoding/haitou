c5aa348171d780a3c8addf9c8bc8c41900a25a70
John <juaorok@hotmail.com>
2019-12-27 12:56:20 -0300

    Create Allow.php

---
+++ app/Models/Allow.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Allow extends Model
+{
+    use Sluggable;
+
+	protected $table = 'allows';
+
+	protected $casts = [
+        'is_staff' => 'bool'
+    ];
+
+	protected $fillable = [
+		'name',
+		'slug',
+        'is_staff'
+	];
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
