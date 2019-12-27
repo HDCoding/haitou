448826cf5b4d869209db513d0c4fa94d4d7ac078
John <juaorok@hotmail.com>
2019-12-27 14:43:33 -0300

    Update index.blade.php

--- resources/views/staff/rules/index.blade.php
+++ resources/views/staff/rules/index.blade.php
@@ -27,7 +27,7 @@
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.rules')</h4>
                         <a href="{{ url('staff/rules/create') }}">
-                            <button type="button" class="btn btn-xs btn-outline-primary">
+                            <button type="button" class="btn btn-xs btn-primary">
                                 <span class="ion ion-md-add"></span> Adicionar
                             </button>
                         </a>
