6eeef07e6cf16302db3b7283fa606d39b55c1709
John <juaorok@hotmail.com>
2019-12-27 14:42:22 -0300

    Update buttons.blade.php

--- resources/views/site/forums/buttons.blade.php
+++ resources/views/site/forums/buttons.blade.php
@@ -1,19 +1,19 @@
 <div class="row">
     <div class="col-md-12  mt-3 mb-3">
         <div class="float-left">
-            <a href="{{ route('forum') }}" class="btn btn-sm btn-outline-success">
+            <a href="{{ route('forum') }}" class="btn btn-sm btn-success">
                 <i class="ion ion-ios-chatbubbles"></i> Fórum
             </a>
-            <a href="{{ route('forum_latest_topics') }}" class="btn btn-sm btn-outline-primary">
+            <a href="{{ route('forum_latest_topics') }}" class="btn btn-sm btn-primary">
                 <i class="fa fa-book"></i> Tópicos recentes
             </a>
-            <a href="{{ route('forum_latest_posts') }}" class="btn btn-sm btn-outline-secondary">
+            <a href="{{ route('forum_latest_posts') }}" class="btn btn-sm btn-secondary">
                 <i class="fa fa-file"></i> Últimas postagens
             </a>
-            <a href="{{ route('forum_subscriptions') }}" class="btn btn-sm btn-outline-danger">
+            <a href="{{ route('forum_subscriptions') }}" class="btn btn-sm btn-danger">
                 <i class="fa fa-heart"></i> Favoritos
             </a>
-            <a href="{{ route('forum.search') }}" class="btn btn-sm btn-outline-dark">
+            <a href="{{ route('forum.search') }}" class="btn btn-sm btn-dark">
                 <i class="fa fa-search"></i> Pesquisar
             </a>
         </div>
