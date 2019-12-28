7cf761283e7815d210001d80183bf4746ea71cf1
John <juaorok@hotmail.com>
2019-12-28 15:57:33 -0300

    Update FansubsController.php

--- app/Http/Controllers/Staff/FansubsController.php
+++ app/Http/Controllers/Staff/FansubsController.php
@@ -14,6 +14,7 @@ class FansubsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:fansubs-mod');
     }
 
     public function index()
