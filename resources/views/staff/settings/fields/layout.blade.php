9903f3f6acfbad56df302c9b59bade4b189ecd67
John <juaorok@hotmail.com>
2019-12-28 15:58:16 -0300

    Update layout.blade.php

--- resources/views/staff/settings/fields/layout.blade.php
+++ resources/views/staff/settings/fields/layout.blade.php
@@ -1,11 +1,14 @@
 <div class="form-group {{ $errors->has($field['name']) ? ' has-error' : '' }}">
-<label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
-<input type="{{ $field['type'] }}"
-       name="{{ $field['name'] }}"
-       value="{{ old($field['name'], \setting($field['name'])) }}"
-       class="form-control {{ array_get( $field, 'class') }}"
-       id="{{ $field['name'] }}"
-       placeholder="{{ $field['label'] }}">
-
-@if ($errors->has($field['name'])) <small class="help-block">{{ $errors->first($field['name']) }}</small> @endif
+    <label for="{{ $field['name'] }}">{{ $field['label'] }}</label>
+    <input type="{{ $field['type'] }}"
+            name="{{ $field['name'] }}"
+            value="{{ old($field['name'], \setting($field['name'])) }}"
+            class="form-control {{ array_get( $field, 'class') }}"
+            id="{{ $field['name'] }}"
+            placeholder="{{ $field['label'] }}">
+    @if ($errors->has($field['name']))
+        <small id="{{ $field['name'] }}" class="form-text text-muted">
+            {{ $errors->first($field['name']) }}
+        </small>
+    @endif
 </div>
