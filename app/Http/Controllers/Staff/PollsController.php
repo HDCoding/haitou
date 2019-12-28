a2c2b5ad27480b668537f3b3e88813002cb02475
John <juaorok@hotmail.com>
2019-12-28 15:57:48 -0300

    Update PollsController.php

--- app/Http/Controllers/Staff/PollsController.php
+++ app/Http/Controllers/Staff/PollsController.php
@@ -13,6 +13,7 @@ class PollsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:pesquisas-mod');
     }
 
     public function index()
