d642db6ea3ba3ce790b07ace6519182a067293ad
John <juaorok@hotmail.com>
2019-12-28 15:57:54 -0300

    Update StaffController.php

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -11,6 +11,7 @@ class StaffController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:painel-staff');
     }
 
     public function index()
