5b8b78fb9c840c95c1be29734a3f1e332c2592ff
John <juaorok@hotmail.com>
2019-12-28 15:57:44 -0300

    Update MediasController.php

--- app/Http/Controllers/Staff/MediasController.php
+++ app/Http/Controllers/Staff/MediasController.php
@@ -18,6 +18,7 @@ class MediasController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:midias-mod');
     }
 
     public function index()
