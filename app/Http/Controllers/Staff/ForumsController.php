120390b875d20db36b5039d5d74f7603cf0c6026
John <juaorok@hotmail.com>
2019-12-28 15:57:36 -0300

    Update ForumsController.php

--- app/Http/Controllers/Staff/ForumsController.php
+++ app/Http/Controllers/Staff/ForumsController.php
@@ -18,6 +18,7 @@ class ForumsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:forum-mod');
     }
 
     public function index()
