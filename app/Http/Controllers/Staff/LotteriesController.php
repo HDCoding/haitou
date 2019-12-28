dac172629f3e8d0f91d2f96c22be2423fb4d99ee
John <juaorok@hotmail.com>
2019-12-28 15:57:42 -0300

    Update LotteriesController.php

--- app/Http/Controllers/Staff/LotteriesController.php
+++ app/Http/Controllers/Staff/LotteriesController.php
@@ -9,6 +9,7 @@ class LotteriesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:sorteios-mod');
     }
 
     public function index()
