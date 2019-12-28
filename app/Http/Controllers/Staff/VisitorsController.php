9b4188164675731c69981fb2b9cc1a16868a6286
John <juaorok@hotmail.com>
2019-12-28 15:58:03 -0300

    Update VisitorsController.php

--- app/Http/Controllers/Staff/VisitorsController.php
+++ app/Http/Controllers/Staff/VisitorsController.php
@@ -9,6 +9,7 @@ class VisitorsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:visitantes-mod');
     }
 
     public function index()
