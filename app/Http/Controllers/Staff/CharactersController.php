856e6232ce932f2804ddfa7b9871360a2b241168
John <juaorok@hotmail.com>
2019-12-28 15:57:27 -0300

    Update CharactersController.php

--- app/Http/Controllers/Staff/CharactersController.php
+++ app/Http/Controllers/Staff/CharactersController.php
@@ -14,6 +14,7 @@ class CharactersController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:personagens-mod');
         $this->imageFile = new ImageUploader();
     }
 
