302ab035b5c0217ea40418e515091c50289321f0
John <juaorok@hotmail.com>
2019-12-28 15:57:35 -0300

    Update FaqsController.php

--- app/Http/Controllers/Staff/FaqsController.php
+++ app/Http/Controllers/Staff/FaqsController.php
@@ -12,6 +12,7 @@ class FaqsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:faq-mod');
     }
 
     public function index()
