b488c7450690286dce0afa00d3e760e7c5757147
John <juaorok@hotmail.com>
2019-12-28 13:00:42 -0300

    Update index.blade.php

--- resources/views/staff/settings/index.blade.php
+++ resources/views/staff/settings/index.blade.php
@@ -2,11 +2,6 @@
 
 @section('title', trans('dashboard.settings'))
 
-@section('css')
-    <!-- DataTables -->
-    <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
-@endsection
-
 @section('content')
 
     <div class="page-breadcrumb">
@@ -42,45 +37,13 @@
                         <div class="tab-content tabcontent-border">
                             <div class="tab-pane active" id="setting" role="tabpanel">
                                 @includeIf('errors.errors', [$errors])
-                                <form method="post" action="{{ route('settings.store') }}" class="form-horizontal">
-                                    @csrf
 
-                                    @if(count(config('settings', [])) )
-
-                                        @foreach(config('settings') as $section => $fields)
-                                            <div class="panel panel-info">
-                                                <div class="panel-heading">
-                                                    {{ $fields['title'] }}
-                                                </div>
-
-                                                <div class="panel-body">
-                                                    <div class="row">
-                                                        <div class="col-md-7 col-md-offset-2">
-                                                            @foreach($fields['elements'] as $field)
-                                                                @includeIf('staff.settings.fields.' . $field['type'] )
-                                                            @endforeach
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                        <!-- end panel for {{ $fields['title'] }} -->
-                                        @endforeach
-
-                                    @endif
-
-                                    <div class="row m-b-md">
-                                        <div class="col-md-12">
-                                            <button class="btn-primary btn">Salvar Configurações</button>
-                                        </div>
-                                    </div>
-                                </form>
                             </div>
                             <div class="tab-pane p-20" id="favicon" role="tabpanel">
-                                2
+                                favicon
                             </div>
                             <div class="tab-pane p-20" id="images" role="tabpanel">
-                                3
+                                images
                             </div>
                         </div>
                     </div>
@@ -89,21 +52,4 @@
         </div>
     </div>
 
-@endsection
-
-@section('scripts')
-    <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
-    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
-        $(document).ready(function () {
-            $('#datatable').DataTable({
-                "displayLength": 25,
-                "searching": true,
-                "responsive": true,
-                "order": [[ 1, "asc" ]],
-                "language": {
-                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
-                }
-            });
-        });
-    </script>
 @endsection
