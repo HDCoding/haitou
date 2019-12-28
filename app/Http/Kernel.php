1ec2078d1c8167020c3e04a828c960ffea5b3e4e
John <juaorok@hotmail.com>
2019-12-28 15:58:04 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'lockscreen' => \App\Http\Middleware\Lockscreen::class,
+        'allow' => \App\Http\Middleware\Allow::class,
     ];
 
     /**
