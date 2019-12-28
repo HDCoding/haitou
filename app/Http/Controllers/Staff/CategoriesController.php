b58511bfa604555e07026bd46a5d8bf2b626f1b3
John <juaorok@hotmail.com>
2019-12-28 15:57:26 -0300

    Update CategoriesController.php

--- app/Http/Controllers/Staff/CategoriesController.php
+++ app/Http/Controllers/Staff/CategoriesController.php
@@ -13,6 +13,7 @@ class CategoriesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:categorias-mod');
     }
 
     public function index()
