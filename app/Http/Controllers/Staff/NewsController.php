13a351b3d8da7736e12c81df4cc8586ed971c298
John <juaorok@hotmail.com>
2019-12-28 15:57:47 -0300

    Update NewsController.php

--- app/Http/Controllers/Staff/NewsController.php
+++ app/Http/Controllers/Staff/NewsController.php
@@ -11,6 +11,7 @@ class NewsController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('allow:noticias-mod');
     }
 
     public function index()
