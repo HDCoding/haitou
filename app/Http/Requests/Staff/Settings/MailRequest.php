a798b76f95972ea742b7d31ee4b708bf8568b411
John <juaorok@hotmail.com>
2019-12-28 21:04:56 -0300

    Create MailRequest.php

---
+++ app/Http/Requests/Staff/Settings/MailRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Staff\Settings;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //Mail
+            'mail_driver' => '',
+            'mail_host' => '',
+            'mail_port' => '',
+            'mail_username' => '',
+            'mail_password' => '',
+            'mail_encryption' => '',
+        ];
+    }
+}
