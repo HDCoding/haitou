1fc013abd43e13b707d90ae26b7273c20c9fecaf
John <juaorok@hotmail.com>
2019-12-28 21:04:49 -0300

    Update SettingsController.php

--- app/Http/Controllers/Staff/SettingsController.php
+++ app/Http/Controllers/Staff/SettingsController.php
@@ -3,6 +3,15 @@
 namespace App\Http\Controllers\Staff;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Staff\Settings\AnalyticsRequest;
+use App\Http\Requests\Staff\Settings\FaviconRequest;
+use App\Http\Requests\Staff\Settings\ImagesRequest;
+use App\Http\Requests\Staff\Settings\MailRequest;
+use App\Http\Requests\Staff\Settings\OthersRequest;
+use App\Http\Requests\Staff\Settings\PointsRequest;
+use App\Http\Requests\Staff\Settings\PolicyRequest;
+use App\Http\Requests\Staff\Settings\SeoRequest;
+use App\Http\Requests\Staff\Settings\SocialRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 
@@ -14,31 +23,119 @@ class SettingsController extends Controller
         $this->middleware('allow:configuracoes-mod');
     }
 
-    public function index()
+    public function analytics(AnalyticsRequest $request)
     {
-        return view('staff.settings.index');
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.analytics');
+        }
     }
 
-    public function edit($setting_id)
+    public function favicon(FaviconRequest $request)
     {
-        $setting = Setting::find($setting_id);
-        return view('staff.settings.edit', compact('setting'));
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.favicon');
+        }
     }
 
-    public function store(Request $request)
+    public function images(ImagesRequest $request)
     {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
 
-        $rules = Setting::getValidationRules();
-        $data = $this->validate($request, $rules);
-        $validSettings = array_keys($rules);
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.images');
+        }
+    }
 
+    public function mail(MailRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.mail');
+        }
+    }
+
+    public function others(OthersRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.others');
+        }
+    }
+
+    public function points(PointsRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.points');
+        }
+    }
+
+    public function policy(PolicyRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.policy');
+        }
+    }
+
+    public function seo(SeoRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.seo');
+        }
+    }
+
+    public function social(SocialRequest $request)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->except('_token');
+            $this->save($data);
+            return redirect()->back();
+        }
+        if ($request->isMethod('GET')) {
+            return view('staff.settings.social');
+        }
+    }
+
+    public function save(array $data)
+    {
         foreach ($data as $key => $val) {
-            if (in_array($key, $validSettings)) {
-                Setting::add($key, $val, Setting::getDataType($key));
-            }
+            Setting::add($key, $val);
         }
 
         toastr()->info('Configuração atualizada.', 'Sucesso');
-        return redirect()->to('staff/settings');
     }
+
 }
