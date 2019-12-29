94bc2b2f618f5c1ed0ec343d85c5d7b8f8572e17
John <juaorok@hotmail.com>
2019-12-28 21:05:35 -0300

    Create nav.blade.php

---
+++ resources/views/staff/settings/nav.blade.php
@@ -0,0 +1,14 @@
+<div class="col-lg-4 col-xl-3">
+    <!-- Nav tabs -->
+    <div class="nav flex-column nav-pills">
+        <a class="nav-link {{ ative_page('staff/settings/analytics') }}" href="{{ route('setting.analytics') }}">Analytics</a>
+        <a class="nav-link {{ ative_page('staff/settings/favicon') }}" href="{{ route('setting.favicon') }}">Favicon</a>
+        <a class="nav-link {{ ative_page('staff/settings/images') }}" href="{{ route('setting.images') }}">Imagens</a>
+        <a class="nav-link {{ ative_page('staff/settings/mail') }}" href="{{ route('setting.mail') }}">Mail</a>
+        <a class="nav-link {{ ative_page('staff/settings/others') }}" href="{{ route('setting.others') }}">Outros</a>
+        <a class="nav-link {{ ative_page('staff/settings/points') }}" href="{{ route('setting.points') }}">Pontos</a>
+        <a class="nav-link {{ ative_page('staff/settings/policy') }}" href="{{ route('setting.policy') }}">Políticas</a>
+        <a class="nav-link {{ ative_page('staff/settings/seo') }}" href="{{ route('setting.seo') }}">SEO</a>
+        <a class="nav-link {{ ative_page('staff/settings/social') }}" href="{{ route('setting.social') }}">Social</a>
+    </div>
+</div>
