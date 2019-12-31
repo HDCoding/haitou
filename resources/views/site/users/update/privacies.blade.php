e3a1124423303cfd21ca5f3183402bc66cecdccf
John <juaorok@hotmail.com>
2019-12-30 21:59:28 -0300

    Update privacies.blade.php

--- resources/views/site/users/update/privacies.blade.php
+++ resources/views/site/users/update/privacies.blade.php
@@ -33,179 +33,107 @@
                             @includeIf('errors.errors', [$errors])
                             {!! Form::open(['url' => 'user/edit/privacy', 'class' => 'form-horizontal', 'autocomplete' => 'off', 'id' => 'privacy', 'name' => 'privacy']) !!}
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_achievements', 1, auth()->user()->show_achievements, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Consquistas</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_achievements">
+                                    {!! Form::checkbox('show_achievements', 1, auth()->user()->show_achievements, ['class' => 'custom-control-input', 'id' => 'show_achievements']) !!}
+                                    <span class="custom-control-label">Exibir Consquistas</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_mood', 1, auth()->user()->show_mood, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Humor</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_mood">
+                                    {!! Form::checkbox('show_mood', 1, auth()->user()->show_mood, ['class' => 'custom-control-input', 'id' => 'show_mood']) !!}
+                                    <span class="custom-control-label">Exibir Humor</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_state', 1, auth()->user()->show_state, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir UF/Estado</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_state">
+                                    {!! Form::checkbox('show_state', 1, auth()->user()->show_state, ['class' => 'custom-control-input', 'id' => 'show_state']) !!}
+                                    <span class="custom-control-label">Exibir UF</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_role', 1, auth()->user()->show_role, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Grupo</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_group">
+                                    {!! Form::checkbox('show_group', 1, auth()->user()->show_group, ['class' => 'custom-control-input', 'id' => 'show_group']) !!}
+                                    <span class="custom-control-label">Exibir Grupo</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_downloaded', 1, auth()->user()->show_downloaded, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Donwloaded</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_downloaded">
+                                    {!! Form::checkbox('show_downloaded', 1, auth()->user()->show_downloaded, ['class' => 'custom-control-input', 'id' => 'show_downloaded']) !!}
+                                    <span class="custom-control-label">Exibir Donwloaded</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_uploaded', 1, auth()->user()->show_uploaded, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Uploaded</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_uploaded">
+                                    {!! Form::checkbox('show_uploaded', 1, auth()->user()->show_uploaded, ['class' => 'custom-control-input', 'id' => 'show_uploaded']) !!}
+                                    <span class="custom-control-label">Exibir Uploaded</span>
                                 </label>
                             </div>
                             <hr class="border-light m-0">
                             <h6 class="mb-4 mt-4">Perfil</h6>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile', 1, auth()->user()->show_profile, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Perfil Privado</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile">
+                                    {!! Form::checkbox('show_profile', 1, auth()->user()->show_profile, ['class' => 'custom-control-input', 'id' => 'show_profile']) !!}
+                                    <span class="custom-control-label">Perfil Privado</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_points', 1, auth()->user()->show_profile_points, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil pontos</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_points">
+                                    {!! Form::checkbox('show_profile_points', 1, auth()->user()->show_profile_points, ['class' => 'custom-control-input', 'id' => 'show_profile_points']) !!}
+                                    <span class="custom-control-label">Exibir pontos</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_level', 1, auth()->user()->show_profile_level, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil level</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_level">
+                                    {!! Form::checkbox('show_profile_level', 1, auth()->user()->show_profile_level, ['class' => 'custom-control-input', 'id' => 'show_profile_level']) !!}
+                                    <span class="custom-control-label">Exibir level</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_info', 1, auth()->user()->show_profile_info, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil info</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_info">
+                                    {!! Form::checkbox('show_profile_info', 1, auth()->user()->show_profile_info, ['class' => 'custom-control-input', 'id' => 'show_profile_info']) !!}
+                                    <span class="custom-control-label">Exibir info</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_title', 1, auth()->user()->show_profile_title, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil titulo</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_title">
+                                    {!! Form::checkbox('show_profile_title', 1, auth()->user()->show_profile_title, ['class' => 'custom-control-input', 'id' => 'show_profile_title']) !!}
+                                    <span class="custom-control-label">Exibir titulo</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_signature', 1, auth()->user()->show_profile_signature, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil assinatura</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_signature">
+                                    {!! Form::checkbox('show_profile_signature', 1, auth()->user()->show_profile_signature, ['class' => 'custom-control-input', 'id' => 'show_profile_signature']) !!}
+                                    <span class="custom-control-label">Exibir assinatura</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_birthday', 1, auth()->user()->show_profile_birthday, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil aniversario</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_birthday">
+                                    {!! Form::checkbox('show_profile_birthday', 1, auth()->user()->show_profile_birthday, ['class' => 'custom-control-input', 'id' => 'show_profile_birthday']) !!}
+                                    <span class="custom-control-label">Exibir aniversario</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_social_links', 1, auth()->user()->show_profile_social_links, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil links sociais</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_social_links">
+                                    {!! Form::checkbox('show_profile_social_links', 1, auth()->user()->show_profile_social_links, ['class' => 'custom-control-input', 'id' => 'show_profile_social_links']) !!}
+                                    <span class="custom-control-label">Exibir links sociais</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_friends', 1, auth()->user()->show_profile_friends, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil amigos</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_profile_warning">
+                                    {!! Form::checkbox('show_profile_warning', 1, auth()->user()->show_profile_warning, ['class' => 'custom-control-input', 'id' => 'show_profile_warning']) !!}
+                                    <span class="custom-control-label">Exibir advertencia</span>
                                 </label>
                             </div>
                             <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_profile_warning', 1, auth()->user()->show_profile_warning, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir perfil icone advertencia</span>
+                                <label class="custom-control custom-checkbox form-check-inline" for="show_forum_signatures">
+                                    {!! Form::checkbox('show_forum_signatures', 1, auth()->user()->show_forum_signatures, ['class' => 'custom-control-input', 'id' => 'show_forum_signatures']) !!}
+                                    <span class="custom-control-label">Exibir Forum Assinaturas</span>
                                 </label>
                             </div>
-                            <div class="form-group">
-                                <label class="switcher">
-                                    {!! Form::checkbox('show_forum_signatures', 1, auth()->user()->show_forum_signatures, ['class' => 'switcher-input']) !!}
-                                    <span class="switcher-indicator">
-                                        <span class="switcher-yes"></span>
-                                        <span class="switcher-no"></span>
-                                    </span>
-                                    <span class="switcher-label">Exibir Forum Assinaturas</span>
-                                </label>
+                            <div class="mt-5">
+                                <button type="submit" class="btn btn-primary btn-rounded">Salvar alterações</button>&nbsp;
+                                <button type="reset" class="btn btn-default btn-rounded">Cancelar</button>
                             </div>
-                            <button type="submit" class="btn btn-primary">Salvar alterações</button>&nbsp;
-                            <button type="button" class="btn btn-default">Cancelar</button>
                             {!! Form::close() !!}
                         </div>
                     </div>
