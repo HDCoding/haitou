0ad0d2fa549c583fd9c3bef751ef0b6a04737182
John <juaorok@hotmail.com>
2019-12-31 12:40:52 -0300

    Create GiveAllPermissionsToOneUser.php

---
+++ app/Console/Commands/GiveAllPermissionsToOneUser.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Allow;
+use App\Models\UserAllow;
+use Illuminate\Console\Command;
+
+class GiveAllPermissionsToOneUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'give:permissions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Give all permissions to User ID: 3';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $allows = Allow::all();
+
+        foreach ($allows as $allow) {
+            UserAllow::create(['user_id' => 3, 'allow_id' => $allow->id]);
+        }
+    }
+}
