047850e82837f07673d4681e34cf66a87ab5e008
John <juaorok@hotmail.com>
2020-01-01 19:47:13 -0300

    Update torrent.blade.php

--- resources/views/site/torrents/torrent.blade.php
+++ resources/views/site/torrents/torrent.blade.php
@@ -44,7 +44,7 @@
                         <div class="row">
                             <div class="col-lg-3 col-md-3 col-sm-6">
                                 <div class="white-box text-center">
-                                    <img class="img-responsive" src="{{ $torrent->media->poster }}" alt="Poster">
+                                    <img class="img-responsive" src="{{ $torrent->media->poster }}" alt="Poster" width="300px">
                                 </div>
                             </div>
                             <div class="col-lg-9 col-md-9 col-sm-6">
