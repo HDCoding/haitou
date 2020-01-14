75a0d2f0e9334cee2c5c57853d52af787d3bc0c1
John <juaorok@hotmail.com>
2020-01-14 11:46:00 -0300

    Create AutoPreHitAndRun.php

---
+++ app/Console/Commands/AutoPreHitAndRun.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Historic;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class AutoPreHitAndRun extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'haitou:auto-pre-hit-and-run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Automatically Sends Pre Warning PM To Users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if (setting('rnh_on') == true) {
+            $current = new Carbon();
+            $historics = Historic::with(['user', 'torrent'])
+                ->where('real_downloaded', '>', 0)
+                ->where('is_released', '=', 0)
+                ->where('is_active', '=', 0)
+                ->where('seed_time', '<', 201600)
+                ->where('updated_at', '<', $current->copy()->subDays(3)->toDateTimeString())
+                ->get();
+
+            foreach ($historics as $historic) {
+                //checa se o usuario tem direitos de VIP
+                if (!$historic->user->vips->where('user_id', '=', $historic->user->id)->first()) {
+                    if ($historic->real_downloaded > ($historic->torrent->size * 3 / 100)) {
+                        //TODO finish this
+                    }
+                }
+            }
+        }
+    }
+}
