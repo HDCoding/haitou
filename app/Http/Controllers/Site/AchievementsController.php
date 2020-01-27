36430146c2883e92c2d342a0f46de42dbdc537f4
John <juaorok@hotmail.com>
2020-01-27 17:25:29 -0300

    Update AchievementsController.php

--- app/Http/Controllers/Site/AchievementsController.php
+++ app/Http/Controllers/Site/AchievementsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
@@ -14,10 +15,32 @@ class AchievementsController extends Controller
 
     public function index(Request $request)
     {
+        //Logged user
         $user = $request->user();
-        $achievements = $user->achievements;
-        $unlocked = $user->unlockedAchievements()->count();
 
-        return view('site.users.achievements', compact('achievements', 'unlocked'));
+        // For Cache
+        $expire_at = Carbon::now()->addMinutes(20);
+
+        // Unlocked
+        $achievements = cache()->remember('achievements_'.$user->id, $expire_at, function () use ($user) {
+            return $user->unlockedAchievements();
+        });
+
+        //Pending
+        $pending = cache()->remember('pending_achievement_'.$user->id, $expire_at, function () use ($user) {
+            return $user->inProgressAchievements();
+        });
+
+        //Count unlocked
+        $unlocked = cache()->remember('unlocked_achievement_'.$user->id, $expire_at, function () use ($user) {
+            return $user->unlockedAchievements()->count();
+        });
+
+        //Count locked
+        $locked = cache()->remember('locked_achievement_'.$user->id, $expire_at, function () use ($user) {
+            return $user->lockedAchievements()->count();
+        });
+
+        return view('site.users.achievements', compact('achievements', 'pending', 'unlocked', 'locked'));
     }
 }
