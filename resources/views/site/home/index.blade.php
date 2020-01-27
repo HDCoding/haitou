b91a33ffd03a662bb1fb9b224503559b26e9814c
John <juaorok@hotmail.com>
2020-01-27 17:26:27 -0300

    Update index.blade.php

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -21,129 +21,31 @@
             <div class="col-sm-12 col-lg-12">
                 <div class="card vegas-fixed-background" id="home-cover">
                     <div class="card-body py-5 my-5">
-{{--                        <h4 class="text-center text-dark">Texto</h4>--}}
+                        <h2 class="text-center text-primary">Okaeri {{ auth()->user()->username }}-chan</h2>
                     </div>
                 </div>
             </div>
         </div>
 
         <!-- News -->
-        <div class="card">
-            <div class="card-header">
-                Últimas notícias
-                <div class="card-actions">
-                    <a class="" data-action="collapse"><i class="ti-minus"></i></a>
-                </div>
-            </div>
-            <div class="card-body collapse show">
-                @forelse($news as $new)
-                    <p class="card-text">
-                        {{ link_to_route('read.news', $new->name, ['id' => $new->id, 'slug' => $new->slug]) }}
-                        &nbsp;
-                        {{ format_date($new->created_at) }}
-                    </p>
-                @empty
-                    <p class="card-text">Nenhum conteúdo informativo no momento.</p>
-                @endforelse
-            </div>
-        </div>
+        @include('site.home.block_news')
         <!-- End News -->
 
-        <!-- Others -->
-        <div class="row">
-            <div class="col-sm-12 col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Últimos Tópicos</h4>
-                        <table class="table v-middle">
-                            <thead>
-                            <tr>
-                                <th class="border-top-0">Fórum</th>
-                                <th class="border-top-0">Tópico</th>
-                                <th class="border-top-0">Autor(a)</th>
-                                <th class="border-top-0">Data</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($topics as $topic)
-                                @if($topic->viewable())
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('forum.topics', ['id' => $topic->forum->id, 'slug' => $topic->slug]) }}">
-                                                {{ $topic->forum->name }}
-                                            </a>
-                                        </td>
-                                        <td>
-                                            <a href="{{ route('forum.topic', ['id' => $topic->id, 'slug' => $topic->slug]) }}">
-                                                {{ $topic->name }}
-                                            </a>
-                                        </td>
-                                        <td>{{ $topic->first_post_username }}</td>
-                                        <td>{{ $topic->created_at->diffForHumans() }}</td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <!-- Last Topics -->
+        @include('site.home.block_last_topics')
+        <!-- End Last Topics -->
 
-            <div class="col-sm-12 col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Últimas Postagens</h4>
-                        <table class="table v-middle">
-                            <thead>
-                            <tr>
-                                <th class="border-top-0">Post</th>
-                                <th class="border-top-0">Tópico</th>
-                                <th class="border-top-0">Autor(a)</th>
-                                <th class="border-top-0">Data</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($posts as $post)
-                                @if ($post->topic->viewable())
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('forum.topic', ['id' => $post->topic->id, 'slug' => $post->topic->slug]) }}?page={{ $post->pageNumber() }}#post-{{ $post->id }}">{{ preg_replace('#\[[^\]]+\]#', '', str_limit(htmlspecialchars_decode($post->content), 40)) }}</a>
-                                        </td>
-                                        <td>{{ $post->topic->name }}</td>
-                                        <td>{{ $post->post_username }}</td>
-                                        <td>{{ $post->updated_at->diffForHumans() }}</td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+        <!-- Last Post -->
+        @include('site.home.block_last_posts')
+        <!-- End Last Post -->
 
-            <div class="col-sm-12 col-lg-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Últimas Pesquisas</h4>
-                        <table class="table v-middle">
-                            <thead>
-                            <tr>
-                                <th class="border-top-0">Pergunta</th>
-                                <th class="border-top-0">Votos</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($polls as $poll)
-                                <td>{{ link_to_route('site.poll.show', $poll->name, ['id' => $poll->id, 'slug' => $poll->slug]) }}</td>
-                                <td>{{ $poll->totalVotes() }}</td>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End Others -->
+        <!-- Last Poll -->
+        @include('site.home.block_last_polls')
+        <!-- End Last Poll -->
+
+        <!-- Last Users Online -->
+        @include('site.home.block_last_users_online')
+        <!-- End Last Users Online -->
 
     </div>
 
