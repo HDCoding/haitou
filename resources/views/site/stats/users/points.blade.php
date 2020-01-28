4f9a1eea34ef96594065410675f38dc51eb73e9d
John <juaorok@hotmail.com>
2020-01-28 17:54:44 -0300

    Create points.blade.php

---
+++ resources/views/site/stats/users/points.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Top Pontos')
+
+@section('content')
+
+    <div class="page-breadcrumb">
+        <div class="row">
+            <div class="col-5 align-self-center">
+                <h4 class="page-title">Home</h4>
+                <div class="d-flex align-items-center">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('statistics') }}">Estatísticas</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Top Pontos</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Top Pontos</h4>
+                        @include('site.stats.users.block_user_menu')
+                        <div class="table-responsive m-t-15">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Membro</th>
+                                    <th>Pontos</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($points as $key => $point)
+                                    <tr>
+                                        <td>{{ ++$key }}</td>
+                                        <td {{ auth()->user()->username == $point->username ? 'class=bg-success' : '' }}>
+                                            @if ($point->show_profile == false)
+                                                <span class="badge badge-pill badge-light font-weight-bold">
+                                                    <span class="text-orange">
+                                                        <i class="fa fa-eye-slash" aria-hidden="true"></i>
+                                                        Escondido
+                                                    </span>
+                                                    @if (auth()->user()->id == $point->id || auth()->user()->can('usuarios-mod'))
+                                                        <a href="{{ route('user.profile', ['slug' => $point->slug]) }}">({{ $point->username }})</a>
+                                                    @endif
+                                                 </span>
+                                            @else
+                                                <span class="badge badge-pill badge-light font-weight-bold">
+                                                    <a href="{{ route('user.profile', ['slug' => $point->slug]) }}">{{ $point->username }}</a>
+                                                </span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <span class="text-success">{{ $point->points() }}</span>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
