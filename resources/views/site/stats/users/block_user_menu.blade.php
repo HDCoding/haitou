032baffe8c4d65e1f6e9505ce069aaf1609d011b
John <juaorok@hotmail.com>
2020-01-28 17:54:30 -0300

    Create block_user_menu.blade.php

---
+++ resources/views/site/stats/users/block_user_menu.blade.php
@@ -0,0 +1,11 @@
+<ul class="nav nav-tabs">
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/uploaded') }}" href="{{ route('stats.uploaded') }}"><span class="hidden-sm-up"><i class="fas fa-upload"></i></span> <span class="hidden-xs-down">Top Uploaders</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/downloaded') }}" href="{{ route('stats.downloaded') }}"><span class="hidden-sm-up"><i class="fas fa-download"></i></span> <span class="hidden-xs-down">Top Downloaders</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/seeders') }}" href="{{ route('stats.seeders') }}"><span class="hidden-sm-up"><i class="fas fa-arrow-up"></i></span> <span class="hidden-xs-down">Top Seeders</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/leechers') }}" href="{{ route('stats.leechers') }}"><span class="hidden-sm-up"><i class="fas fa-arrow-down"></i></span> <span class="hidden-xs-down">Top Leechers</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/uploaders') }}" href="{{ route('stats.uploaders') }}"><span class="hidden-sm-up"><i class="fas fa-file-upload"></i></span> <span class="hidden-xs-down">Top Upers</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/points') }}" href="{{ route('stats.points') }}"><span class="hidden-sm-up"><i class="fas fa-file-upload"></i></span> <span class="hidden-xs-down">Top Pontos</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/levels') }}" href="{{ route('stats.levels') }}"><span class="hidden-sm-up"><i class="fas fa-file-upload"></i></span> <span class="hidden-xs-down">Top Level</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/seedtime') }}" href="{{ route('stats.seedtime') }}"><span class="hidden-sm-up"><i class="fas fa-clock"></i></span> <span class="hidden-xs-down">Seed Time</span></a> </li>
+    <li class="nav-item"> <a class="nav-link {{ ative_page('statistics/user/seedsize') }}" href="{{ route('stats.seedsize') }}"><span class="hidden-sm-up"><i class="ti-email"></i></span> <span class="hidden-xs-down">Seed Size</span></a> </li>
+</ul>
