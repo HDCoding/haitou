c6419b124937d50445c7efe2e33789361a2bf909
John <juaorok@hotmail.com>
2020-01-31 17:51:25 -0300

    Update account.blade.php

--- resources/views/site/users/update/account.blade.php
+++ resources/views/site/users/update/account.blade.php
@@ -5,6 +5,8 @@
 @section('css')
     <!-- DateTimePicker -->
     <link href="{{ asset('vendor/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}" rel="stylesheet"/>
+    <!-- Vegas -->
+    <link href="{{ asset('vendor/vegas/vegas.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -27,8 +29,25 @@
 
     <div class="container-fluid">
         <div class="row">
-            @include('site.users.update.card')
-            <!-- Column -->
+            <div class="col-sm-12 col-lg-12">
+                <div class="card vegas-fixed-background" id="user-cover" style="height: 400px">
+                    <div class="card-body py-5 my-5">
+                        {!! Form::open(['route' => 'post.cover', 'files' => true, 'class' => 'form-horizontal', 'id' => 'form-cover']) !!}
+                        <h2 class="text-center text-primary">
+                            <button class="btn btn-outline-light waves-effect waves-light" type="button" onclick="chooseCover()">
+                                <span class="btn-label"><i class="fa fa-pencil-alt"></i></span>
+                            </button>
+                        </h2>
+                        <input type="file" id="coverInput" name="cover" class="form-control" accept="image/*" style="display: none"/>
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+        @include('site.users.update.card')
+        <!-- Column -->
             <div class="col-lg-8 col-xlg-9 col-md-7">
                 <div class="card">
                     @include('site.users.update.links')
@@ -36,7 +55,7 @@
                         <div class="card-body">
                             <h4 class="card-title">Editar Conta</h4>
                             @includeIf('errors.errors', [$errors])
-                            {!! Form::model(['url' => 'user/edit/account', 'class' => 'form-horizontal', 'autocomplete' => 'off', 'id' => 'account']) !!}
+                            {!! Form::open(['route' => 'post.profile', 'class' => 'form-horizontal']) !!}
                             <div class="form-group">
                                 {!! Form::label('state_id', 'Estado:', ['class' => 'form-label']) !!}
                                 {!! Form::select('state_id', $states, auth()->user()->state_id, ['class' => 'custom-select']) !!}
@@ -44,14 +63,6 @@
                             <div class="form-group">
                                 {!! Form::label('mood_id', 'Humor:', ['class' => 'form-label']) !!}
                                 {!! Form::select('mood_id', $moods, auth()->user()->mood_id, ['class' => 'custom-select']) !!}
-                            </div>
-                            <div class="form-group">
-                                {!! Form::label('avatar', 'Avatar:', ['class' => 'form-label']) !!}
-                                {!! Form::text('avatar', auth()->user()->avatar, ['class' => 'form-control']) !!}
-                            </div>
-                            <div class="form-group">
-                                {!! Form::label('cover', 'Cover:', ['class' => 'form-label']) !!}
-                                {!! Form::text('cover', auth()->user()->cover, ['class' => 'form-control']) !!}
                             </div>
                             <div class="form-group">
                                 {!! Form::label('birthday', 'Nascimento:', ['class' => 'form-label']) !!}
@@ -79,17 +90,49 @@
 
 @section('scripts')
     <!-- DateTimePicker -->
-        <script src="{{ asset('vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
-        <script
-            src="{{ asset('vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap-datepicker/dist/locales/bootstrap-datepicker.pt-BR.min.js') }}"></script>
+    <!-- VegasJS -->
+    <script src="{{ asset('vendor/vegas/vegas.js') }}"></script>
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         <!-- datepicker -->
-        <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
-            $(document).ready(function () {
-                $('#birthday').datepicker({
-                    format: 'yyyy-mm-dd',
-                    language: 'pt-BR',
-                    autoclose: true
-                });
+        $(document).ready(function () {
+            $('#birthday').datepicker({
+                format: 'yyyy-mm-dd',
+                language: 'pt-BR',
+                autoclose: true
             });
-        </script>
+        });
+        $(function() {
+            // Fixed bg
+            $('#user-cover').vegas({
+                overlay: false,
+                timer: false,
+                shuffle: true,
+                slides: [
+                    { src: "{{ auth()->user()->cover() }}" },
+                ],
+                transition: ['fade', 'zoomOut', 'zoomIn', 'blur'],
+                animation: ['kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight']
+            });
+        });
+    </script>
+
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        function chooseAvatar() {
+            $("#avatarInput").click();
+        }
+        function chooseCover() {
+            $("#coverInput").click();
+        }
+        $(document).ready(function () {
+            $('#avatarInput').on('change', function () {
+                $('#form-avatar').submit();
+            });
+            $('#coverInput').on('change', function () {
+                $('#form-cover').submit();
+            });
+        });
+    </script>
+
 @endsection
