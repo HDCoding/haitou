98a9a631a1e4395e605249975572184ba1019ae9
John <juaorok@hotmail.com>
2020-02-01 19:20:46 -0300

    Create block_buttons.blade.php

---
+++ resources/views/site/bonus/block_buttons.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-md-12 mt-3 mb-3">
+        <div class="float-left">
+            <a href="{{ route('bonus') }}" class="btn btn-sm btn-success btn-rounded">
+                <i class="ion ion-ios-chatbubbles"></i> Bônus
+            </a>
+            <a href="{{ route('bonus.stats') }}" class="btn btn-sm btn-primary btn-rounded">
+                <i class="fa fa-book"></i> Pontos
+            </a>
+            <a href="{{ route('bonus.gifts') }}" class="btn btn-sm btn-secondary btn-rounded">
+                <i class="fa fa-file"></i> Presentes
+            </a>
+            <a href="{{ route('bonus.donates') }}" class="btn btn-sm btn-danger btn-rounded">
+                <i class="fa fa-heart"></i> Doar
+            </a>
+        </div>
+    </div>
+</div>
