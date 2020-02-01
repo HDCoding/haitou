da5c648489ad2259149d7a2820c0ac5f5aaedbcf
John <juaorok@hotmail.com>
2020-02-01 10:16:16 -0300

    Update links.blade.php

--- resources/views/site/users/update/links.blade.php
+++ resources/views/site/users/update/links.blade.php
@@ -1,22 +1,22 @@
 <!-- Tabs -->
 <ul class="nav nav-pills custom-pills" id="pills-tab" role="tablist">
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/account') }}" href="{{ url('user/edit/account') }}">Geral</a>
+        <a class="nav-link {{ active_page('user/edit/account') }}" href="{{ url('user/edit/account') }}">Geral</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/password') }}" href="{{ url('user/edit/password') }}">Senha</a>
+        <a class="nav-link {{ active_page('user/edit/password') }}" href="{{ url('user/edit/password') }}">Senha</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/privacy') }}" href="{{ url('user/edit/privacy') }}">Privacidade</a>
+        <a class="nav-link {{ active_page('user/edit/privacy') }}" href="{{ url('user/edit/privacy') }}">Privacidade</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/social') }}" href="{{ url('user/edit/social') }}">Social e Info</a>
+        <a class="nav-link {{ active_page('user/edit/social') }}" href="{{ url('user/edit/social') }}">Social e Info</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/passkey') }}" href="{{ url('user/edit/passkey') }}">Passkey</a>
+        <a class="nav-link {{ active_page('user/edit/passkey') }}" href="{{ url('user/edit/passkey') }}">Passkey</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link {{ ative_page('user/edit/email') }}" href="{{ url('user/edit/email') }}">E-mail</a>
+        <a class="nav-link {{ active_page('user/edit/email') }}" href="{{ url('user/edit/email') }}">E-mail</a>
     </li>
 </ul>
 <!-- Tabs -->
