e3366413639b43ae2c862703552fb1c06c6c995b
John <juaorok@hotmail.com>
2020-02-01 19:20:40 -0300

    Update BonusTableSeeder.php

--- database/seeds/BonusTableSeeder.php
+++ database/seeds/BonusTableSeeder.php
@@ -14,8 +14,16 @@ class BonusTableSeeder extends Seeder
     {
         $bonus = [
             [
+                'name' => 'Doou seu pontos',
+                'cost'=> 0,
+                'quantity' => 0,
+                'bonus_type'=> 0,
+                'bytes' => 0,
+                'is_enabled' => false,
+                'deleted_at' => now()
+            ],
+            [
                 'name' => '1GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um Crédito de Upload. Os pontos são então removidos do seu Banco de Bônus e o crédito é adicionado ao o seu montante total carregado.',
                 'cost'=> 275,
                 'quantity' => 1073741824,
                 'bonus_type'=> 1,
@@ -24,7 +32,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '2.5GB Uploaded',
-                'description' => 'Com pontos de bónus suficientes adquiridos, pode trocá-los por um Crédito de Upload Os pontos são então retirados do seu Banco de Bónus e o crédito é adicionado ao seu montante total carregado.',
                 'cost'=> 350,
                 'quantity' => 2684354560,
                 'bonus_type'=> 1,
@@ -33,7 +40,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '5GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um Crédito de Upload. Os pontos são removidos do seu Banco de Bônus e o crédito é adicionado ao seu total de upload.',
                 'cost'=> 550,
                 'quantity' => 5368709120,
                 'bonus_type'=> 1,
@@ -42,7 +48,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '10GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um Crédito de Upload Os pontos são então retirados do seu Banco de Bónus e o crédito é adicionado ao seu montante total carregado.',
                 'cost'=> 1000,
                 'quantity' => 10737418240,
                 'bonus_type'=> 1,
@@ -51,7 +56,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '25GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um Crédito de Upload. Os pontos são removidos do seu Banco de Bônus e o crédito é adicionado ao seu valor total de upload.',
                 'cost'=> 2000,
                 'quantity' => 26843545600,
                 'bonus_type'=> 1,
@@ -60,7 +64,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '50GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você poderá trocá-los por um crédito de upload. Os pontos são então retirados do seu Banco de Bónus e o crédito é adicionado ao seu montante total carregado.',
                 'cost'=> 4000,
                 'quantity' => 53687091200,
                 'bonus_type'=> 1,
@@ -69,7 +72,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '100GB Uploaded',
-                'description' => 'Com o suficiente pontos de bônus adquiridos, você pode trocá-los por um Crédito de Upload. Os pontos são removidos do seu Banco de Bônus e o crédito é adicionado ao seu total de upload.',
                 'cost'=> 8000,
                 'quantity' => 107374182400,
                 'bonus_type'=> 1,
@@ -78,7 +80,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '520GB Uploaded',
-                'description' => 'Com pontos de bônus suficientes, você pode trocá-los por um Crédito de Upload. Os pontos são removidos do seu Banco de Bônus e o crédito é adicionado ao seu valor total de upload.',
                 'cost'=> 40000,
                 'quantity' => 558345748480,
                 'bonus_type'=> 1,
@@ -87,7 +88,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '1TB Uploaded',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um Crédito de Upload. Os pontos são então removidos do seu Banco de Bónus e o crédito é adicionado ao seu montante total carregado.',
                 'cost'=> 80000,
                 'quantity' => 1099511627776,
                 'bonus_type'=> 1,
@@ -96,7 +96,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '3 Convites',
-                'description' => 'Com pontos de bônus suficientes, você pode trocá-los por alguns convites. Os pontos são removidos do seu Banco de Bônus e os convites são adicionados aos seus convites. unt.',
                 'cost'=> 650,
                 'quantity' => 3,
                 'bonus_type'=> 4,
@@ -105,7 +104,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => 'Remover Aviso',
-                'description' => 'Com pontos de bônus suficientes adquiridos... Então você foi desobediente... tsk tsk: P Sim, agora para o preço Low Low de apenas 1000 pontos você pode ter esse aviso tirado lol.!',
                 'cost'=> 1000,
                 'quantity' => 1000,
                 'bonus_type'=> 3,
@@ -114,7 +112,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '1GB Download Removal',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um crédito de download Remoção: Os pontos são removidos do seu Banco de Bônus e o crédito de download é removido do seu total de downloads.',
                 'cost'=> 150,
                 'quantity' => 1073741824,
                 'bonus_type'=> 0,
@@ -123,7 +120,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '2.5GB Download Removal',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você poderá trocá-los por uma Remoção de Crédito de Download. Os pontos serão removidos do seu Banco de Bônus e o crédito de download será removido do seu valor total de download.',
                 'cost'=> 300,
                 'quantity' => 2684354560,
                 'bonus_type'=> 0,
@@ -132,7 +128,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '5GB Download Removal',
-                'description' => 'Com pontos de bônus suficientes, você pode trocá-los por uma Remoção de Crédito de Download. Os pontos são removidos do seu Banco de Bônus e o crédito de download é removido do seu total de download.',
                 'cost'=> 500,
                 'quantity' => 5368709120,
                 'bonus_type'=> 0,
@@ -140,8 +135,7 @@ class BonusTableSeeder extends Seeder
                 'is_enabled' => true
             ],
             [
-                'name' => 'Freeleech por 1 ano',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por um ano para o Freelech. Os pontos são removidos do seu Banco de Bônus e a freeleech será ativada em sua conta.',
+                'name' => 'VIP por 1 ano',
                 'cost'=> 100000,
                 'quantity' => 365,
                 'bonus_type'=> 2,
@@ -150,7 +144,6 @@ class BonusTableSeeder extends Seeder
             ],
             [
                 'name' => '+3 Slots',
-                'description' => 'Com pontos de bônus suficientes adquiridos, você pode trocá-los por alguns Slots Freeleech. Os pontos são então removidos do seu Banco de Bônus e os slots são adicionados ao seu quantidade de slots grátis.',
                 'cost'=> 1000,
                 'quantity' => 3,
                 'bonus_type'=> 5,
