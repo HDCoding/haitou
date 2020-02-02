7c58b8c5fbeca531d298693ef6324c28463f8517
John <juaorok@hotmail.com>
2020-02-01 23:27:42 -0300

    Update TrafficsController.php

--- app/Http/Controllers/Staff/TrafficsController.php
+++ app/Http/Controllers/Staff/TrafficsController.php
@@ -24,25 +24,25 @@ class TrafficsController extends Controller
 
     public function hourly()
     {
-        $hourly = $this->vnstat->hourly('eth1');
+        $hourly = $this->vnstat->hourly('ens4');
         return view('staff.traffics.hourly', compact('hourly'));
     }
 
     public function daily()
     {
-        $daily = $this->vnstat->daily('eth1');
+        $daily = $this->vnstat->daily('ens4');
         return view('staff.traffics.daily', compact('daily'));
     }
 
     public function monthly()
     {
-        $monthly = $this->vnstat->monthly('eth1');
+        $monthly = $this->vnstat->monthly('ens4');
         return view('staff.traffics.monthly', compact('monthly'));
     }
 
     public function topten()
     {
-        $topten = $this->vnstat->topten('eth1');
+        $topten = $this->vnstat->topten('ens4');
         return view('staff.traffics.top10', compact('topten'));
     }
 }
