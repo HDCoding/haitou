b43740f83be8ef051745ff6ec466d28bc0349579
John <juaorok@hotmail.com>
2020-02-01 22:49:13 -0300

    Update GitUpdater.php

--- app/Console/Commands/GitUpdater.php
+++ app/Console/Commands/GitUpdater.php
@@ -225,7 +225,6 @@ class GitUpdater extends Command
 
         $this->commands([
             'git add .',
-            'git checkout origin/master -- package-lock.json',
             'git checkout origin/master -- composer.lock',
         ]);
     }
@@ -326,7 +325,6 @@ class GitUpdater extends Command
 
         $additional = [
             '.env',
-            'laravel-echo-server.json',
         ];
 
         return array_merge($paths, $additional);
