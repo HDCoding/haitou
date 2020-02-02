eec95745205ada54ff4e267501bd17db815f5fb9
John <juaorok@hotmail.com>
2020-02-02 14:53:49 -0300

    Create FastPostRequest.php

---
+++ app/Http/Requests/Forum/FastPostRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Forum;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FastPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'content' => 'required|string|min:1|max:65500'
+        ];
+    }
+}
