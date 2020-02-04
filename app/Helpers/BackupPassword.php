7d7d0bd4e4323563a15c6660d96db71860219ca9
John <juaorok@hotmail.com>
2020-02-04 09:55:40 -0300

    Create BackupPassword.php

---
+++ app/Helpers/BackupPassword.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Helpers;
+
+use PhpZip\ZipFile;
+use PhpZip\Exception\ZipException;
+
+class BackupPassword
+{
+    /**
+     * Path to .zip-file.
+     *
+     * @var string
+     */
+    public $path;
+
+    /**
+     * Read the .zip, apply password and encryption, then rewrite the file.
+     *
+     * @param string $path the path to the .zip-file
+     */
+    public function __construct(string $path)
+    {
+        consoleOutput()->info('Aplicando senha e criptografia ao zip...');
+
+        // Create a new zip, add the zip from spatie/backup, encrypt and resave/overwrite
+        $zipFile = new ZipFile();
+
+        try {
+            $zipFile
+                ->addFile($path, 'backup.zip', ZipFile::METHOD_DEFLATED)
+                ->setPassword(config('backup.security.password'), config('backup.security.encryption'))
+                ->saveAsFile($path)
+                ->close();
+        } catch (ZipException $exception) {
+            // handle exception
+        } finally {
+            $zipFile->close();
+        }
+
+        consoleOutput()->info('Senha e criptografia aplicadas com êxito ao zip.');
+
+        $this->path = $path;
+    }
+}
