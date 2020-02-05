664a2595d809de9116959ec4804f38d35177a71f
John <juaorok@hotmail.com>
2020-02-05 10:10:26 -0300

    Update bonus.blade.php

--- resources/views/site/bonus/bonus.blade.php
+++ resources/views/site/bonus/bonus.blade.php
@@ -22,7 +22,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Bônus</h4>
                         <div class="table-responsive m-t-15">
-                            <table class="table" id="datatable">
+                            <table class="table">
                                 <thead>
                                 <tr>
                                     <th>Descrição</th>
