396dc053628615f9ea503ea19817fb29fac70b91
John <juaorok@hotmail.com>
2020-02-04 22:30:12 -0300

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Helpers\Mention;
 use App\Models\Setting;
 use Carbon\Carbon;
+use Illuminate\Foundation\Application;
 
 if (!function_exists('md5_gen')) {
     /**
@@ -56,30 +58,72 @@ if (!function_exists('day_month_year')) {
         $year = date('Y');
 
         switch (date('w')) {
-            case 0: $today = 'Domingo'; break;
-            case 1: $today = 'Segunda-Feira'; break;
-            case 2: $today = 'Terça-Feira'; break;
-            case 3: $today = 'Quarta-Feira'; break;
-            case 4: $today = 'Quinta-Feira'; break;
-            case 5: $today = 'Sexta-Feira'; break;
-            case 6: $today = 'Sábado'; break;
-            default: $today = 'Bugou'; break;
+            case 0:
+                $today = 'Domingo';
+                break;
+            case 1:
+                $today = 'Segunda-Feira';
+                break;
+            case 2:
+                $today = 'Terça-Feira';
+                break;
+            case 3:
+                $today = 'Quarta-Feira';
+                break;
+            case 4:
+                $today = 'Quinta-Feira';
+                break;
+            case 5:
+                $today = 'Sexta-Feira';
+                break;
+            case 6:
+                $today = 'Sábado';
+                break;
+            default:
+                $today = 'Bugou';
+                break;
         }
 
         switch (date('n')) {
-            case 1: $mes = 'Janeiro'; break;
-            case 2: $mes = 'Fevereiro'; break;
-            case 3: $mes = 'Março'; break;
-            case 4: $mes = 'Abril'; break;
-            case 5: $mes = 'Maio'; break;
-            case 6: $mes = 'Junho'; break;
-            case 7: $mes = 'Julho'; break;
-            case 8: $mes = 'Agosto'; break;
-            case 9: $mes = 'Setembro'; break;
-            case 10: $mes = 'Outubro'; break;
-            case 11: $mes = 'Novembro'; break;
-            case 12: $mes = 'Dezembro'; break;
-            default: $mes = 'Bugou'; break;
+            case 1:
+                $mes = 'Janeiro';
+                break;
+            case 2:
+                $mes = 'Fevereiro';
+                break;
+            case 3:
+                $mes = 'Março';
+                break;
+            case 4:
+                $mes = 'Abril';
+                break;
+            case 5:
+                $mes = 'Maio';
+                break;
+            case 6:
+                $mes = 'Junho';
+                break;
+            case 7:
+                $mes = 'Julho';
+                break;
+            case 8:
+                $mes = 'Agosto';
+                break;
+            case 9:
+                $mes = 'Setembro';
+                break;
+            case 10:
+                $mes = 'Outubro';
+                break;
+            case 11:
+                $mes = 'Novembro';
+                break;
+            case 12:
+                $mes = 'Dezembro';
+                break;
+            default:
+                $mes = 'Bugou';
+                break;
         }
 
         echo "Hoje é {$today}, {$day_number} de {$mes} de {$year}";
@@ -111,9 +155,9 @@ if (!function_exists('toastr')) {
 
 if (!function_exists('mention')) {
     /**
-     * @return \Illuminate\Foundation\Application|mixed
+     * @return Application|mixed
      */
-    function mention(): \App\Helpers\Mention
+    function mention(): Mention
     {
         return app('mention');
     }
