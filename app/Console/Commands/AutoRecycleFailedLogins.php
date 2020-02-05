d5c03fd8c2dd50b92db3e05fbb4a7bf6bcc7757f
John <juaorok@hotmail.com>
2020-02-04 22:30:06 -0300

    Update AutoRecycleFailedLogins.php

--- app/Console/Commands/AutoRecycleFailedLogins.php
+++ app/Console/Commands/AutoRecycleFailedLogins.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
 use App\Models\FailedLogin;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 
 class AutoRecycleFailedLogins extends Command
