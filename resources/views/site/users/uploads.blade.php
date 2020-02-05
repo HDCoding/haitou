4a4960b1240e4781900e3b25bbc4dc046e643762
John <juaorok@hotmail.com>
2020-02-05 10:11:46 -0300

    Update uploads.blade.php

--- resources/views/site/users/uploads.blade.php
+++ resources/views/site/users/uploads.blade.php
@@ -32,7 +32,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Meus Uploads</h4>
                         <div class="table-responsive m-t-15">
-                            <table class="table" id="datatable">
+                            <table class="table">
                                 <thead>
                                 <tr>
                                     <th>Nome</th>
