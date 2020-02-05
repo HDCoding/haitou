36908c72369be0e99dc38e8acde6dd79501a7db8
John <juaorok@hotmail.com>
2020-02-04 22:33:08 -0300

    Update TorrentTag.php

--- app/Models/TorrentTag.php
+++ app/Models/TorrentTag.php
@@ -6,24 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class TorrentTag extends Model
 {
-	protected $table = 'torrent_tags';
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $table = 'torrent_tags';
+    protected $casts = [
+        'torrent_id' => 'int',
+        'tag_id' => 'int'
+    ];
 
-	public $incrementing = false;
+    public function tag()
+    {
+        return $this->belongsTo(Tag::class);
+    }
 
-	public $timestamps = false;
-
-	protected $casts = [
-		'torrent_id' => 'int',
-		'tag_id' => 'int'
-	];
-
-	public function tag()
-	{
-		return $this->belongsTo(Tag::class);
-	}
-
-	public function torrent()
-	{
-		return $this->belongsTo(Torrent::class);
-	}
+    public function torrent()
+    {
+        return $this->belongsTo(Torrent::class);
+    }
 }
