4781c64713bed197b9b70d8be0ee6838813c4e6f
John <juaorok@hotmail.com>
2020-02-04 22:32:38 -0300

    Update AccountInvitation.php

--- app/Mail/AccountInvitation.php
+++ app/Mail/AccountInvitation.php
@@ -3,9 +3,7 @@
 namespace App\Mail;
 
 use App\Models\Invitation;
-use App\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
