b574e382102421a5466ba844302821311161d04b
John <juaorok@hotmail.com>
2020-02-04 22:32:34 -0300

    Update SearchTerm.php

--- app/Listeners/SearchTerm.php
+++ app/Listeners/SearchTerm.php
@@ -2,9 +2,6 @@
 
 namespace App\Listeners;
 
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-
 class SearchTerm
 {
     /**
@@ -20,7 +17,7 @@ class SearchTerm
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param object $event
      * @return void
      */
     public function handle($event)
