88b17bf30e403afcaaa3712ff042fff28ac4b34e
John <juaorok@hotmail.com>
2020-02-04 22:10:26 -0300

    Create SendPasswordNotificationMail.php

---
+++ app/Jobs/SendPasswordNotificationMail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\PasswordNotification;
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class SendPasswordNotificationMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 3;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if ($this->attempts() > 2) {
+            $this->delay(min(30 * $this->attempts(), 300));
+        }
+
+        Mail::to($this->user)->send(new PasswordNotification());
+    }
+}
