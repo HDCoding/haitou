d510174064a69d3649916ac13760db331c6bfc06
John <juaorok@hotmail.com>
2020-02-06 17:00:07 -0300

    Update block_last_posts.blade.php

--- resources/views/site/home/block_last_posts.blade.php
+++ resources/views/site/home/block_last_posts.blade.php
@@ -33,6 +33,6 @@
             @endforeach
             </tbody>
         </table>
-        <p class="text-right font-weight-bold"><a href="{{ route('forum_latest_posts') }}">Leia mais</a></p>
+        <p class="text-right font-weight-bold"><a href="{{ route('forum.latest.posts') }}">Leia mais</a></p>
     </div>
 </div>
