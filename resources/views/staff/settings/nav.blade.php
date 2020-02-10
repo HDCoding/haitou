cd0fd82cafdaf8c29472171a6381dfd694fda547
John <juaorok@hotmail.com>
2020-02-10 19:08:47 -0300

    Update nav.blade.php

--- resources/views/staff/settings/nav.blade.php
+++ resources/views/staff/settings/nav.blade.php
@@ -2,7 +2,6 @@
     <!-- Nav tabs -->
     <div class="nav flex-column nav-pills">
         <a class="nav-link {{ active_page('staff/settings/analytics') }}" href="{{ route('setting.analytics') }}">Analytics</a>
-        <a class="nav-link {{ active_page('staff/settings/favicon') }}" href="{{ route('setting.favicon') }}">Favicon</a>
         <a class="nav-link {{ active_page('staff/settings/images') }}" href="{{ route('setting.images') }}">Imagens</a>
         <a class="nav-link {{ active_page('staff/settings/mail') }}" href="{{ route('setting.mail') }}">Mail</a>
         <a class="nav-link {{ active_page('staff/settings/others') }}" href="{{ route('setting.others') }}">Outros</a>
