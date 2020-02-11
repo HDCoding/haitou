9dd336841e134957a7160b6510b42005377ef98d
John <juaorok@hotmail.com>
2020-02-11 18:09:44 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\UpdateLastAction::class,
-            \App\Http\Middleware\IncrementTimeOnline::class,
+            //\App\Http\Middleware\IncrementTimeOnline::class,
         ],
 
         'api' => [
