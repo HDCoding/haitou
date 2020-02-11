875574ac10228ce6e83b9e087333baa02883438c
John <juaorok@hotmail.com>
2020-02-11 18:10:53 -0300

    Update comment.blade.php

--- resources/views/site/comments/comment.blade.php
+++ resources/views/site/comments/comment.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.dashboard')
 
-@section('title', 'Conquistas')
+@section('title', 'Comentário')
 
 @section('content')
 
@@ -25,38 +25,38 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title">Comentário feito em</h4>
-                        <h6 class="card-subtitle">
-                            @if($comment->actor)
-                                <b class="text-danger">Atrizes e Atores</b>:
-                                <a class="ml-2" href="{{ route('actor.show', [$comment->actor->id, $comment->actor->slug]) }}" target="_blank">{{ $comment->actor->name }}</a>
-                            @endif
-                            @if($comment->calendar)
-                                <b class="text-danger">Calendário</b>:
-                                <a class="ml-2" href="{{ route('calendars.show', [$comment->calendar->id, $comment->calendar->slug]) }}" target="_blank">{{ $comment->calendar->name }}</a>
-                            @endif
-                            @if($comment->character)
-                                <b class="text-danger">Personagem</b>:
-                                <a class="ml-2" href="{{ route('character.show', [$comment->character->id, $comment->character->slug]) }}" target="_blank">{{ $comment->character->name }}</a>
-                            @endif
-                            @if($comment->fansub)
-                                <b class="text-danger">Fansub</b>:
-                                <a class="ml-2" href="{{ route('fansub.show', [$comment->fansub->id, $comment->fansub->slug]) }}" target="_blank">{{ $comment->fansub->name }}</a>
-                            @endif
-                            @if($comment->media)
-                                <b class="text-danger">Mídia</b>:
-                                <a class="ml-2" href="{{ route('media.show', [$comment->media->id, $comment->media->slug]) }}" target="_blank">{{ $comment->media->name }}</a>
-                            @endif
-                            @if($comment->studio)
-                                <b class="text-danger">Estúdio</b>:
-                                <a class="ml-2" href="{{ route('media.show', [$comment->studio->id, $comment->studio->slug]) }}" target="_blank">{{ $comment->studio->name }}</a>
-                            @endif
-                            @if($comment->torrent)
-                                <b class="text-danger">Torrent</b>:
-                                <a class="ml-2" href="{{ route('torrent.show', [$comment->torrent->id, $comment->torrent->slug]) }}" target="_blank">{{ $comment->torrent->name }}</a>
-                            @endif
-                        </h6>
-
+                        <h4 class="card-title">Comentário feito em
+                            <span>
+                                @if($comment->actor)
+                                    <b class="text-danger">Atrizes e Atores</b>:
+                                    <a class="ml-2" href="{{ route('actor.show', [$comment->actor->id, $comment->actor->slug]) }}" target="_blank">{{ $comment->actor->name }}</a>
+                                @endif
+                                @if($comment->calendar)
+                                    <b class="text-danger">Calendário</b>:
+                                    <a class="ml-2" href="{{ route('calendars.show', [$comment->calendar->id, $comment->calendar->slug]) }}" target="_blank">{{ $comment->calendar->name }}</a>
+                                @endif
+                                @if($comment->character)
+                                    <b class="text-danger">Personagem</b>:
+                                    <a class="ml-2" href="{{ route('character.show', [$comment->character->id, $comment->character->slug]) }}" target="_blank">{{ $comment->character->name }}</a>
+                                @endif
+                                @if($comment->fansub)
+                                    <b class="text-danger">Fansub</b>:
+                                    <a class="ml-2" href="{{ route('fansub.show', [$comment->fansub->id, $comment->fansub->slug]) }}" target="_blank">{{ $comment->fansub->name }}</a>
+                                @endif
+                                @if($comment->media)
+                                    <b class="text-danger">Mídia</b>:
+                                    <a class="ml-2" href="{{ route('media.show', [$comment->media->id, $comment->media->slug]) }}" target="_blank">{{ $comment->media->name }}</a>
+                                @endif
+                                @if($comment->studio)
+                                    <b class="text-danger">Estúdio</b>:
+                                    <a class="ml-2" href="{{ route('studio.show', [$comment->studio->id, $comment->studio->slug]) }}" target="_blank">{{ $comment->studio->name }}</a>
+                                @endif
+                                @if($comment->torrent)
+                                    <b class="text-danger">Torrent</b>:
+                                    <a class="ml-2" href="{{ route('torrent.show', [$comment->torrent->id, $comment->torrent->slug]) }}" target="_blank">{{ $comment->torrent->name }}</a>
+                                @endif
+                            </span>
+                        </h4>
                         <div class="media pb-1 mb-3">
                             <img src="{{ $comment->user->avatar() }}" class="d-block ui-w-40 img-rounded" alt="avatar">
                             <div class="media-body ml-3">
@@ -65,20 +65,18 @@
                                     <mark class="text-danger">Spoiler!</mark>
                                 @endif
                                 <p class="my-1">{{ $comment->content }}</p>
-                                <div class="clearfix">
+                                <div class="clearfix mt-4">
                                     <span class="float-left text-muted small mr-3">{{ $comment->created_at->format('d/m/Y H:i') }}</span>
 
                                     @if($comment->user_id == auth()->user()->id || auth()->user()->can('painel-staff'))
-                                        <a href="{{ route('comments.edit', [$comment->id]) }}">
-                                            <button type="button" class="btn btn-xs btn-primary">
-                                                <span class="fas fa-pencil-alt" data-toggle="tooltip" title="Editar Comentário"></span>
-                                            </button>
+                                        <a href="{{ route('comments.edit', [$comment->id]) }}" data-toggle="tooltip" title="Editar Comentário">
+                                            <i class="fas fa-pencil-alt"></i>
                                         </a>
                                     @endif
 
                                     @if($comment->user_id == auth()->user()->id || auth()->user()->can('painel-staff'))
                                         <a href="javascript:;" onclick="document.getElementById('comment-del-{{ $comment->id }}').submit();">
-                                            <i class="fas fa-times" data-toggle="tooltip" title="Deletar Comentário"></i>
+                                            <i class="fas fa-times text-danger m-l-15" data-toggle="tooltip" title="Deletar Comentário"></i>
                                         </a>
                                         {!! Form::open(['url' => 'comments/' . $comment->id, 'method' => 'DELETE', 'id' => 'comment-del-' . $comment->id , 'style' => 'display: none']) !!}
                                         {!! Form::close() !!}
