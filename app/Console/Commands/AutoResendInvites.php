9934d195e4223719923205e1fc057a903122d65d
John <juaorok@hotmail.com>
2020-02-11 22:15:44 -0300

    Create AutoResendInvites.php

---
+++ app/Console/Commands/AutoResendInvites.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendInvitationMail;
+use App\Models\Invitation;
+use App\Notifications\ResendInviteNotification;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class AutoResendInvites extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'haitou:auto-resend-invites';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Resend not accepted invitation after X days created_at.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $current = new Carbon();
+        $invites = Invitation::with('user:id,username')
+            ->whereNull('accepted_at')
+            ->whereNull('accepted_by')
+            ->where('created_at', '<', $current->copy()->subDays(2)->toDateTimeString())
+            ->whereNull('resend_at')
+            ->get();
+
+        foreach ($invites as $invite) {
+            //set resend datetime
+            $invite->resend_at = now();
+            //resend email
+            dispatch(new SendInvitationMail($invite));
+            //save
+            $invite->save();
+            //send notification
+            $invite->user->notify(new ResendInviteNotification());
+        }
+    }
+}
