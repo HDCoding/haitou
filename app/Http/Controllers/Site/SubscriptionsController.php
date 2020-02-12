f97f91f71022d474a2a56cca5b906b586cb71371
John <juaorok@hotmail.com>
2020-02-12 10:06:20 -0300

    Update SubscriptionsController.php

--- app/Http/Controllers/Site/SubscriptionsController.php
+++ app/Http/Controllers/Site/SubscriptionsController.php
@@ -29,8 +29,8 @@ class SubscriptionsController extends Controller
             toastr()->success('Agora você está inscrito no tópico, ' . $topic->name . ' Agora você receberá notificações do site quando uma resposta for postada.', 'Inscrição');
             return redirect()->route($logger, $params);
         } else {
-            return redirect()->route($logger, $params)
-                ->withErrors('Você já está inscrito neste tópico.');
+            toastr()->error('Você já está inscrito neste tópico.', 'Inscrição');
+            return redirect()->route($logger, $params);
         }
     }
 
@@ -45,8 +45,77 @@ class SubscriptionsController extends Controller
             toastr()->info('Você não está mais inscrito no tópico, ' . $topic->name . '. Você não receberá mais notificações do site quando uma resposta for deixada.', 'Inscrição Cancelada');
             return redirect()->route($logger, $params);
         } else {
-            return redirect()->route($logger, $params)
-                ->withErrors('Você não está inscrito neste tópico para começar...');
+            toastr()->error('Você não está inscrito neste tópico para começar...', 'Inscrição');
+            return redirect()->route($logger, $params);
+        }
+    }
+
+    public function emailNotifyOn(Request $request, $topic_id)
+    {
+        $user = $request->user();
+
+        if (!$request->user()->topicEmailNotification($topic_id)) {
+            $notify = Subscription::where('topic_id', '=', $topic_id)->where('user_id', '=', $user->id)->first();
+            $notify->email = true;
+            $notify->update();
+
+            toastr()->success('Agora você receberá notificações do tópico por e-mail.', 'E-mail Notificação');
+            return redirect()->route('forum.subscriptions');
+        } else {
+            toastr()->error('Você já está recebendo notificações neste tópico.', 'E-mail Notificação');
+            return redirect()->route('forum.subscriptions');
+        }
+    }
+
+    public function emailNotifyOff(Request $request, $topic_id)
+    {
+        $user = $request->user();
+
+        if ($request->user()->topicEmailNotification($topic_id)) {
+
+            $notify = Subscription::where('topic_id', '=', $topic_id)->where('user_id', '=', $user->id)->first();
+            $notify->email = false;
+            $notify->update();
+
+            toastr()->success('Agora você não receberá notificações do tópico por e-mail.', 'E-mail Notificação');
+            return redirect()->route('forum.subscriptions');
+        } else {
+            toastr()->error('Você não está recebendo notificações neste tópico.', 'E-mail Notificação');
+            return redirect()->route('forum.subscriptions');
+        }
+    }
+
+    public function notifyOn(Request $request, $topic_id)
+    {
+        $user = $request->user();
+
+        if (!$request->user()->topicNotification($topic_id)) {
+            $notify = Subscription::where('topic_id', '=', $topic_id)->where('user_id', '=', $user->id)->first();
+            $notify->notify = true;
+            $notify->update();
+
+            toastr()->success('Agora você receberá notificações do tópico', 'Notificação Tópico');
+            return redirect()->route('forum.subscriptions');
+        } else {
+            toastr()->error('Você já está recebendo notificações neste tópico.', 'Notificação Tópico');
+            return redirect()->route('forum.subscriptions');
+        }
+    }
+
+    public function notifyOff(Request $request, $topic_id)
+    {
+        $user = $request->user();
+
+        if ($request->user()->topicNotification($topic_id)) {
+            $notify = Subscription::where('topic_id', '=', $topic_id)->where('user_id', '=', $user->id)->first();
+            $notify->notify = false;
+            $notify->update();
+
+            toastr()->success(' Agora você não receberá notificações do tópico', 'Notificação Tópico');
+            return redirect()->route('forum.subscriptions');
+        } else {
+            toastr()->error('Você não está recebendo notificações neste tópico.', 'Notificação Tópico');
+            return redirect()->route('forum.subscriptions');
         }
     }
 }
