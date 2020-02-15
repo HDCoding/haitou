607b3a50cef79f9bba94be1b03b2c18b49e5842a
John <juaorok@hotmail.com>
2020-02-14 23:25:04 -0300

    Update reply.blade.php

--- resources/views/site/forums/reply.blade.php
+++ resources/views/site/forums/reply.blade.php
@@ -19,7 +19,7 @@
                             <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
                             <li class="breadcrumb-item"><a href="{{ url('forum') }}">Fórum</a></li>
                             <li class="breadcrumb-item">{{ link_to_route('forum.threads', $topic->forum->name, ['forum_id' => $topic->forum->id, 'slug' => $topic->forum->slug]) }}</li>
-                            <li class="breadcrumb-item"><a href="{{ route('forum.topic', [$topic->id, $topic->slug]) }}">{{ $topic->name }}</a></li>
+                            <li class="breadcrumb-item">{{ link_to_route('forum.topic', $topic->name, ['topic_id' => $topic->id, 'slug' => $topic->slug]) }}</li>
                             <li class="breadcrumb-item active" aria-current="page">Reply</li>
                         </ol>
                     </nav>
@@ -35,7 +35,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Reply</h4>
                         @includeIf('errors.errors', [$errors])
-                        {!! Form::open(['route' => ['reply', 'topicId' => $topic->id], 'class' => 'form-horizontal']) !!}
+                        {!! Form::open(['route' => ['reply', 'topic_id' => $topic->id], 'class' => 'form-horizontal']) !!}
                         <div class="form-group">
                             {!! Form::label('content', 'Conteúdo: *', ['class' => 'form-label']) !!}
                             {!! Form::textarea('content', '[quote='.$post->post_username.' escreveu] '. $post->content . '[/quote]', ['class' => 'form-control', 'rows' => 10]) !!}
