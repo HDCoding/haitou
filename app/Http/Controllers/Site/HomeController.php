f1cc71b261502bba7d8a62379d6ead11c35fdfd8
John <juaorok@hotmail.com>
2020-02-16 10:33:58 -0300

    Update HomeController.php

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -21,24 +21,33 @@ class HomeController extends Controller
 
     public function index()
     {
-		//TODO
-		//update select * from, select only necessary
         // For Cache
         $expire_at = Carbon::now()->addMinutes(5);
 
         // Latest News
         $news = cache()->remember('latest_news', $expire_at, function () {
-            return News::select(['id', 'name', 'slug', 'created_at'])->latest()->take(5)->get();
+            return News::select('id', 'name', 'slug', 'created_at')
+                ->latest()
+                ->take(5)
+                ->get();
         });
 
         // Latest Topics
         $topics = cache()->remember('latest_topics', $expire_at, function () {
-            return Topic::with('forum')->latest()->take(5)->get();
+            return Topic::with('forum:id,name,slug')
+                ->select('id', 'forum_id', 'name', 'slug', 'first_post_username', 'created_at')
+                ->latest()
+                ->take(5)
+                ->get();
         });
 
         // Latest Posts
         $posts = cache()->remember('latest_posts', $expire_at, function () {
-            return Post::with('topic', 'user')->latest()->take(5)->get();
+            return Post::with('topic:id,name,slug')
+                ->select('id', 'topic_id', 'post_username', 'content', 'updated_at')
+                ->latest()
+                ->take(5)
+                ->get();
         });
 
         // Latest Poll
@@ -46,18 +55,22 @@ class HomeController extends Controller
             return Poll::select('id', 'name', 'slug', 'created_at')
                 ->where('is_main', '=', true)
                 ->where('is_closed', '=', false)
-                ->latest()->take(5)->get();
+                ->latest()
+                ->take(5)
+                ->get();
         });
 
         // Users Online
-        $users = User::with('group')
+        $users = User::with('group:id,name,color,icon')
             ->select('id', 'group_id', 'username', 'slug', 'is_warned', 'show_profile')
             ->where('last_action', '>', now()->subMinutes(5))
             ->get();
 
         // Groups
         $groups = cache()->remember('user_groups', $expire_at, function () {
-            return Group::select(['name', 'color', 'icon'])->oldest('id')->get();
+            return Group::select('name', 'color', 'icon')
+                ->oldest('id')
+                ->get();
         });
 
         $covers = Storage::files('public/homecovers');
