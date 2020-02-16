46a0a03d8e26c542200f891db19208674d78b4f3
John <juaorok@hotmail.com>
2020-02-15 21:43:33 -0300

    Update covers.blade.php

--- resources/views/site/users/blocks/covers.blade.php
+++ resources/views/site/users/blocks/covers.blade.php
@@ -1,8 +1,8 @@
 @if(!empty($member->cover()))
     <div class="col-sm-12 col-lg-12">
         <div class="card vegas-fixed-background" id="member-cover">
-            <div class="card-body py-5 my-5">
-                <h4 class="text-center text-dark">{{ $member->username }}</h4>
+            <div class="card-body py-home">
+                <h2 class="text-center text-info">{{ $member->username }}</h2>
             </div>
         </div>
     </div>
