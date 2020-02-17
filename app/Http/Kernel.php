11fad13ba8c4e0212553ef5298f22cc02f4c8448
John <juaorok@hotmail.com>
2020-02-17 13:29:20 -0300

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,7 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'lockscreen' => \App\Http\Middleware\Lockscreen::class,
+        //'lockscreen' => \App\Http\Middleware\Lockscreen::class,
         'allow' => \App\Http\Middleware\Allow::class,
     ];
 
