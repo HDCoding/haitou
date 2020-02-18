5f86bd41186345803fda6d573240c5c66c5a66bc
John <juaorok@hotmail.com>
2020-02-18 14:56:49 -0300

    Update BookmarksController.php

--- app/Http/Controllers/Site/BookmarksController.php
+++ app/Http/Controllers/Site/BookmarksController.php
@@ -42,33 +42,36 @@ class BookmarksController extends Controller
 
     public function actors(Request $request)
     {
-        //return all logged user bookmarks
         $user = $request->user();
 
-        $bookmarks = $user->bookmarks()->with('actor:id,name,slug,image')
-            ->where('actor_id', '!=', null)->paginate(30);
+        $bookmarks = $user->bookmarks()
+            ->with('actor:id,name,slug,image')
+            ->where('actor_id', '!=', null)
+            ->paginate(30);
 
         return view('site.bookmarks.actors', compact('bookmarks'));
     }
 
     public function characters(Request $request)
     {
-        //return all logged user bookmarks
         $user = $request->user();
 
-        $bookmarks = $user->bookmarks()->with('character:id,name,slug,image')
-            ->where('character_id', '!=', null)->paginate(30);
+        $bookmarks = $user->bookmarks()
+            ->with('character:id,name,slug,image')
+            ->where('character_id', '!=', null)
+            ->paginate(30);
 
         return view('site.bookmarks.characters', compact('bookmarks'));
     }
 
     public function medias(Request $request)
     {
-        //return all logged user bookmarks
         $user = $request->user();
 
-        $bookmarks = $user->bookmarks()->with('media:id,name,slug,poster')
-            ->where('media_id', '!=', null)->paginate(30);
+        $bookmarks = $user->bookmarks()
+            ->with('media:id,name,slug,poster')
+            ->where('media_id', '!=', null)
+            ->paginate(30);
 
         return view('site.bookmarks.medias', compact('bookmarks'));
     }
