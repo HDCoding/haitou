0a47a0626a0c02c992318eee2a3725cda0ec35ff
John <juaorok@hotmail.com>
2020-02-18 11:37:02 -0300

    Update LikesController.php

--- app/Http/Controllers/Site/LikesController.php
+++ app/Http/Controllers/Site/LikesController.php
@@ -15,64 +15,6 @@ class LikesController extends Controller
         $this->middleware('auth');
     }
 
-    public function likeTorrent(Request $request, $torrent_id)
-    {
-        $torrent = Torrent::findOrFail($torrent_id);
-
-        $user = $request->user();
-
-        $postUrl = "torrent/{$torrent->id}.{$torrent->slug}";
-
-        $like = $user->likes()->where('torrent_id', '=', $torrent->id)->where('is_like', '=', true)->first();
-        $dislike = $user->likes()->where('torrent_id', '=', $torrent->id)->where('is_dislike', '=', true)->first();
-
-        if ($like || $dislike) {
-            toastr()->warning('Você já deu like deste torrent!', 'Aviso');
-            return redirect()->to($postUrl);
-        } elseif ($torrent->user_id == $user->id) {
-            toastr()->info('Você não pode dar like do seu próprio torrent!', 'Aviso');
-            return redirect()->to($postUrl);
-        } else {
-            $new = new Like();
-            $new->user_id = $user->id;
-            $new->torrent_id = $torrent->id;
-            $new->is_like = 1;
-            $new->save();
-
-            toastr()->success('Like aplicado com sucesso!', 'Like');
-            return redirect()->to($postUrl);
-        }
-    }
-
-    public function dislikeTorrent(Request $request, $torrent_id)
-    {
-        $torrent = Torrent::findOrFail($torrent_id);
-
-        $user = $request->user();
-
-        $postUrl = "torrent/{$torrent->id}.{$torrent->slug}";
-
-        $like = $user->likes()->where('torrent_id', '=', $torrent->id)->where('is_like', '=', true)->first();
-        $dislike = $user->likes()->where('torrent_id', '=', $torrent->id)->where('is_dislike', '=', true)->first();
-
-        if ($like || $dislike) {
-            toastr()->warning('Você já deu dislike deste torrent!', 'Aviso');
-            return redirect()->to($postUrl);
-        } elseif ($torrent->user_id == $user->id) {
-            toastr()->info('Você não pode dar dislike do seu próprio torrent!', 'Aviso');
-            return redirect()->to($postUrl);
-        } else {
-            $new = new Like();
-            $new->user_id = $user->id;
-            $new->torrent_id = $torrent->id;
-            $new->is_dislike = 1;
-            $new->save();
-
-            toastr()->success('Dislike aplicado com sucesso!', 'Dislike');
-            return redirect()->to($postUrl);
-        }
-    }
-
     public function likePost(Request $request, $post_id)
     {
         $post = Post::findOrFail($post_id);
@@ -81,10 +23,9 @@ class LikesController extends Controller
 
         $postUrl = "forum/topic/{$post->topic->id}.{$post->topic->slug}?page={$post->pageNumber()}#post-{$post_id}";
 
-        $like = $user->likes()->where('post_id', '=', $post->id)->where('is_like', '=', true)->first();
-        $dislike = $user->likes()->where('post_id', '=', $post->id)->where('is_dislike', '=', true)->first();
+        $like = $user->likes()->where('post_id', '=', $post->id)->first();
 
-        if ($like || $dislike) {
+        if ($like) {
             toastr()->warning('Você já deu like deste post!', 'Aviso');
             return redirect()->to($postUrl);
         } elseif ($post->user_id == $user->id) {
@@ -94,7 +35,6 @@ class LikesController extends Controller
             $new = new Like();
             $new->user_id = $user->id;
             $new->post_id = $post->id;
-            $new->is_like = 1;
             $new->save();
 
             toastr()->success('Like aplicado com sucesso!', 'Like');
@@ -102,32 +42,4 @@ class LikesController extends Controller
         }
     }
 
-    public function dislikePost(Request $request, $post_id)
-    {
-        $post = Post::findOrFail($post_id);
-
-        $user = $request->user();
-
-        $postUrl = "forum/topic/{$post->topic->id}.{$post->topic->slug}?page={$post->pageNumber()}#post-{$post_id}";
-
-        $like = $user->likes()->where('post_id', '=', $post->id)->where('is_like', '=', true)->first();
-        $dislike = $user->likes()->where('post_id', '=', $post->id)->where('is_dislike', '=', true)->first();
-
-        if ($like || $dislike) {
-            toastr()->warning('Você já deu dislike deste post!', 'Aviso');
-            return redirect()->to($postUrl);
-        } elseif ($post->user_id == $user->id) {
-            toastr()->info('Você não pode dar dislike do seu próprio post!', 'Aviso');
-            return redirect()->to($postUrl);
-        } else {
-            $new = new Like();
-            $new->user_id = $user->id;
-            $new->post_id = $post->id;
-            $new->is_dislike = 1;
-            $new->save();
-
-            toastr()->success('Dislike aplicado com sucesso!', 'Dislike');
-            return redirect()->to($postUrl);
-        }
-    }
 }
