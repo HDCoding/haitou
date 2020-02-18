100ce48890546f089468e2b90debf589cc9ccd10
John <juaorok@hotmail.com>
2020-02-17 21:46:16 -0300

    Update 2019_11_18_002756_create_forums_table.php

--- database/migrations/2019_11_18_002756_create_forums_table.php
+++ database/migrations/2019_11_18_002756_create_forums_table.php
@@ -16,6 +16,8 @@ class CreateForumsTable extends Migration
         Schema::create('forums', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('category_id')->index();
+            $table->unsignedBigInteger('last_topic_id')->index()->nullable();
+            $table->unsignedBigInteger('last_post_user_id')->index()->nullable();
             $table->tinyInteger('position')->default(1);
             $table->string('name');
             $table->string('slug');
@@ -24,11 +26,17 @@ class CreateForumsTable extends Migration
             $table->integer('num_topic')->default(0);
             $table->integer('num_post')->default(0);
             $table->integer('views')->default(0);
+            $table->string('last_topic_name')->nullable();
+            $table->string('last_topic_slug')->nullable();
+            $table->string('last_post_username', 25)->nullable();
             $table->softDeletes();
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories')
                 ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('last_topic_id')->references('id')->on('topics');
+            $table->foreign('last_post_user_id')->references('id')->on('users');
         });
     }
 
