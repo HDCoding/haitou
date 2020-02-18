b6a6c221429ec983863c25098bdfcdb70e15f61d
John <juaorok@hotmail.com>
2020-02-18 13:34:41 -0300

    Update InvitationController.php

--- app/Http/Controllers/Auth/InvitationController.php
+++ app/Http/Controllers/Auth/InvitationController.php
@@ -81,7 +81,7 @@ class InvitationController extends Controller
 
             //update the invitation
             $invitation->accepted_by = $user->id;
-            $invitation->code = now();
+            $invitation->code = null;
             $invitation->accepted = true;
             $invitation->expires_on = null;
             $invitation->accepted_at = now();
