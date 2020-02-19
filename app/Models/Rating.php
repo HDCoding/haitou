d817f40943898a214a12a00f35b3c5f72976885d
John <juaorok@hotmail.com>
2020-02-18 21:00:00 -0300

    Update Rating.php

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,29 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     public $timestamps = false;
+
     protected $table = 'ratings';
+
     protected $casts = [
         'user_id' => 'int',
         'media_id' => 'int',
-        'torrent_id' => 'int',
         'vote' => 'int'
     ];
 
     protected $fillable = [
         'user_id',
         'media_id',
-        'torrent_id',
         'vote'
     ];
 
     public function media()
     {
         return $this->belongsTo(Media::class);
-    }
-
-    public function torrent()
-    {
-        return $this->belongsTo(Torrent::class);
     }
 
     public function user()
