c3fc6669ec6727c757b45803f3f3d196c222bf5c
John <juaorok@hotmail.com>
2020-02-21 10:39:01 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/freeslots/index.blade.php
+++ resources/views/staff/freeslots/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.freeslots'))
 
+@section('css')
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -26,10 +31,8 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.freeslots')</h4>
-                        <a href="{{ url('staff/freeslots/create') }}">
-                            <button type="button" class="btn btn-xs btn-primary">
-                                <span class="ion ion-md-add"></span> Adicionar
-                            </button>
+                        <a href="{{ url('staff/freeslots/create') }}" class="btn btn-xs btn-primary">
+                            <i class="ion ion-md-add"></i> Adicionar
                         </a>
                         <div class="table-responsive m-t-15">
                             <table class="table">
@@ -59,14 +62,15 @@
                                         <td>{{ $freeslot->is_doubleup == 1 ? 'Sim' : 'Nao' }}</td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/freeslots/' . $freeslot->id . '/edit') }}" data-toggle="tooltip" title="Editar FreeSlot">
-                                                    <span class="fas fa-pencil-alt text-info"></span>
+                                                <a href="{{ url('staff/freeslots/' . $freeslot->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar FreeSlot">
+                                                    <i class="fas fa-pencil-alt text-info"></i>
                                                 </a>
-                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('freeslot-del-{{ $freeslot->id }}').submit();" data-toggle="tooltip" title="Remover FreeSlot">
-                                                    <span class="fas fa-times text-danger"></span>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover FreeSlot"
+                                                   onclick="deleteData({{ $freeslot->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
                                                 </a>
-                                                {!! Form::open(['url' => 'staff/freeslots/' . $freeslot->id, 'method' => 'DELETE', 'id' => 'freeslot-del-' . $freeslot->id , 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
                                             </div>
                                         </td>
                                     </tr>
@@ -80,4 +84,57 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/freeslots') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
+    </script>
 @endsection
