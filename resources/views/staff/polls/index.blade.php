17d2291837a01b61f6e831804ea4b436b48b1544
John <juaorok@hotmail.com>
2020-02-21 10:39:28 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/polls/index.blade.php
+++ resources/views/staff/polls/index.blade.php
@@ -5,6 +5,8 @@
 @section('css')
     <!-- DataTables -->
     <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
 @endsection
 
 @section('content')
@@ -59,29 +61,43 @@
                                         <td>{{ $poll->name() }}</td>
                                         <td>
                                             @if($poll->is_closed)
-                                            {!! Form::open(['url' => 'staff/poll/' . $poll->id . '/open']) !!}
-                                                <button type="submit" class="btn btn-sm" data-toggle="tooltip" title="Abrir Pesquisa">
+                                                {!! Form::open(['url' => 'staff/poll/' . $poll->id . '/open']) !!}
+                                                <button type="submit" class="btn btn-sm" data-toggle="tooltip"
+                                                        title="Abrir Pesquisa">
                                                     <i class="fa fa-play text-success"></i>
                                                 </button>
-                                            {!! Form::close() !!}
+                                                {!! Form::close() !!}
                                             @else
-                                            {!! Form::open(['url' => 'staff/poll/' . $poll->id . '/close']) !!}
-                                                <button type="submit" class="btn btn-sm" data-toggle="tooltip" title="Fechar Pesquisa">
+                                                {!! Form::open(['url' => 'staff/poll/' . $poll->id . '/close']) !!}
+                                                <button type="submit" class="btn btn-sm" data-toggle="tooltip"
+                                                        title="Fechar Pesquisa">
                                                     <i class="fa fa-stop text-danger"></i>
                                                 </button>
-                                            {!! Form::close() !!}
+                                                {!! Form::close() !!}
                                             @endif
                                         </td>
                                         <td>{{ $poll->votes()->count() }}</td>
                                         <td>{{ format_date($poll->created_at) }}</td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/poll/' . $poll->id . '/options/add') }}" data-toggle="tooltip" title="Adicionar Opções"><i class="fa fa-plus text-warning"></i></a>
-                                                <a class="m-l-15" href="{{ url('staff/poll/' . $poll->id . '/options/remove') }}" data-toggle="tooltip" title="Remover Opções"><i class="fa fa-minus text-success"></i></a>
-                                                <a class="m-l-15" href="{{ url('staff/polls/' . $poll->id . '/edit') }}" data-toggle="tooltip" title="Editar Poll"><i class="fa fa-pencil-alt text-info"></i></a>
-                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('poll-del-{{ $poll->id }}').submit();" data-toggle="tooltip" title="Remover Poll"><i class="fa fa-times text-danger"></i></a>
-                                                {!! Form::open(['url' => 'staff/polls/' . $poll->id, 'method' => 'DELETE', 'id' => 'poll-del-' . $poll->id, 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
+                                                <a href="{{ url('staff/poll/' . $poll->id . '/options/add') }}"
+                                                   data-toggle="tooltip" title="Adicionar Opções">
+                                                    <i class="fa fa-plus text-warning"></i>
+                                                </a>
+                                                <a class="m-l-15"
+                                                   href="{{ url('staff/poll/' . $poll->id . '/options/remove') }}"
+                                                   data-toggle="tooltip" title="Remover Opções">
+                                                    <i class="fa fa-minus text-success"></i>
+                                                </a>
+                                                <a class="m-l-15" href="{{ url('staff/polls/' . $poll->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Poll">
+                                                    <i class="fa fa-pencil-alt text-info"></i>
+                                                </a>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Poll"
+                                                   onclick="deleteData({{ $poll->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
@@ -98,18 +114,71 @@
 @endsection
 
 @section('scripts')
+    <!-- DataTables -->
     <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <!-- DataTables -->
     <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         $(document).ready(function () {
             $('#datatable').DataTable({
                 "displayLength": 50,
                 "searching": true,
                 "responsive": true,
-                "order": [[ 0, "asc" ]],
+                "order": [[0, "asc"]],
                 "language": {
-                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                    "url": '{{ asset('vendor/datatables/Portuguese-Brasil.json') }}'
                 }
             });
         });
+    </script>
+
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/polls') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
     </script>
 @endsection
