46da803bfd974fc5723de9581f02e8e56134ed81
John <juaorok@hotmail.com>
2020-02-21 10:38:53 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/faqs/index.blade.php
+++ resources/views/staff/faqs/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.faqs'))
 
+@section('css')
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -28,59 +33,120 @@
             @includeIf('errors.errors', [$errors])
             @include('includes.messages')
             @forelse($categories as $category)
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">{{ $category->name }}</h4>
-                    </div>
-                    <table class="table m-b-0">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Pergunta</th>
-                            <th>Ativado</th>
-                            <th class="text-center">Opções</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @forelse($faqs as $faq)
-                            @if($faq->category_id == $category->id)
-                                <tr>
-                                    <th>{{ $faq->id }}</th>
-                                    <td>{{ $faq->question }}</td>
-                                    <td>{!! $faq->enabled() !!}</td>
-                                    <td class="text-center">
-                                        <div class="btn-group">
-                                            <a href="javascript:;" onclick="document.getElementById('faq-disable-{{ $faq->id }}').submit();">
-                                                @if($faq->is_enable)
-                                                    <i class="fa fa-pause text-warning" data-toggle="tooltip" title="Desativar Pergunta"></i>
-                                                @else
-                                                    <i class="fa fa-play text-success" data-toggle="tooltip" title="Ativar Pergunta"></i>
-                                                @endif
-                                            </a>
-                                            {!! Form::open(['url' => 'faq/'.$faq->id.'/update/', 'method' => 'PUT', 'id' => 'faq-disable-' . $faq->id, 'style' => 'display: none']) !!}
-                                            {!! Form::close() !!}
-                                            <a class="m-l-15" href="{{ url('staff/faqs/' . $faq->id . '/edit') }}" data-toggle="tooltip" title="Editar Pergunta"><i class="fa fa-pencil-alt text-info"></i></a>
-                                            <a class="m-l-15" href="javascript:;" onclick="document.getElementById('faq-del-{{ $faq->id }}').submit();" data-toggle="tooltip" title="Remover Pergunta"><i class="fa fa-times text-danger"></i></a>
-                                            {!! Form::open(['url' => 'staff/faqs/' . $faq->id, 'method' => 'DELETE', 'id' => 'faq-del-' . $faq->id, 'style' => 'display: none']) !!}
-                                            {!! Form::close() !!}
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endif
-                        @empty
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $category->name }}</h4>
+                        </div>
+                        <table class="table m-b-0">
+                            <thead>
                             <tr>
-                                <td colspan="4" class="text-center">Nenhuma pergunta cadastrada até o momento</td>
+                                <th>#</th>
+                                <th>Pergunta</th>
+                                <th>Ativado</th>
+                                <th class="text-center">Opções</th>
                             </tr>
-                        @endforelse
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @forelse($faqs as $faq)
+                                @if($faq->category_id == $category->id)
+                                    <tr>
+                                        <th>{{ $faq->id }}</th>
+                                        <td>{{ $faq->question }}</td>
+                                        <td>{!! $faq->enabled() !!}</td>
+                                        <td class="text-center">
+                                            <div class="btn-group">
+                                                <a href="javascript:;"
+                                                   onclick="document.getElementById('faq-disable-{{ $faq->id }}').submit();">
+                                                    @if($faq->is_enable)
+                                                        <i class="fa fa-pause text-warning" data-toggle="tooltip"
+                                                           title="Desativar Pergunta"></i>
+                                                    @else
+                                                        <i class="fa fa-play text-success" data-toggle="tooltip"
+                                                           title="Ativar Pergunta"></i>
+                                                    @endif
+                                                </a>
+                                                {!! Form::open(['url' => 'faq/'.$faq->id.'/update/', 'method' => 'PUT', 'id' => 'faq-disable-' . $faq->id, 'style' => 'display: none']) !!}
+                                                {!! Form::close() !!}
+                                                <a class="m-l-15" href="{{ url('staff/faqs/' . $faq->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Pergunta">
+                                                    <i class="fa fa-pencil-alt text-info"></i>
+                                                </a>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Pergunta"
+                                                   onclick="deleteData({{ $faq->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
+                                                </a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endif
+                            @empty
+                                <tr>
+                                    <td colspan="4" class="text-center">Nenhuma pergunta cadastrada até o momento</td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
             @empty
                 <p class="text-center">Nenhuma categoria cadastrada até o momento</p>
             @endforelse
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/faqs') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
+    </script>
 @endsection
