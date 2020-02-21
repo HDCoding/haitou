e6ab9d2358013ce4fa734c4a19923037e989a630
John <juaorok@hotmail.com>
2020-02-21 10:39:32 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/rules/index.blade.php
+++ resources/views/staff/rules/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.rules'))
 
+@section('css')
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -51,10 +56,15 @@
                                         <td>{{ $rule->icon }}</td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/rules/' . $rule->id . '/edit') }}" data-toggle="tooltip" title="Editar Regra"><i class="fas fa-pencil-alt text-info"></i></a>
-                                                <a href="javascript:;" onclick="document.getElementById('rule-del-{{ $rule->id }}').submit();" class="m-l-15" data-toggle="tooltip" title="Remover Regra"><i class="fa fa-times text-danger"></i></a>
-                                                {!! Form::open(['url' => 'staff/rules/' . $rule->id, 'method' => 'DELETE', 'id' => 'rule-del-' . $rule->id , 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
+                                                <a href="{{ url('staff/rules/' . $rule->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Regra">
+                                                    <i class="fas fa-pencil-alt text-info"></i>
+                                                </a>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Regra"
+                                                   onclick="deleteData({{ $rule->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
@@ -68,4 +78,57 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/rules') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
+    </script>
 @endsection
