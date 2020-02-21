02600d8d598b1c21aec7c052f188bab55ada5d07
John <juaorok@hotmail.com>
2020-02-21 10:39:14 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/medias/index.blade.php
+++ resources/views/staff/medias/index.blade.php
@@ -5,6 +5,8 @@
 @section('css')
     <!-- DataTables -->
     <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
 @endsection
 
 @section('content')
@@ -52,20 +54,37 @@
                                 <tbody>
                                 @foreach($medias as $media)
                                     <tr>
-                                        <td class="align-middle"><img class="img-fluid" src="{{ $media->poster() }}" alt="{{ $media->name }}" width="70px"></td>
+                                        <td class="align-middle">
+                                            <img class="img-fluid" src="{{ $media->poster() }}" alt="{{ $media->name }}" width="70px">
+                                        </td>
                                         <td class="align-middle">{{ $media->category->name }}</td>
                                         <td class="align-middle">{{ $media->name }}</td>
                                         <td class="align-middle">{!! $media->type() !!}</td>
-                                        <td class="align-middle"><span class="badge badge-info">{{ $media->views }}</span></td>
+                                        <td class="align-middle">
+                                            <span class="badge badge-info">{{ $media->views }}</span>
+                                        </td>
                                         <td class="align-middle">{{ $media->studio->name }}</td>
                                         <td class="text-center align-middle">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/media/' . $media->id . '/images') }}" data-toggle="tooltip" title="Imagens"><i class="fas fa-image text-success"></i></a>
-                                                <a class="m-l-15" href="{{ url('staff/media/' . $media->id . '/casts') }}" data-toggle="tooltip" title="Casts"><i class="fas fa-users text-warning"></i></a>
-                                                <a class="m-l-15" href="{{ url('staff/medias/' . $media->id . '/edit') }}" data-toggle="tooltip" title="Editar Mídia"><i class="fas fa-pencil-alt text-info"></i></a>
-                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('media-del-{{ $media->id }}').submit();" data-toggle="tooltip" title="Remover Mídia"><i class="fa fa-times text-danger"></i></a>
-                                                {!! Form::open(['url' => 'staff/medias/' . $media->id, 'method' => 'DELETE', 'id' => 'media-del-' . $media->id , 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
+                                                <a href="{{ url('staff/media/' . $media->id . '/images') }}"
+                                                   data-toggle="tooltip" title="Imagens">
+                                                    <i class="fas fa-image text-success"></i>
+                                                </a>
+                                                <a class="m-l-15"
+                                                   href="{{ url('staff/media/' . $media->id . '/casts') }}"
+                                                   data-toggle="tooltip" title="Casts">
+                                                    <i class="fas fa-users text-warning"></i>
+                                                </a>
+                                                <a class="m-l-15"
+                                                   href="{{ url('staff/medias/' . $media->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Mídia">
+                                                    <i class="fas fa-pencil-alt text-info"></i>
+                                                </a>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Mídia"
+                                                   onclick="deleteData({{ $media->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
@@ -82,18 +101,71 @@
 @endsection
 
 @section('scripts')
+    <!-- DataTables -->
     <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <!-- DataTables -->
     <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         $(document).ready(function () {
             $('#datatable').DataTable({
                 "displayLength": 50,
                 "searching": true,
                 "responsive": true,
-                "order": [[ 2, "asc" ]],
+                "order": [[2, "asc"]],
                 "language": {
-                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                    "url": '{{ asset('vendor/datatables/Portuguese-Brasil.json') }}'
                 }
             });
         });
+    </script>
+
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/medias') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
     </script>
 @endsection
