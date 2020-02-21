21500ce06dde574d6ec578dad42673a657cc75e5
John <juaorok@hotmail.com>
2020-02-21 10:39:10 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/groups/index.blade.php
+++ resources/views/staff/groups/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('title', trans('dashboard.groups'))
 
+@section('css')
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
+@endsection
+
 @section('content')
 
     <div class="page-breadcrumb">
@@ -53,11 +58,16 @@
                                         <td>{{ ($group->hnr / 3600) }} Horas</td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/groups/' . $group->id . '/edit') }}" data-toggle="tooltip" title="Editar Grupo"><i class="fas fa-pencil-alt text-info"></i></a>
+                                                <a href="{{ url('staff/groups/' . $group->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Grupo">
+                                                    <i class="fas fa-pencil-alt text-info"></i>
+                                                </a>
                                                 @if(auth()->user()->can('acesso-total'))
-                                                    <a class="m-l-15" href="javascript:;" onclick="document.getElementById('role-del-{{ $group->id }}').submit();" data-toggle="tooltip" title="Remover Grupo"><i class="fa fa-times text-danger"></i></a>
-                                                    {!! Form::open(['url' => 'staff/groups/' . $group->id, 'method' => 'DELETE', 'id' => 'role-del-' . $group->id , 'style' => 'display: none']) !!}
-                                                    {!! Form::close() !!}
+                                                    <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                       data-original-title="Remover Grupo"
+                                                       onclick="deleteData({{ $group->id }})" type="submit">
+                                                        <i class="fa fa-times text-danger"></i>
+                                                    </a>
                                                 @endif
                                             </div>
                                         </td>
@@ -72,4 +82,57 @@
         </div>
     </div>
 
+@endsection
+
+@section('scripts')
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/groups') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
+    </script>
 @endsection
