a58d22bc0265122e73e64ff3881325cec926f650
John <juaorok@hotmail.com>
2020-02-21 10:38:18 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/actors/index.blade.php
+++ resources/views/staff/actors/index.blade.php
@@ -3,7 +3,10 @@
 @section('title', trans('dashboard.actors'))
 
 @section('css')
+    <!-- dataTables  -->
     <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
 @endsection
 
 @section('content')
@@ -55,14 +58,15 @@
                                         <td><span class="badge badge-info">{{ $actor->views }}</span></td>
                                         <td class="text-center">
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/actors/' . $actor->id . '/edit') }}" data-toggle="tooltip" title="Editar Atriz/Ator">
+                                                <a href="{{ url('staff/actors/' . $actor->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Atriz/Ator">
                                                     <i class="fas fa-pencil-alt"></i>
                                                 </a>
-                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('actor-del-{{ $actor->id }}').submit();" data-toggle="tooltip" title="Remover Atriz/Ator">
-                                                    <i class="fas fa-times text-danger"></i>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Atriz/Ator"
+                                                   onclick="deleteData({{ $actor->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
                                                 </a>
-                                                {!! Form::open(['url' => 'staff/actors/' . $actor->id, 'method' => 'DELETE', 'id' => 'actor-del-' . $actor->id , 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
                                             </div>
                                         </td>
                                     </tr>
@@ -79,19 +83,71 @@
 @endsection
 
 @section('scripts')
+    <!-- dataTables  -->
     <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- dataTables  -->
     <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         $(document).ready(function () {
             $('#datatable').DataTable({
                 "displayLength": 50,
                 "searching": true,
                 "responsive": true,
-                "order": [[ 1, "asc" ]],
+                "order": [[1, "asc"]],
                 "language": {
-                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                    "url": '{{ asset('vendor/datatables/Portuguese-Brasil.json') }}'
                 }
             });
 
         });
+    </script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/actors') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
     </script>
 @endsection
