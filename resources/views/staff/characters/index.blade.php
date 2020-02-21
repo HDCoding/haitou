128d5b92989d5493fc779e439ab165079533bded
John <juaorok@hotmail.com>
2020-02-21 10:38:42 -0300

    Sweet-Alert
    Sweet-Alert

--- resources/views/staff/characters/index.blade.php
+++ resources/views/staff/characters/index.blade.php
@@ -5,6 +5,8 @@
 @section('css')
     <!-- DataTables -->
     <link href="{{ asset('vendor/datatables/DataTables-1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+    <!-- Sweet-Alert  -->
+    <link href="{{ asset('vendor/sweetalert/sweetalert.css') }}" rel="stylesheet"/>
 @endsection
 
 @section('content')
@@ -50,16 +52,22 @@
                                 @foreach($characters as $character)
                                     <tr>
                                         <th>
-                                            <img src="{{ $character->image() }}" alt="{{ $character->name }}" width="70px">
+                                            <img src="{{ $character->image() }}" alt="{{ $character->name }}"
+                                                 width="70px">
                                         </th>
                                         <td>{{ $character->name }}</td>
                                         <td><span class="badge badge-info">{{ $character->views }}</span></td>
                                         <td>
                                             <div class="btn-group">
-                                                <a href="{{ url('staff/characters/' . $character->id . '/edit') }}" data-toggle="tooltip" title="Editar Personagem"><i class="fas fa-pencil-alt text-info"></i></a>
-                                                <a class="m-l-15" href="javascript:;" onclick="document.getElementById('character-del-{{ $character->id }}').submit();" data-toggle="tooltip" title="Remover Personagem"><i class="fa fa-times text-danger"></i></a>
-                                                {!! Form::open(['url' => 'staff/characters/' . $character->id, 'method' => 'DELETE', 'id' => 'character-del-' . $character->id , 'style' => 'display: none']) !!}
-                                                {!! Form::close() !!}
+                                                <a href="{{ url('staff/characters/' . $character->id . '/edit') }}"
+                                                   data-toggle="tooltip" title="Editar Personagem">
+                                                    <i class="fas fa-pencil-alt text-info"></i>
+                                                </a>
+                                                <a class="m-l-15" href="#" data-toggle="tooltip"
+                                                   data-original-title="Remover Personagem"
+                                                   onclick="deleteData({{ $character->id }})" type="submit">
+                                                    <i class="fa fa-times text-danger"></i>
+                                                </a>
                                             </div>
                                         </td>
                                     </tr>
@@ -76,18 +84,71 @@
 @endsection
 
 @section('scripts')
+    <!-- dataTables  -->
     <script src="{{ asset('vendor/datatables/datatables.min.js') }}"></script>
+    <!-- dataTables  -->
     <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
         $(document).ready(function () {
             $('#datatable').DataTable({
                 "displayLength": 50,
                 "searching": true,
                 "responsive": true,
-                "order": [[ 1, "asc" ]],
+                "order": [[1, "asc"]],
                 "language": {
-                    "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Portuguese-Brasil.json"
+                    "url": '{{ asset('vendor/datatables/Portuguese-Brasil.json') }}'
                 }
             });
         });
+    </script>
+
+    <!-- Sweet-Alert  -->
+    <script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}"></script>
+
+    <!-- Sweet-Alert -->
+    <script nonce="{{ Bepsvpt\SecureHeaders\SecureHeaders::nonce() }}">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        function deleteData(dataId) {
+            swal({
+                title: "Confirmar exclusão",
+                text: "Tem certeza de que deseja excluir?",
+                type: "warning",
+                showCancelButton: true,
+                cancelButtonText: "Cancelar",
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Sim, apague!",
+            }, function (isConfirm) {
+                if (!isConfirm) {
+                    return;
+                }
+                $.ajax({
+                    url: "{{ url('staff/characters') }}" + '/' + dataId,
+                    type: "POST",
+                    data: {'_method': 'DELETE'},
+                    success: function () {
+                        swal({
+                                title: "Sucesso!",
+                                text: "OK, excluído! \nClique em 'Ok' para atualizar a página.",
+                                type: "success",
+                            },
+                            function () {
+                                location.reload();
+                            });
+                    },
+                    error: function () {
+                        swal({
+                            title: 'Opps...',
+                            text: data.message,
+                            type: 'error',
+                            timer: '1500'
+                        })
+                    }
+                })
+            });
+        }
     </script>
 @endsection
