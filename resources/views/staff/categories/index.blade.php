062cb1a10360a6681ae72ca30e957c02cd4120d5
John <juaorok@hotmail.com>
2020-02-24 12:31:13 -0300

    Update index.blade.php

--- resources/views/staff/categories/index.blade.php
+++ resources/views/staff/categories/index.blade.php
@@ -33,10 +33,8 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">@lang('dashboard.categories')</h4>
-                        <a href="{{ url('staff/categories/create') }}">
-                            <button type="button" class="btn btn-primary">
-                                <span class="ion ion-md-add"></span> Adicionar
-                            </button>
+                        <a href="{{ url('staff/categories/create') }}" class="btn btn-xs btn-primary">
+                            <i class="ion ion-md-add"></i> Adicionar
                         </a>
                         <div class="demo-vertical-spacing m-t-15">
                             <ul class="nav nav-pills nav-fill">
