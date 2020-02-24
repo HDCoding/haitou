1b77d76325fd7df166d18f43f7ae8fd0d503ce0e
John <juaorok@hotmail.com>
2020-02-24 12:31:28 -0300

    Update links.blade.php

--- resources/views/staff/icons/links.blade.php
+++ resources/views/staff/icons/links.blade.php
@@ -1,31 +1,31 @@
 <ul class="nav container-m-nx bg-lighter container-p-x m-b-4">
     <li class="nav-item">
         <a class="nav-link {{ request()->is('staff/icons/fontawesome') ? 'text-body font-weight-bold pl-0' : 'text-muted' }}" href="{{ url('staff/icons/fontawesome') }}">
-            <span class="fab fa-font-awesome-flag"></span>
+            <i class="fas fa-font-awesome-flag"></i>
             Font Awesome 5
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ request()->is('staff/icons/ionicons') ? 'text-body font-weight-bold pl-0' : 'text-muted' }}" href="{{ url('staff/icons/ionicons') }}">
-            <span class="ion ion-logo-ionic"></span>
+            <i class="ion ion-logo-ionic"></i>
             Ionicons
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ request()->is('staff/icons/linearicons') ? 'text-body font-weight-bold pl-0' : 'text-muted' }}" href="{{ url('staff/icons/linearicons') }}">
-            <span class="lnr lnr-linearicons"></span>
+            <i class="lnr lnr-linearicons"></i>
             Linearicons
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ request()->is('staff/icons/openiconic') ? 'text-body font-weight-bold pl-0' : 'text-muted' }}" href="{{ url('staff/icons/openiconic') }}">
-            <span class="oi oi-heart"></span>
+            <i class="oi oi-heart"></i>
             Open Iconic
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link {{ request()->is('staff/icons/strokeicons') ? 'text-body font-weight-bold pl-0' : 'text-muted' }}" href="{{ url('staff/icons/strokeicons') }}">
-            <span class="pe-7s-box2"></span>
+            <i class="pe-7s-box2"></i>
             Stroke Icons 7
         </a>
     </li>
