7a294da66c7812049236c74071ffbfcf18db76ab
John <juaorok@hotmail.com>
2020-02-26 19:53:14 -0300

    Update thanks_activation.blade.php

--- resources/views/emails/thanks_activation.blade.php
+++ resources/views/emails/thanks_activation.blade.php
@@ -1,18 +1,18 @@
 @component('mail::message')
     Olá!
 
-    Agradeçemos por criar uma conta do {{ config('app.name') }}. Você está pronta(o) para prosseguir!
+    Agradeçemos por criar uma conta do {{ $site_name }}. Você está pronta(o) para prosseguir!
 
-    {{ config('app.name') }} torna mais fácil para o torrent/fórum e usuários.
+    {{ $site_name }} torna mais fácil para o torrent/fórum e usuários.
     Acompanhe as conversas em tempo real com as quais eles se importam. Traz mais flexibilidade
     e insight para os usuários, acompanhe as pessoas e os tópicos mais importantes para você.
     E você pode participar da conversas, compartilhando fotos e links para o site.
     tópicos de notícias e muito mais.
 
-    Para mais informações sobre {{ config('app.name') }}, incluindo downloads e links.
+    Para mais informações sobre {{ $site_name }}, incluindo downloads e links.
     Por favor leia a página de Regras e F.A.Q. para qualquer dúvida.
 
     Equipe,
-    {{ config('app.name') }}
+    {{ $site_name }}
 
 @endcomponent
