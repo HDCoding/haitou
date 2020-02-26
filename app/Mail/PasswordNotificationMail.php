5c09d26351f97e2a57341d4726ecad42b3139c98
John <juaorok@hotmail.com>
2020-02-26 19:53:52 -0300

    Create PasswordNotificationMail.php

---
+++ app/Mail/PasswordNotificationMail.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordNotificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $site_name;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->site_name = setting('site_title');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Login não autorizado!')
+            ->markdown('emails.password_notification')
+            ->with(['site_name' => $this->site_name]);
+    }
+}
