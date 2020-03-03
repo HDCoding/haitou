1e97006343367b633fcd781b0466b8313269e3d4
John <juaorok@hotmail.com>
2020-03-03 17:16:17 -0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,3 @@
-import Vue from 'vue';
-import App from "./App";
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -22,7 +19,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('chat-app', require('./components/chatbox/ChatApp'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -31,6 +28,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
-    render: (h) => h(App)
+    el: '#chatbox',
 });
