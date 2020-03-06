7cc2571007ba4b6dc1a78f8f3105dfd7ef7d399e
John <juaorok@hotmail.com>
2020-03-06 15:30:22 -0300

    Create ChatMessages.vue

---
+++ resources/js/components/chat/ChatMessages.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <li class="chat-item" v-for="message in messages">
+            <div class="chat-content">
+                <h6 class="font-medium">{{ message.username }}</h6>
+                <div class="box bg-light-info">
+                    {{ message.message }}
+                </div>
+            </div>
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChatMessages",
+        props: ['messages']
+    }
+</script>
+
+<style scoped>
+
+</style>
