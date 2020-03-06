60e6131d6558a5acd2112aee16734fc3fc1c9024
John <juaorok@hotmail.com>
2020-03-06 15:30:17 -0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,10 +16,10 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-// Vue.component('chat-app', require('./components/chatbox/ChatApp'));
+Vue.component('chat-app', require('./components/chat/ChatApp'));
+Vue.component('chat-rooms', require('./components/chat/Chatrooms'));
+Vue.component('chat-messages', require('./components/chat/ChatMessages'));
+Vue.component('chat-form', require('./components/chat/ChatForm'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
