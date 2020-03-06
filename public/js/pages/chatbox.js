5f0f8a4b8fbb8bd6f0060a61d59fa066138947c3
John <juaorok@hotmail.com>
2020-03-06 20:11:11 -0300

    Create chatbox.js

---
+++ public/js/pages/chatbox.js
@@ -0,0 +1,60 @@
+var scollBox = $('.scrollable');
+var height = 0;
+
+$("ul li").each(function () {
+    height += $(this).outerHeight(true); // to include margins
+});
+
+$('.chat-box .chat-list').animate({
+    scrollTop: height
+});
+
+load_data = {
+    'fetch': 1
+};
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('input[name=_token]').val()
+    }
+});
+
+window.setInterval(function () {
+    $('.chat-box .chat-list');
+    $.ajax({
+        url: "messages",
+        type: 'post',
+        data: load_data,
+        dataType: 'json',
+        success: function (data) {
+            $('.chat-box .chat-list').html(data.data);
+        }
+    });
+}, 3000);
+
+$("#submit").click(function () {
+
+    let message = document.getElementById("message").value;
+
+    $.ajax({
+        url: "send",
+        type: 'post',
+        data: {'message': message},
+        dataType: 'json',
+        success: function (data) {
+            // $(data.data).hide().appendTo('.chat-box .chat-list').fadeIn();
+            $('#chat-error').addClass('hidden');
+            $('#message').removeClass('invalid');
+            $("#message").val("");
+            $('.chat-box .chat-list').animate({
+                scrollTop: height
+            });
+        },
+        error: function (data) {
+            $('#message').addClass('invalid');
+            $('#chat-error').removeClass('hidden');
+            $('#chat-error').text(error.responseText);
+        }
+    });
+
+});
