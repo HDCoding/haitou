b17d59686f1082977f695095b38b9a979576f5cf
John <juaorok@hotmail.com>
2020-03-06 15:30:24 -0300

    Create Chatrooms.vue

---
+++ resources/js/components/chat/Chatrooms.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="message-center chat-scroll">
+            <!-- Groups -->
+            <a v-for="chatroom in chatrooms" href="#" class="message-item">
+                <span class="user-img">
+                    <img :src="chatroom.image" :alt="chatroom.name" class="img-rounded">
+                    <span class="profile-status online pull-right"></span>
+                </span>
+                <div class="mail-contnet">
+                    <h5 class="message-title">{{ chatroom.name }}</h5>
+                    <span class="time">{{ chatroom.updated_at }}</span>
+                </div>
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Chatrooms",
+
+        props: {
+            currentchatroom: {
+                type: Object,
+                required: true,
+            },
+            chatrooms: {
+                type: Array,
+            }
+        },
+        data: function () {
+            return {
+                currentChatroomValue: this.currentchatroom
+            }
+        },
+        methods: {
+            onChatroomChange() {
+                this.$emit('changechatroom', this.currentChatroomValue)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
