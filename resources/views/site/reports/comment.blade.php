093a099651b559670c9375e3e9a682fa6bd180b4
John <juaorok@hotmail.com>
2020-03-10 16:29:33 -0300

    Update comment.blade.php

--- resources/views/site/reports/comment.blade.php
+++ resources/views/site/reports/comment.blade.php
@@ -7,7 +7,7 @@
     <div class="page-breadcrumb">
         <div class="row">
             <div class="col-5 align-self-center">
-                <h4 class="page-title">Staff Painel</h4>
+                <h4 class="page-title">Home</h4>
                 <div class="d-flex align-items-center">
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
