ce332ca151141bf9890c6198d90bf5cb8c2a4fb6
John <juaorok@hotmail.com>
2020-03-10 16:29:31 -0300

    Update result.blade.php

--- resources/views/site/polls/result.blade.php
+++ resources/views/site/polls/result.blade.php
@@ -12,6 +12,7 @@
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('polls') }}">Pesquisas</a></li>
                             <li class="breadcrumb-item active" aria-current="page">Resultados da pesquisa</li>
                         </ol>
                     </nav>
