1d51783ce3ca334d0734b0f5503e2268f3a629ae
John <juaorok@hotmail.com>
2020-11-09 13:10:31 -0300

    Update block_last_users_online.blade.php

--- resources/views/site/home/block_last_users_online.blade.php
+++ resources/views/site/home/block_last_users_online.blade.php
@@ -5,13 +5,13 @@
         </h5>
     </div>
     <div class="card-body">
-        @foreach ($users as $user)
-            @if ($user->show_profile == false)
+        @foreach($users as $user)
+            @if($user->show_profile == false)
                 <span class="badge text-orange font-weight-bold" style="margin-bottom: 10px;">
                     <i class="fa fa-user-ninja"></i> Escondido
                     @if (auth()->user()->can('usuarios-mod'))
                         <a href="{{ route('user.profile', ['slug' => $user->slug]) }}"> {{ $user->username }}
-                            @if ($user->is_warned == true)
+                            @if($user->is_warned == true)
                                 <i class="fa fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" data-original-title="Aviso ativo"></i>
                             @endif
                         </a>
@@ -22,7 +22,7 @@
                     <span class="badge font-weight-bold" style="color:{{ $user->group->color }}; margin-bottom: 10px;">
                         <i class="{{ $user->group->icon }}" data-toggle="tooltip" data-original-title="{{ $user->group->name }}"></i>
                         {{ $user->username }}
-                        @if ($user->is_warned == true)
+                        @if($user->is_warned == true)
                             <i class="fa fa-exclamation-circle text-orange" aria-hidden="true" data-toggle="tooltip" data-original-title="Aviso ativo"></i>
                         @endif
                     </span>
@@ -37,7 +37,7 @@
                     <span class="badge badge-pill badge-light text-orange font-weight-bold mr-2">
                         <i class="fa fa-eye-slash" aria-hidden="true"></i> Escondido
                     </span>
-                    @foreach ($groups as $group)
+                    @foreach($groups as $group)
                         <span class="badge badge-pill badge-light font-weight-bold mr-2" style="color:{{ $group->color }};">
                             <i class="{{ $group->icon }}" aria-hidden="true"></i> {{ $group->name }}
                         </span>
