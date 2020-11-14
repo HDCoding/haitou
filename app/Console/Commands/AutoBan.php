46d9f5c3b0e1fb9a1d01a405e6c514c1c084e4c2
John <juaorok@hotmail.com>
2020-11-14 20:36:51 -0300

    Update AutoBan.php

--- app/Console/Commands/AutoBan.php
+++ app/Console/Commands/AutoBan.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Jobs\SendBannedJob;
+use App\Models\Moderate;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class AutoBan extends Command
 {
@@ -18,7 +21,7 @@ class AutoBan extends Command
      *
      * @var string
      */
-    protected $description = 'Ban User If Has More Than X Active Warnings.';
+    protected $description = 'Ban User If Has More Than 7 Active Warnings.';
 
     /**
      * Create a new command instance.
@@ -37,6 +40,32 @@ class AutoBan extends Command
      */
     public function handle()
     {
-        //TODO
+        $warnings = Moderate::with('user:id,email,username,status,disabled_at')->select(DB::raw('user_id, count(*) as warnings'))
+            ->where('is_banned', '=', false)
+            ->where('is_enabled', '=', false)
+            ->groupBy('user_id')
+            ->having('warnings', '>=', 7)
+            ->get();
+
+        foreach ($warnings as $warning) {
+            // If User Has 7 or More Active Warnings Ban Set to Banned
+
+            $modUser = new Moderate();
+            $modUser->user_id = $warning->user_id;
+            $modUser->staff_id = 1;
+            $modUser->title = 'Limite de advertência atingido';
+            $modUser->description = 'Limite de advertência atingido, tem 7 avisos.';
+            $modUser->is_banned = true;
+            $modUser->save();
+
+            DB::table('users')
+                ->where('id', '=', $warning->user_id)
+                ->update(['status' => 4, 'disabled_at' => now()]);
+
+            $email = $warning->user()->email;
+            $username = $warning->user()->username;
+
+            dispatch(new SendBannedJob($email, $username));
+        }
     }
 }
