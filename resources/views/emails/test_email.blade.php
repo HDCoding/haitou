43b28e73d2f8987931f40b004b32b127bf714765
John <juaorok@hotmail.com>
2020-11-15 18:53:28 -0300

    Create test_email.blade.php

---
+++ resources/views/emails/test_email.blade.php
@@ -0,0 +1,7 @@
+@component('mail::message')
+    # Email de teste
+    Seu e-mail de teste foi entregue com sucesso! Parece que suas configurações de e-mail estão corretas!
+
+    Equipe,
+    Haitou
+@endcomponent
