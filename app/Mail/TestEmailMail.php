5404a4f66bb0e4cd802e80fa2bc970cffa2fe1a3
John <juaorok@hotmail.com>
2020-11-15 18:53:35 -0300

    Create TestEmailMail.php

---
+++ app/Mail/TestEmailMail.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEmailMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Teste E-mail!')
+            ->markdown('emails.test_email');
+    }
+}
