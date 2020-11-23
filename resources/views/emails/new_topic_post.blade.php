7c78b9e6945dde637a2fcb6b1b0660ff1b7299e8
John <juaorok@hotmail.com>
2020-11-23 12:32:21 -0300

    Update new_topic_post.blade.php

--- resources/views/emails/new_topic_post.blade.php
+++ resources/views/emails/new_topic_post.blade.php
@@ -1,20 +1,19 @@
 @component('mail::message')
 
-Olá {{ $username }},
+    Olá {{ $username }},
 
-Você recebeu este email porque você monitoriza o tópico "{{ $topic_name }}".
-O tópico recebeu uma resposta desde a sua última visita.
+    Você recebeu este email porque você monitoriza o tópico "{{ $topic_name }}".
+    O tópico recebeu uma resposta desde a sua última visita.
 
-Ao clicar no link, você poderá visualizar as respostas que foram feitas.
-Nenhuma outra notificação será enviada antes que você visite o tópico.
+    Ao clicar no link, você poderá visualizar as respostas que foram feitas.
+    Nenhuma outra notificação será enviada antes que você visite o tópico.
 
-{{ $link }}
+    {{ $link }}
 
-@component('mail::button', ['url' => url($link), 'color' => 'blue', 'target' => '_blank'])
-    Acessar link
-@endcomponent
+    @component('mail::button', ['url' => url($link), 'color' => 'blue', 'target' => '_blank'])
+        Acessar link
+    @endcomponent
 
---
-
-{{ $site_name }}
+    Equipe,
+    {{ $site_name }}
 @endcomponent
