02c8603b53a7bf7bb527fae248595ee21214475e
Joao <juaorok@hotmail.com>
2021-03-30 14:08:16 -0300

    Update CalendarsController.php

--- app/Http/Controllers/Api/CalendarsController.php
+++ app/Http/Controllers/Api/CalendarsController.php
@@ -22,48 +22,4 @@ class CalendarsController extends Controller
         return CalendarResource::collection($calendars);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
